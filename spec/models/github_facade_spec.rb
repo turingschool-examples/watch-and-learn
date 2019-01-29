0bedea0b41df7c53d055dd6ff1a0a13beefbb6a1
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-29 10:59:02 -0700

    adds spec for github_facade - owned_repos

--- spec/models/github_facade_spec.rb
+++ spec/models/github_facade_spec.rb
@@ -5,13 +5,21 @@ describe GithubFacade, type: :model do
     github_facade = GithubFacade.new(ENV["GITHUB_API_KEY"])
     expect(github_facade).to be_a(GithubFacade)
   end
+
   describe 'instance methods' do
-    context '#repos' do
-      it 'returns num of github repos given valid key' do
+    context '#owned_repos' do
+      it 'returns num of repos for user given valid key' do
+        json_response = File.open('./spec/fixtures/github_owner_repos.json')
+        stub_request(:get, "https://api.github.com/user/repos?affiliation=owner").to_return(status: 200, body: json_response)
         github_facade = GithubFacade.new(ENV["GITHUB_API_KEY"])
-        expect(github_facade.repos.count).to eq(5)
-        expect(github_facade.repos(7).count).to eq(7)
-        expect(github_facade.repos.first).to be_a(Repo)
+
+        repos = github_facade.owned_repos(5)
+        expect(repos.count).to eq(5)
+        expect(repos.first).to be_a(Repo)
+
+        repos = github_facade.owned_repos(10)
+        expect(repos.count).to eq(10)
+        expect(repos.first).to be_a(Repo)
       end
     end
   end
