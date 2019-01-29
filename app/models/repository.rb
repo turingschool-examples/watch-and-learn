3f2e232edfbdccc3c50c9c1bc423d890c71ef4de
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-29 15:53:39 -0700

    Changes data hash from string to symbol.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,7 +2,7 @@ class Repository
   attr_reader :url, :name
 
   def initialize(data)
-    @url = data["html_url"]
-    @name = data["name"]
+    @url = data[:html_url]
+    @name = data[:name]
   end
 end
