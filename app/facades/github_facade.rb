4f547975666edc33a52f1db2997a932d7c98f58e
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-29 12:58:06 -0700

    adds default param to owned_repos and proper break statement

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -3,11 +3,11 @@ class GithubFacade
     @github_key = github_key
   end
 
-  def owned_repos(num)
+  def owned_repos(num=5)
     repos = []
     service.owned_repos.each_with_index do |raw_repo, i|
       repos << Repo.new(raw_repo)
-      break if i == num
+      break if i == num - 1
     end
     repos
   end
