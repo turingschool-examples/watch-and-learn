89daabc9df1dfcc79e26064d9ff01ea067ab14bf
Leah Miller <lkmiller.87@gmail.com>
2019-01-29 11:50:05 -0700

    Adds relationship tests for users, videos, and user_videos

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Video, type: :model do
   describe 'relationships' do
     it { should belong_to(:tutorial)}
+    it {should have_many(:user_videos)}
+    it {should have_many(:users).through(:user_videos)}
   end
 end
