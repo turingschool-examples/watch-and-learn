51f590f9c7c5675bce53c7ac0b3ac30d721f3b04
Maddie Jones <maddyg91@gmail.com>
2019-01-29 14:14:09 -0700

    Adds repo_by_user code

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,4 +2,15 @@ class GithubService
   def initialize(user)
     @user = user
   end
+
+  def repos_by_user
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+           faraday.headers["Authentication"] = @user.token
+           faraday.adapter Faraday.default_adapter
+         end
+
+    response = conn.get("user/repos")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
 end
