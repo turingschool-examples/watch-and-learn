54173ba61bdd47fe606cc14704e0521e1a8e04bd
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-29 15:26:37 -0700

    adds GithubService#following

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -11,6 +11,10 @@ class GithubService
     get_json("/user/followers")
   end
 
+  def following
+    get_json("/user/following")    
+  end
+
   def get_json(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
