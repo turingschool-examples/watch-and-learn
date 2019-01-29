3da5d919ab6f42c584756968b31a1901c5c8d17f
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-29 13:52:12 -0700

    adds spec for github followers on user dashboard

--- spec/features/user/user_sees_github_followers_spec.rb
+++ spec/features/user/user_sees_github_followers_spec.rb
@@ -36,5 +36,17 @@ describe 'User Dashboard' do
         end
       end
     end
+
+    context 'when not logged in with user credentials' do
+      it 'does not show github followers section' do
+        user = create(:user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        visit dashboard_path
+
+        expect(page).to_not have_css(".github")
+        expect(page).to_not have_css(".followers")
+        expect(page).to_not have_css(".follower")
+      end
+    end
   end
 end
