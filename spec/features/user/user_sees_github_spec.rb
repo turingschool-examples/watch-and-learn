bdab90ff77357acc9a1d12588eb166487b2f945e
abenetka <ali.benetka@gmail.com>
2019-01-29 18:35:57 -0700

    Updates test for token and expected github repos

--- spec/features/user/user_sees_github_spec.rb
+++ spec/features/user/user_sees_github_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe 'as a logged in user' do
-  it 'should see a section for Github with a list of 5 repositories linking to github' do
-    user = create(:user)
+  it 'should see a section for Github with a list of 5 repositories linking to github', :vcr do
+    user = create(:user, token: "cheezytoken")
+    token = user.token
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -11,7 +12,7 @@ describe 'as a logged in user' do
     expect(page).to have_content("Github")
 
     within('.github') do
-      # expect(page).to have_link('repo_name')
+      expect(page).to have_link("2win_playlist")
       expect(page).to have_css(".repository", count: 5)
     end
   end
