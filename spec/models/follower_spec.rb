6eaf1408aab260678c2331856f2ae1446d736412
Leah Miller <lkmiller.87@gmail.com>
2019-01-30 16:00:22 -0700

    Adds update to naming convention for followers

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe Follower do
   it "exists" do
     attributes = {}
-    repository = Follower.new(attributes)
+    follower = Follower.new(attributes)
 
-    expect(repository).to be_a(Follower)
+    expect(follower).to be_a(Follower)
   end
   it "has attributes" do
     attributes = { login: "bradpsheehan",
                   html_url: "https://github.com/bradpsheehan"
                   }
     follower = Follower.new(attributes)
-    
+
     expect(follower.login).to eq("bradpsheehan")
     expect(follower.html_url).to eq("https://github.com/bradpsheehan")
   end
