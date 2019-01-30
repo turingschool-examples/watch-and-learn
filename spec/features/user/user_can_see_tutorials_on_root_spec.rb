c9694a26f9cefb02cfc1b3387524a0ce21fab357
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-30 14:21:46 -0700

    user can see public or classroom tutorials

--- spec/features/user/user_can_see_tutorials_on_root_spec.rb
+++ spec/features/user/user_can_see_tutorials_on_root_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-describe 'visitor sees a video show' do
-
-  it 'visitor does not see videos that have a classroom label' do
+describe 'user sees all tutorials' do
+  it 'user can see all tutorials' do
     tutorial = create(:tutorial, classroom: false)
-    tutorial_2 = create(:tutorial, classroom: false)
+    tutorial_2 = create(:tutorial, classroom: true)
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
