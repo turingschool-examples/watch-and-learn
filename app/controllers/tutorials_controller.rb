3daff6227b54067f0097e47e9152f1b17ebfb98a
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-30 14:16:17 -0700

    visitor can not see a tutorial that is considered classroom content

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,14 @@
 class TutorialsController < ApplicationController
+  before_action :require_user!
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
+  end
+
+private
+
+  def require_user!
+    four_oh_four unless current_user || Tutorial.where('tutorials.classroom = ?', false)
   end
 end
