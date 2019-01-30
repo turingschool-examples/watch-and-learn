33c88cb8d1b2d9dd31f9f8cf547be0db6e7de5cd
Leah Miller <lkmiller.87@gmail.com>
2019-01-30 09:37:42 -0700

    Adds test for logged in user without a token

--- spec/features/user/user_sees_github_spec.rb
+++ spec/features/user/user_sees_github_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 
-describe 'as a logged in user' do
-  it 'should see a section for Github with a list of 5 repositories linking to github', :vcr do
-    user = create(:user, token: "cheezytoken")
-    token = user.token
+describe 'dashboard' do
+  context 'as a logged in user with a token' do
+    it 'sees Github list of 5 repositories linking to repo', :vcr do
+      user = create(:user, token: "cheezytoken")
+      token = user.token
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
+      visit '/dashboard'
 
-    expect(page).to have_content("Github")
+      expect(page).to have_content("Github")
 
-    within('.github') do
-      expect(page).to have_link("2win_playlist")
-      expect(page).to have_css(".repository", count: 5)
+      within('.github') do
+        expect(page).to have_link("2win_playlist")
+        expect(page).to have_css(".repository", count: 5)
+      end
+    end
+    context 'as a logged in user without a token' do
+      it "does not see links to github repos" do
+        user = create(:user)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit '/dashboard'
+
+        expect(page).to have_content("Github")
+        expect(page).to_not have_css(".repository")
+        expect(page).to have_content("No token generated")
+      end
+    end
+    context 'as a visitor not logged in' do
+
     end
   end
 end
