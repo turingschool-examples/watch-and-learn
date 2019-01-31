d5c3a8d57a4df4e1949244fa918b0576a1a1b001
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-31 10:28:35 -0700

    adds initial spec for connecting Github

--- spec/features/user/user_can_connect_github_profile_spec.rb
+++ spec/features/user/user_can_connect_github_profile_spec.rb
@@ -7,18 +7,19 @@ describe 'Github Oauth' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit dashboard_path
-      expect(page).to have_link("Connect to Github", href: "https://github.com/login/oauth/authorize")
+      expect(page).to have_link("Connect to Github")
     end
     context "when clicking 'Connect to Github'" do
       before :each do
         user = create(:user)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-        click_button "Connect to Github"
+        visit dashboard_path
+        click_link "Connect to Github"
       end
       it 'goes through Oauth process and redirects back' do
         expect(current_path).to eq(dashboard_path)
       end
-      it 'shows content from Github profile' do
+      xit 'shows content from Github profile' do
 
       end
     end
