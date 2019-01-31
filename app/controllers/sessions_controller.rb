63c02454af9060106899cfa8622860989316509d
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-31 15:01:23 -0700

    adds passing test for omnioauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    unless params[:code]
+    unless auth_hash
       user = User.find_by(email: params[:session][:email])
       if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
--- spec/features/user/user_oath_spec.rb
+++ spec/features/user/user_oath_spec.rb
@@ -9,4 +9,18 @@ describe "User Oath funtionality" do
 
     expect(page).to have_link("Connect to Github")
   end
+  it "testing for omnioath" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :credentials => {"token" => ENV["GITHUB_API_KEY"]},
+      :info => {"nickname" => "funny guy"}
+      })
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/dashboard"
+    click_on "Connect to Github"
+
+    expect(page.status_code).to eq(200)
+  end
 end
