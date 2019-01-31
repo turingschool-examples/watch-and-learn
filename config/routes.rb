156f4c8385d64605d80c5e7f84d9797c2bb197d0
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-31 13:17:10 -0700

    adds github_login to routes (omniauth)

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
   get '/auth/github/callback', to: 'oauth#create'
+  get '/auth/github', as: :github_login
 
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
