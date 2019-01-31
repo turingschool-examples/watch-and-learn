bc8539c0606a3f8ff2f0df1636e2cbe5b0c51d11
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-31 13:17:54 -0700

    adds pulling access token from env variable - omniauth

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,11 +1,16 @@
 class OauthController < ApplicationController
   def create
-    access_token = GithubService.retrieve_access_token(params[:code])
-    # current_user.update(github_key: access_token)
-    current_user.github_key = access_token
-    current_user.password = "asdhgahg342gh"
+    access_token = retrieve_access_token
+    current_user.github_key = "token #{access_token}"
+    current_user.password = current_user.password
     current_user.save!
     binding.pry
     redirect_to dashboard_path
+  end
+
+  private
+
+  def retrieve_access_token
+    request.env["omniauth.auth"]["credentials"]["token"]
   end
 end
