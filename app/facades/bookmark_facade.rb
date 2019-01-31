513bbe1bbb38061f543186f70b0802f804f4063c
Ben Lee <bendelonlee@gmail.com>
2019-01-31 14:36:21 -0700

    Improves test to test for position order. It already is ordered by the tutorial model

--- app/facades/bookmark_facade.rb
+++ app/facades/bookmark_facade.rb
@@ -4,6 +4,6 @@ class BookmarkFacade
   end
 
   def tutorials
-    Tutorial.joins(videos: :users).where(users: {id: @user.id}).includes(:videos)
+    Tutorial.joins(videos: :users).where(users: {id: @user.id}).group(:id).includes(:videos)
   end
 end
--- spec/features/user/user_can_bookmark_a_video_spec.rb
+++ spec/features/user/user_can_bookmark_a_video_spec.rb
@@ -34,9 +34,11 @@ describe 'A registered user' do
 
   it 'shows added bookmarks on the user dashboard' do
     tutorial= create(:tutorial)
-    video = create(:video, tutorial_id: tutorial.id)
+    video_1 = create(:video, tutorial_id: tutorial.id, position: 2)
+    video_2 = create(:video, tutorial_id: tutorial.id, position: 1)
     user = create(:user)
-    user_video = create(:user_video, user: user, video: video)
+    user_video = create(:user_video, user: user, video: video_1)
+    user_video = create(:user_video, user: user, video: video_2)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -45,7 +47,7 @@ describe 'A registered user' do
     within '#bookmarked_segments' do
       expect(page).to have_content(tutorial.title)
       within "#tutorial-#{tutorial.id}" do
-        expect(page).to have_content(video.title)
+        expect(page).to have_content("#{video_2.title} #{video_1.title}")
       end
     end
   end
