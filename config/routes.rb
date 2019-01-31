c5c48905e35380c18f22569c8e59388435d36dee
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-31 10:29:38 -0700

    adds github callback to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
+  get '/auth/github/callback', to: 'oauth#create'
 
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
