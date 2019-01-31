50db85819fa3df894c95b6610600807e65a987cc
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-31 13:16:50 -0700

    updates oauth spec with mocking omniauth and showing github content

--- spec/features/user/user_can_connect_github_profile_spec.rb
+++ spec/features/user/user_can_connect_github_profile_spec.rb
@@ -13,14 +13,51 @@ describe 'Github Oauth' do
       before :each do
         user = create(:user)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        stub_omniauth
+        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+
         visit dashboard_path
         click_link "Connect to Github"
       end
-      it 'goes through Oauth process and redirects back' do
+      it 'redirects back to dashboard and shows content from Github profile' do
         expect(current_path).to eq(dashboard_path)
-      end
-      xit 'shows content from Github profile' do
+        
+        json_response = File.read('./spec/fixtures/github_owner_repos.json')
+        repos_json = JSON.parse(json_response, symbolize_names: true)[0..4]
 
+        json_response = File.read('./spec/fixtures/github_user_followers.json')
+        followers_json = JSON.parse(json_response, symbolize_names: true)
+
+        json_response = File.read('./spec/fixtures/github_user_following.json')
+        following_json = JSON.parse(json_response, symbolize_names: true)
+
+        within ".github" do
+          within ".repositories" do
+            repos_json.each do |repo|
+              expect(page).to have_link("#{repo[:name]}", href: "#{repo[:html_url]}")
+            end
+          end
+          within '.followers' do
+            followers_json.each do |follower|
+              expect(page).to have_link("#{follower[:login]}", href: "#{follower[:html_url]}")
+            end
+          end
+          within '.following' do
+            following_json.each do |user|
+              expect(page).to have_link("#{user[:login]}", href: "#{user[:html_url]}")
+            end
+          end
+        end
+      end
+      def stub_omniauth
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:github] = OmniAuth::Authhash.new({
+          provider: 'github',
+          credentials: {
+            token: ENV["GITHUB_API_KEY"]
+          }
+        })
       end
     end
   end
