05883eb9f99a5fd6a14a72ae51c1b2cfe83ee545
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-01 16:42:25 -0700

    adds capybara-email config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'vcr'
+require 'capybara/email/rspec'
 require 'webmock/rspec'
 
 VCR.configure do |config|
