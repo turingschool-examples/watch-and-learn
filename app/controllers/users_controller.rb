73ca9537f280f5c9afb7c0aaa26ebdfd53fcb903
Ben Lee <bendelonlee@gmail.com>
2019-02-01 16:45:52 -0700

    Adds activation instructions in flash message after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:message] = "This account has not yet been activated. Please check your email."
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -24,12 +24,13 @@ describe 'vister can create an account', :js do
     fill_in 'user[password]', with: password
     fill_in 'user[password_confirmation]', with: password
 
-      click_on'Create Account'
-      expect(current_path).to eq(dashboard_path)
+    click_on'Create Account'
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
 
-      expect(page).to have_content(email)
-      expect(page).to have_content(first_name)
-      expect(page).to have_content(last_name)
-      expect(page).to_not have_content('Sign In')
+    expect(page).to have_content(email)
+    expect(page).to have_content(first_name)
+    expect(page).to have_content(last_name)
+    expect(page).to_not have_content('Sign In')
   end
 end
