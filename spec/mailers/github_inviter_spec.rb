e58d2c3e3e4f53b84eb232255fe8c76ca1c85611
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-01 17:12:16 -0700

    adds initial spec for invite mailer

--- spec/mailers/github_inviter_spec.rb
+++ spec/mailers/github_inviter_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe GithubInviterMailer, type: :mailer do
   context '.invite' do
     it 'sends an invitation email with proper format' do
       user = create(:user, first_name: 'Jon', last_name: 'Doe')
-      GithubInviterMailer.invite(user, )
+      GithubInviterMailer.invite(user, 'smtp://127.0.0.1:1025')
+      open_email('smtp://127.0.0.1:1025')
       # Hello <INVITEE_NAME_AS_IT_APPEARS_ON_GITHUB>,
       # <INVITER_NAME_AS_IT_APPEARS_ON_GITHUB> has invited you to join <YOUR_APP_NAME>. You can create an account <here (should be a link to /signup)>.
     end
