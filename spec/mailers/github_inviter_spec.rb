26044f599a05ba9d65c415135ff402acf0fff2dc
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-01 18:31:54 -0700

    adds passing object to GithubInviteMailer, adds .deliver_now

--- spec/mailers/github_inviter_spec.rb
+++ spec/mailers/github_inviter_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe GithubInviterMailer, type: :mailer do
   context '.invite' do
     it 'sends an invitation email with proper format' do
       user = create(:user, first_name: 'Jon', last_name: 'Doe')
-      GithubInviterMailer.invite(user, 'smtp://127.0.0.1:1025')
+      invitee = GithubUser.new({login: "stoic-plus", html_url: "https://api.github.com/users/stoic-plus", email: "smtp://127.0.0.1:1025"})
+
+      GithubInviterMailer.invite(user, invitee).deliver_now
       open_email('smtp://127.0.0.1:1025')
-      # Hello <INVITEE_NAME_AS_IT_APPEARS_ON_GITHUB>,
-      # <INVITER_NAME_AS_IT_APPEARS_ON_GITHUB> has invited you to join <YOUR_APP_NAME>. You can create an account <here (should be a link to /signup)>.
     end
   end
 end
