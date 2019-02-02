908a03db09ee8ca8444c98b702685dfa39b87ed0
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-02 08:58:18 -0700

    adds configuration in test enviroment for action mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # default host url
-  config.action_mailer.default_url_options = { :host => "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # config.active_job.queue_adapter = :inline
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
