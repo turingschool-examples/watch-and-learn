85765be7bdffa371b72412bc1dfb2c26a9036299
Ben Lee <bendelonlee@gmail.com>
2019-02-02 16:02:05 -0700

    WIP

--- spec/features/user/github/user_can_connect_to_github_spec.rb
+++ spec/features/user/github/user_can_connect_to_github_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 describe 'as a user on the dashboard screen' do
-  xit 'can connect to Github' do
+  before(:each) do
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :provider => 'github',
+      :uid => '12345'
+      # etc.
+    })
+  end
+  it 'can connect to Github' do
+
     Capybara.app = OauthWorkshop::Application
 
     visit '/dashboard'
 
     click_on 'Connect Your Github'
-
+    require 'pry'; binding.pry
 
   end
 end
