9d0e9cf3fc1aa940862607b8a8319e60513ede1d
Maddie Jones <maddyg91@gmail.com>
2019-02-03 12:03:36 -0700

    Adds test for adding Friends
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

--- spec/features/user/user_sees_friendship_spec.rb
+++ spec/features/user/user_sees_friendship_spec.rb
@@ -17,5 +17,29 @@ describe 'As a logged in user' do
         end
       end
     end
+    it 'adds friends and shows a list', :vcr do
+      user = create(:user, github_token: ENV['GITHUB_TOKEN'])
+      follower = create(:user, github_token: 'pizza', uid: '22285337')
+      follower_2 = create(:user, github_token: 'pizza', uid: '42525195')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      expect(page).to_not have_content('Friends')
+
+      within first('.follower') do
+        click_link 'Add as Friend'
+      end
+
+      expect(current_path).to eq(dashboard_path)
+
+      within '.friends' do
+        expect(page).to have_content(follower.first_name)
+        expect(page).to have_content(follower.last_name)
+        expect(page).to_not have_content(follower_2.first_name)
+        expect(page).to_not have_content(follower_2.last_name)
+      end
+    end
   end
 end
