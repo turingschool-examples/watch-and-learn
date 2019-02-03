70066b57cfa38c730603625eb5bd583b63f9297e
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-02 18:22:05 -0700

    Adds model test for no_classroom_content method.

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'class methods' do
+    it '.no_classroom_content' do
+      create(:tutorial, classroom: true)
+      create(:tutorial, classroom: true)
+      create(:tutorial)
+      create(:tutorial)
+      tutorials = Tutorial.no_classroom_content
+
+      expect(tutorials.count).to eq(2)
+    end
+  end
 end
