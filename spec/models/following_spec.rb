f4e1f7e632ad6e3378b525f7ad3cf64de40f8675
Maddie Jones <maddyg91@gmail.com>
2019-02-03 14:56:08 -0700

    Adds test for user in following model
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 describe Following do
   it 'exists' do
     attributes = {}
     following = Following.new(attributes)
-    
+
     expect(following).to be_a(Following)
   end
   it 'has attributes' do
@@ -13,8 +13,28 @@ describe Following do
                    html_url: 'github.com'
                  }
     following = Following.new(attributes)
-    
+
     expect(following.login).to eq('bob')
     expect(following.html_url).to eq('github.com')
+  end
+  it 'can have nil user' do
+    attributes = {
+                   login: 'bob',
+                   html_url: 'github.com'
+                 }
+    following = Following.new(attributes)
+
+    expect(following.user).to eq(nil)
+  end
+  it 'can have user' do
+    attributes = {
+                   login: 'bob',
+                   html_url: 'github.com',
+                   id: '12345'
+                 }
+    user = create(:user, uid: '12345')
+    following = Following.new(attributes)
+
+    expect(following.user).to eq(user)
   end
 end
