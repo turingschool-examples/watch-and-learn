d3752bb2397887ab06f73372e714d07501e8823d
Ben Lee <bendelonlee@gmail.com>
2019-02-03 11:08:01 -0700

    Deleting a tutorial deletes that tutorial's videos

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  it 'deleting a tutorial also deletes that tutorials videos' do
+    tut_1 = create(:tutorial)
+    create(:video, tutorial: tut_1)
+    create(:video, tutorial: tut_1)
+    video_3 = create(:video)
+    tut_1.destroy
+
+    expect(Video.all).to eq([video_3])
+  end
 end
