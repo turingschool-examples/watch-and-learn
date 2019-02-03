b7c0d57ecbd17280b62313e0b860b54872f819a7
Maddie Jones <maddyg91@gmail.com>
2019-02-03 11:28:00 -0700

    Adds test for Add as Friend
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

---
+++ spec/features/user/user_sees_friendship_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'As a logged in user' do
+  describe 'on dashboard page' do
+    it 'should see link Add as Friend in followers', :vcr do
+      user = create(:user, github_token: ENV['GITHUB_TOKEN'])
+      follower = create(:user, github_token: 'pizza', uid: '22285337')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      within '.github' do
+        expect(page).to have_link('Add as Friend', count: 1)
+        within first('.follower') do
+          expect(page).to have_link('Add as Friend')  
+        end
+      end
+    end
+  end
+end
