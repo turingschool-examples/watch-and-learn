7dd28d34a45aa933801164828d36fca2e291a67b
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-02 18:15:28 -0700

    Adds condition of if statement in welcome controller to account for visitor not being logged in and not seeing classroom content.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,10 @@ class WelcomeController < ApplicationController
   def index
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
+    elsif current_user
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+    else
+      @tutorials = Tutorial.no_classroom_content.paginate(:page => params[:page], :per_page => 5)
     end
   end
 end
