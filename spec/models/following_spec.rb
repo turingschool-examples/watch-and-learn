a06622135db99bf22e679edd605319bafb401184
Leah Miller <lkmiller.87@gmail.com>
2019-02-03 17:37:06 -0700

    Adds tests for user friendships

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -10,21 +10,34 @@ describe Following do
   it "has attributes" do
     attributes = { login: "bradpsheehan",
                   html_url: "https://github.com/bradpsheehan",
-                  id: 12345 }
+                  uid: nil }
     follow = Following.new(attributes)
 
     expect(follow.login).to eq("bradpsheehan")
     expect(follow.html_url).to eq("https://github.com/bradpsheehan")
-    expect(follow.id).to eq(12345)
+    expect(follow.uid).to eq(nil)
   end
 
   describe 'class methods', :vcr do
     it ".find_all_following" do
       user = create(:user, oauth_token: ENV["GITHUB_TOKEN"])
       token = user.oauth_token
-      followers = Following.find_all_following(token)
+      following = Following.find_all_following(token)
 
-      expect(followers.first).to be_an_instance_of(Following)
+      expect(following.first).to be_an_instance_of(Following)
+    end
+  end
+  describe 'instance methods', :vcr do
+    it "#has_account?" do
+      user = create(:user, oauth_token: ENV["GITHUB_TOKEN"])
+      token = user.oauth_token
+      following = Following.find_all_following(token)
+      following_1 = following.first
+
+      following_2 = Following.new(login: "wandadog", html_url: "https://github.com/wandadog", uid: 12345)
+      expect(following_1.uid).to eq(nil)
+      expect(following_1.has_account?).to eq(false)
+      expect(following_2.has_account?).to eq(true)
     end
   end
 end
