e72782a58bd29aec9413a5d5a0e08a9e0f4bfc3f
Anten Skrabec <cebarks@gmail.com>
2019-02-04 14:57:45 -0700

    changes UID find_by

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -3,7 +3,7 @@ class Github::SessionsController < ApplicationController
     access_token = request.env["omniauth.auth"]["credentials"]["token"]
     uid = request.env["omniauth.auth"]["uid"]
 
-    user = User.find_by(github_uid: uid) || current_user
+    user = current_user
 
     user.github_token = access_token
     user.github_uid = uid
