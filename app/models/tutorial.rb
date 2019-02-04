b86453eef42cafa6dab142d0c0d6f9b45f73f881
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-04 13:32:10 -0700

    Adds bookmarked method to Tutorial
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,10 @@ class Tutorial < ApplicationRecord
   has_many :videos, ->  { order(position: :ASC) }
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+  
+  def self.bookmarked(user)
+    Tutorial.joins(videos: :user_videos)
+            .where(user_videos: {user_id: user})
+            .distinct
+  end
 end
