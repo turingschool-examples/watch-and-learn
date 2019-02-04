0e05825b764635743c46801d4fa86f852978d3b5
Ben Lee <bendelonlee@gmail.com>
2019-02-04 13:48:51 -0700

    Adding friendship aspect of test is passing

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     Friendship.create(user_id: current_user.id, friend_id: params[:friend_id])
+    # flash[:error] = "friendship did not save" 
     redirect_to dashboard_path
   end
 end
