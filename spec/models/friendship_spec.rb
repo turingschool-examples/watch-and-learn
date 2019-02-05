c21135c83e75f7cf6e49c3f15ea3cd20915b2fd7
Anten Skrabec <cebarks@gmail.com>
2019-02-05 14:00:00 -0700

    adds model test for friendship

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  
+  it "works" do
+    user = create(:user)
+    user_2 = create(:user, uid: 36653285)
+
+    Friendship.create!(user: user, friend: user_2)
+
+    expect(user.friends).to eq([user_2])
+  end
 end
