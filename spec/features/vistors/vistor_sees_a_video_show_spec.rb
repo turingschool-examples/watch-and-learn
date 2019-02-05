f3471b5187855f9ea90701671b23c35fda423f22
abenetka <ali.benetka@gmail.com>
2019-02-05 14:21:43 -0700

    Adds test for tutorial with no videos

--- spec/features/vistors/vistor_sees_a_video_show_spec.rb
+++ spec/features/vistors/vistor_sees_a_video_show_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
-describe 'visitor sees a video show' do
-  it 'vistor clicks on a tutorial title from the home page' do
-    tutorial = create(:tutorial)
-    video = create(:video, tutorial_id: tutorial.id)
+describe 'As a Visitor' do
+  describe 'visitor sees a video show' do
+    it 'vistor clicks on a tutorial title from the home page' do
+      tutorial = create(:tutorial)
+      video = create(:video, tutorial_id: tutorial.id)
 
-    visit '/'
+      visit '/'
 
-    click_on tutorial.title
+      click_on tutorial.title
 
-    expect(current_path).to eq(tutorial_path(tutorial))
-    expect(page).to have_content(video.title)
-    expect(page).to have_content(tutorial.title)
+      expect(current_path).to eq(tutorial_path(tutorial))
+      expect(page).to have_content(video.title)
+      expect(page).to have_content(tutorial.title)
+    end
+  end
+
+  describe 'when they visit a tutorial show page' do
+    it 'only shows valid tutorials' do
+      tutorial = create(:tutorial)
+
+      visit '/'
+
+      click_on tutorial.title
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("The tutorial you selected has no videos.")
+    end
   end
 end
