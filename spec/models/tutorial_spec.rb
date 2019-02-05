75fc85b67b6e315a014341d848e403f032bb4596
Ben Lee <bendelonlee@gmail.com>
2019-02-05 16:09:17 -0700

    Adds validation matchers to tutorial model

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :thumbnail}
+  end
   describe 'class methods' do
     it '.no_classroom_content' do
       create(:tutorial, classroom: true)
