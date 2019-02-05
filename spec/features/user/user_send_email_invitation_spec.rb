4fcf14843caf79b5ac8fb4d171e00bca897594b0
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-05 16:06:39 -0700

    Adds feature test for github handle with no email
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- spec/features/user/user_send_email_invitation_spec.rb
+++ spec/features/user/user_send_email_invitation_spec.rb
@@ -33,5 +33,17 @@ describe 'As a registered user' do
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content('Successfully sent invite!')
     end
+    it 'can not send an invite if the user does not have email associated with Github', :vcr do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
+      visit invite_path
+      
+      fill_in 'Github Handle', with: ENV['GITHUB_HANDLE']
+      click_on 'Send Invite'
+      
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+    end
   end
 end
