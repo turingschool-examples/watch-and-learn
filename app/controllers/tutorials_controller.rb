b5655d99190d801bb19b4e671a3b2ca85f4bdb9d
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-05 13:22:31 -0700

    Initial logic for controller

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,14 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if tutorial.classroom
+      if current_user
+        @facade = TutorialFacade.new(tutorial, params[:video_id])
+      else
+        four_oh_four
+      end
+    else
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    end
   end
 end
