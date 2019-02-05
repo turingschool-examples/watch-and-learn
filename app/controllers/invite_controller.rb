3438e55032b5b553fa7a5070234f1ceab3bc7acc
jpclark6 <jpclark6@gmail.com>
2019-02-05 15:57:37 -0700

    gets invites working in development

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -8,9 +8,11 @@ class InviteController < ApplicationController
       my_github = @search_github.find_my_name
       user_email = @search_github.find_email(params[:github_handle])
       if user_email
+        binding.pry
         FriendInviterMailer.create(user_email, params[:github_handle], my_github).deliver_now
         flash[:success] = "Successfully sent invite!"
       else
+        binding.pry
         flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
       end
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+  config.action_mailer.default_url_options = { :host => "yourhost" }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
