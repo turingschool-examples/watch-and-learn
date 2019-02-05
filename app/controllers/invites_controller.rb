8cdaeb139f46fd5196d722b16a1059920e2e1a05
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-05 16:23:30 -0700

    Updates emails and info passed from controller to mailer
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,9 +4,9 @@ class InvitesController < ApplicationController
   end
   
   def create
-    email = GithubService.info_by_username(params[:github_handle])[:email]
-    if email
-      InviterMailer.invite(email).deliver_now
+    user_info = GithubService.info_by_username(params[:github_handle])
+    if user_info[:email]
+      InviterMailer.invite(user_info[:email], user_info, current_user).deliver_now
       flash[:notice] = 'Successfully sent invite!'
     else
       flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
--- app/mailers/inviter_mailer.rb
+++ app/mailers/inviter_mailer.rb
@@ -1,5 +1,7 @@
 class InviterMailer < ApplicationMailer
-  def invite(contact)
+  def invite(contact, user_info, current_user)
+    @name = user_info[:name]
+    @inviter_name = current_user.first_name + ' ' + current_user.last_name
     mail(to: contact, subject: 'Join our Turing Tutorials app!')
   end
 end
