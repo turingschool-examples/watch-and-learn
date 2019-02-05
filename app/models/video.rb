28019c1040d0c4af9850b3cacae87f6467e0e020
Ben Lee <bendelonlee@gmail.com>
2019-02-05 14:05:07 -0700

    Adds ::update_each_position to Video

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,11 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+  def self.update_each_position
+    update_count = Video.all.reduce(0) do |count, video|
+      next(count) unless [0,nil].include?(video.position)
+      video.update(position: video.tutorial.videos.maximum(:position) + 1 )
+      count += 1
+    end
+  end
 end
