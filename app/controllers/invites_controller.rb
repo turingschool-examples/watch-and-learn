3b3ce3d8817ee6129ea5530355692877f7f005dd
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-05 16:08:48 -0700

    Adds logic to controller to check for email
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,9 +4,13 @@ class InvitesController < ApplicationController
   end
   
   def create
-    user_info = GithubService.info_by_username(params[:github_handle])
-    InviterMailer.invite(user_info[:email]).deliver_now
-    flash[:notice] = 'Successfully sent invite!'
+    email = GithubService.info_by_username(params[:github_handle])[:email]
+    if email
+      InviterMailer.invite(email).deliver_now
+      flash[:notice] = 'Successfully sent invite!'
+    else
+      flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
+    end
     redirect_to dashboard_path
   end
 end
