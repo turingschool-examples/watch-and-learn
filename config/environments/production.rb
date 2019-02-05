860a59ebf0a5d15a65e77cdcd68a49876ed7548d
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-05 11:47:19 -0700

    Adds heroku link to production.rb to try and get emails to work in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { :host => 'frozen-depths-84537.herokuapp.com' }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
