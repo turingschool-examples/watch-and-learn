b69b77bf2070be3d393a0e6fb7d31660ee7d8762
Ben Lee <bendelonlee@gmail.com>
2019-02-05 15:15:00 -0700

    Adds implementation for tutorial adding sad path

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -4,6 +4,9 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   def create
+    tutorial = Tutorial.create(tutorial_create_params)
+    flash[:success] = 'Successfully created tutorial.'
+    redirect_to tutorial_path(tutorial)
   end
 
   def new
@@ -19,7 +22,10 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   private
-  def tutorial_params
+  def tutorial_update_params
     params.require(:tutorial).permit(:tag_list)
+  end
+  def tutorial_create_params
+    params.require(:tutorial).permit(:title, :description, :thumbnail)
   end
 end
