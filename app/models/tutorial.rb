543656da68b7989331a6caa122bcf91ce328f4fb
Ben Lee <bendelonlee@gmail.com>
2019-02-05 16:14:03 -0700

    Adds validations in the model to prevent nil /blank values being added to the database

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,4 +1,8 @@
 class Tutorial < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :thumbnail
+  validates_presence_of :description
+
   has_many :videos, ->  { order(position: :ASC) }, dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
