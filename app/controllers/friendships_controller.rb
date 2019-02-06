5f0b6a09233c5f102e4b78d1a73dcedbbd7bb4ba
abenetka <ali.benetka@gmail.com>
2019-02-05 20:09:44 -0700

    Implements fucntional friendship create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.new(friend_id: params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added friend."
-      redirect_to dashboard_path
+    new_friend = User.find_by(github_name: params[:login])
+    if new_friend
+      current_user.friends << new_friend
+      flash[:notice] = "Added your new Friend!"
     else
       flash[:error] = "Unable to add friend."
-      redirect_to dashboard_path
     end
+    redirect_to dashboard_path
+
   end
 
 end
