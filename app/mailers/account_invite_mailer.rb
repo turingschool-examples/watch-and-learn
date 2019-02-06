821abed6a424c6da6317e64ad1ac78fad640986e
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-06 12:42:26 -0700

    Updates invite_mailer with new argument we are passing in.

--- app/mailers/account_invite_mailer.rb
+++ app/mailers/account_invite_mailer.rb
@@ -1,7 +1,8 @@
-class AccountInviteMailer < ActionMailer::Base
-  def invite(invitee_name, email, user)
-    @user = user
-    @invitee_name = invitee_name
-    mail(to: email, subject: "#{user.first_name} invites you to the Turing Tutorials App.")
+class AccountInviteMailer < ApplicationMailer
+  def invite(facade)
+    invitation = facade.invitation
+    @invitee_name = invitation.invitee_name
+    @inviter_name = invitation.inviter_name
+    mail(to: invitation.invitee_email, subject: "#{@inviter_name} invites you to the Turing Tutorials App.")
   end
 end
