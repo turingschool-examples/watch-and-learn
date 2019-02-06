1cb3b4a7289cc521f59431725bf0f7fd2e31f740
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-06 16:16:40 -0700

    Refactors mailer argument that is passed in.

--- app/mailers/account_invite_mailer.rb
+++ app/mailers/account_invite_mailer.rb
@@ -1,6 +1,5 @@
 class AccountInviteMailer < ApplicationMailer
-  def invite(facade)
-    invitation = facade.invitation
+  def invite(invitation)
     @invitee_name = invitation.invitee_name
     @inviter_name = invitation.inviter_name
     mail(to: invitation.invitee_email, subject: "#{@inviter_name} invites you to the Turing Tutorials App.")
