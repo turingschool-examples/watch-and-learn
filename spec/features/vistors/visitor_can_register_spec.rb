405b5bfbbec034acc89374bc3b58d8b72d861d2c
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-06 15:54:01 -0700

    Adds feature test for getting account activation notice

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'vister can create an account', :js do
+describe 'Visitor can create an account', :js do
   it ' visits the home page' do
     email = 'jimbob@aol.com'
     first_name = 'Jim'
@@ -32,5 +32,26 @@ describe 'vister can create an account', :js do
     expect(page).to have_content(first_name)
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
+  end
+  it 'gets a notice that the account needs to be activated' do
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+
+    visit '/'
+    click_on 'Register'
+    
+    expect(current_path).to eq('/register')
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+    click_on 'Create Account'
+    
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Logged in as #{first_name} #{last_name}")
+    expect(page).to have_content('This account has not yet been activated. Please check your email.')
   end
 end
