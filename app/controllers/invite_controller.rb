4968cf69876dbb36a05b2a32f185acbed05af558
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-06 16:16:04 -0700

    Refactores invite controller to be under 5 lines.

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,10 +3,32 @@ class InviteController < ApplicationController
   end
 
   def create
+    if invitation.save
+      successful_invitation_actions
+    else
+      unsuccessful_invitation_actions
+    end
+  end
+
+  private
+
+  def facade
     github_handle = params[:invite][:github_handle]
-    facade = GithubInvitationFacade.new(current_user.github_token, github_handle)
-    AccountInviteMailer.invite(facade).deliver_now
+    GithubInvitationFacade.new(current_user.github_token, github_handle)
+  end
+
+  def invitation
+    @_invitation ||= facade.invitation
+  end
+
+  def successful_invitation_actions
+    AccountInviteMailer.invite(invitation).deliver_now
     flash[:success] = 'Successfully sent invite!'
+    redirect_to dashboard_path
+  end
+
+  def unsuccessful_invitation_actions
+    flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
     redirect_to dashboard_path
   end
 end
