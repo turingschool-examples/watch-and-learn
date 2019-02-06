f240dc785f32431993c3ddf4b824cae11b970458
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-05 20:37:31 -0700

    Refactors tutorials controller show method

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,14 +1,9 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    if tutorial.classroom
-      if current_user
-        @facade = TutorialFacade.new(tutorial, params[:video_id])
-      else
-        four_oh_four
-      end
-    else
-      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    unless current_user
+      four_oh_four if tutorial.classroom
     end
+    @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
 end
