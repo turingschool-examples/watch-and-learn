062b58dd12b7ad8e7269f98a874336e675f55834
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-05 20:54:27 -0700

    Refactors welcome controller and adds welcome facade

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    tutorials = get_tutorials
-    if params[:tag]
-      @tutorials = tutorials.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
-      @tutorials = tutorials.paginate(:page => params[:page], :per_page => 5)
-    end
-  end
-  
-  private
-  
-  def get_tutorials
-    if current_user
-      Tutorial.all
-    else
-      Tutorial.where(classroom: false)
-    end
+    @facade = WelcomeFacade.new(current_user, params)
   end
 end
