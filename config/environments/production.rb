87a06f269ea908befc01ebf9e9bb8529725ebd18
jpclark6 <jpclark6@gmail.com>
2019-02-06 17:47:53 -0700

    updates production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,11 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'brownfield-of-dreams-1810.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { :host => 'localhost' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
