665fae3aaf6dd7e76b77bcbf34e779b1a5241c23
abenetka <ali.benetka@gmail.com>
2019-02-06 19:53:31 -0700

    Changes the params for checking user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,6 @@
 class FriendshipsController < ApplicationController
   def create
-    new_friend = User.find_by(github_name: params[:login])
+    new_friend = User.find_by(github_name: params[:github_name])
     if new_friend
       current_user.friends << new_friend
       flash[:notice] = "Added your new Friend!"
@@ -8,7 +8,6 @@ class FriendshipsController < ApplicationController
       flash[:error] = "Unable to add friend."
     end
     redirect_to dashboard_path
-
   end
 
 end
