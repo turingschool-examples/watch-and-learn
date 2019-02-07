c7e185b90fba40b72f7132c70090d17548e8f728
Maddie Jones <maddyg91@gmail.com>
2019-02-06 19:55:39 -0700

    Adds test for video dependent destroy
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,5 +5,15 @@ describe Video do
     it { should have_many(:user_videos) }
     it { should have_many(:users).through(:user_videos) }
     it { should belong_to(:tutorial) }
+    it 'should be deleted when tutorial is deleted' do
+      tutorial = create(:tutorial)
+      video_1 = create(:video, tutorial: tutorial)
+      video_2 = create(:video, tutorial: tutorial)
+
+      expect(Video.all).to eq([video_1, video_2])
+      tutorial.destroy
+
+      expect(Video.all).to eq([])
+    end
   end
 end
