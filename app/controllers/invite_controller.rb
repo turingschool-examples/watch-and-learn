f1bd45a4dd4f712677e94f54ddab53ed95afe296
jpclark6 <jpclark6@gmail.com>
2019-02-07 09:12:40 -0700

    tests passing after refactor

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -13,10 +13,10 @@ class InviteController < ApplicationController
     @search_github = SearchGithubFacade.new(current_user.token)
     my_github = @search_github.find_my_name
     user_email = @search_github.find_email(params[:github_handle])
-    deliver_if_valid(user_email)
+    deliver_if_valid(user_email, my_github)
   end
 
-  def deliver_if_valid(user_email)
+  def deliver_if_valid(user_email, my_github)
     if user_email
       FriendInviterMailer.create(user_email, params[:github_handle], my_github).deliver_now
       flash[:success] = "Successfully sent invite!"
