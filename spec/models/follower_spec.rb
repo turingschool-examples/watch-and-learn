0baa9ef09d566ef3416db7f2dad551fbcf3e5599
Ben Lee <bendelonlee@gmail.com>
2019-02-07 10:26:22 -0700

    Adds on our site? test

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Follower, type: :model do
     expect(follower.name).to eq("bla")
     expect(follower.url).to eq("http://www.website.com")
   end
+  describe 'instance methods' do
+    it '.on_our_site?' do
+      user = create(:user, github_uid: 1)
+      follower_1 = Follower.new({id: '1', login: "bla", html_url: "http://www.website.com"})
+      follower_2 = Follower.new({id: '2', login: "bla", html_url: "http://www.website.com"})
+
+      expect(follower_1.on_our_site?).to eq(true)
+      expect(follower_2.on_our_site?).to eq(false)
+    end
+  end
 end
