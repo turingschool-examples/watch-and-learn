537b97bb334893d9750faaec5fe7375f254a1dbf
Ben Lee <bendelonlee@gmail.com>
2019-02-07 10:00:47 -0700

    Adds validators on video for sad path

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,10 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  validates_presence_of :title
+  validates_presence_of :description
+  
   def self.update_each_position
     update_count = Video.all.reduce(0) do |count, video|
       next(count) unless [0,nil].include?(video.position)
