634a8871ef0029aa0be9035ab6fefdd5e5432029
Maddie Jones <maddyg91@gmail.com>
2019-02-06 19:58:04 -0700

    Adds dependent destroy to Tutorial
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,8 +1,8 @@
 class Tutorial < ApplicationRecord
-  has_many :videos, ->  { order(position: :ASC) }
+  has_many :videos, ->  { order(position: :ASC) }, dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
-  
+
   def self.bookmarked(user)
     Tutorial.includes(videos: :user_videos)
             .where(user_videos: {user_id: user})
