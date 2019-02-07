0de15d9ccff54706ad5ba0b27cc3ae86a632b2c8
Ben Lee <bendelonlee@gmail.com>
2019-02-07 08:57:29 -0700

    Improves sadpath testing and implementation for user registration, refactoring resource error messages into a partial

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'vister can create an account', :js do
+describe 'visitor can create an account' do
   it ' visits the home page' do
     email = 'jimbob@aol.com'
     first_name = 'Jim'
@@ -33,5 +33,35 @@ describe 'vister can create an account', :js do
     expect(page).to have_content(first_name)
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
+  end
+  it 'gives errors when registration info is bad' do
+    email = ''
+    first_name = ''
+    last_name = ''
+    password = ''
+    password_confirmation = ''
+
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up now.'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on'Create Account'
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Password can't be blank")
+
   end
 end
