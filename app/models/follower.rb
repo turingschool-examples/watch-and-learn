c3a354d6a277b947abba535b048d43d95ab4524e
Ben Lee <bendelonlee@gmail.com>
2019-02-07 10:35:43 -0700

    Refactors code from follower / following into a superclasss: GithubRelation

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,18 +1,2 @@
-class Follower
-  attr_reader :url, :name, :uid
-
-  def initialize(data)
-    @url = data[:html_url]
-    @name = data[:login]
-    @uid = data[:id]
-    @user = User.find_by(github_uid: uid)
-  end
-
-  def on_our_site?
-    @user ? true : false
-  end
-
-  def id
-    @user.id if @user
-  end
+class Follower < GithubRelation
 end
