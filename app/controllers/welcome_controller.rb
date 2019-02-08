f15c8ced7d5fd6e78c32609a5dacdc158066b225
Ben Lee <bendelonlee@gmail.com>
2019-02-08 08:38:14 -0700

    Refactors welcome controller to still hide classroom content when a visitor clicks on a tag, as well as to keep all methods under 5 lines long

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,35 @@
 class WelcomeController < ApplicationController
   def index
     if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    elsif current_user
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      set_tutorials_when_tag
     else
-      @tutorials = Tutorial.no_classroom_content.paginate(:page => params[:page], :per_page => 5)
+      set_tutorials_when_no_tag
     end
+  end
+
+  private
+
+  def set_tutorials_when_tag
+    if current_user
+      @tutorials = paginate(Tutorial.tagged_with(params[:tag]))
+    else
+      @tutorials = paginate(visitor_tutorials.tagged_with(params[:tag]))
+    end
+  end
+
+  def set_tutorials_when_no_tag
+    if current_user
+      @tutorials = paginate(Tutorial.all)
+    else
+      @tutorials = paginate(visitor_tutorials)
+    end
+  end
+
+  def paginate(tutorials)
+    tutorials.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def visitor_tutorials
+    Tutorial.no_classroom_content
   end
 end
