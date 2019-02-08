8d110ef8923f4646f5737758274c32f65b796f0e
abenetka <ali.benetka@gmail.com>
2019-02-07 20:40:01 -0700

    Updates friendship tests

--- spec/features/user/user_can_add_friendships_spec.rb
+++ spec/features/user/user_can_add_friendships_spec.rb
@@ -13,9 +13,9 @@ describe 'User Friendships' do
       following = facade_user.find_all_following
 
       visit '/dashboard'
-
+      save_and_open_page
       within '.following-list' do
-        within ".follow-1" do
+        within ".follow-2" do
           expect(page).to have_button("Add as Friend")
         end
       end
@@ -40,7 +40,7 @@ describe 'User Friendships' do
       visit '/dashboard'
 
       within '.following-list' do
-        within ".follow-1" do
+        within ".follow-2" do
           expect(page).to have_button("Add as Friend")
           click_button "Add as Friend"
         end
@@ -72,7 +72,7 @@ describe 'User Friendships' do
       visit '/dashboard'
 
       within '.following-list' do
-        within ".follow-1" do
+        within ".follow-2" do
           click_button "Add as Friend"
         end
       end
