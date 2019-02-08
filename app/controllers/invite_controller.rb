7edca2227287f0d0301ac744a833f8ddb9f134d8
Leah Miller <lkmiller.87@gmail.com>
2019-02-07 19:34:32 -0700

    Adds refactor for create method in invite controller

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -2,13 +2,27 @@ class InviteController < ApplicationController
   def show
   end
 
-
   def create
+    if checks_for_email.email
+      sends_email
+    else
+      returns_error_notice
+    end
+  end
+
+  def checks_for_email
     friend_email = EmailFacade.new(current_user, params[:github_handle])
-    the_email = friend_email.find_the_email
-    InviteNotifierMailer.inform(current_user, the_email).deliver_now
+    friend_email.find_the_email
+  end
+
+  def sends_email
+    InviteNotifierMailer.inform(current_user, checks_for_email).deliver_now
     flash[:notice] = "Successfully sent invite!"
     redirect_to dashboard_path
   end
 
+  def returns_error_notice
+    flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
+    redirect_to dashboard_path
+  end
 end
