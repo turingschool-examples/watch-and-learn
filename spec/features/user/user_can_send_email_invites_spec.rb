7a545f8904e3ff1fd91d371224b033c5b9a67cca
Leah Miller <lkmiller.87@gmail.com>
2019-02-07 19:34:14 -0700

    Adds additional expectations for sending emails

--- spec/features/user/user_can_send_email_invites_spec.rb
+++ spec/features/user/user_can_send_email_invites_spec.rb
@@ -12,10 +12,26 @@ describe 'as a registered user' do
 
       click_button "Send an Invite"
       expect(current_path).to eq('/invite')
-      fill_in :github_handle, with: "mgoodhart5"
+      fill_in :github_handle, with: "stoic-plus"
       click_on "Send Invite"
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Successfully sent invite!")
+    end
+    it 'returns message that contact does not have email address' do
+      user_1 = create(:user, github_token: ENV["GITHUB_TOKEN"])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      visit dashboard_path
+
+      expect(page).to have_button("Send an Invite")
+
+      click_button "Send an Invite"
+      expect(current_path).to eq('/invite')
+      fill_in :github_handle, with: "danbriechle"
+      click_on "Send Invite"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
     end
   end
 end
