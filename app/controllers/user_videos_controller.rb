c3d5bec7710191ac59fd477e042cbf0dcda7931e
Ben Lee <bendelonlee@gmail.com>
2019-02-08 07:51:52 -0700

    Refactors UserVideos#create down to five lines

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -3,15 +3,24 @@ class UserVideosController < ApplicationController
   end
 
   def create
-    if current_user_videos
-      flash[:error] = "Already in your bookmarks"
-    elsif user_video.save
-      flash[:success] = "Bookmark added to your dashboard!"
-    end
+    create_catch_duplicate_if_duplicate
+    create_succeed_if_successful
     redirect_back(fallback_location: root_path)
   end
 
   private
+
+  def create_succeed_if_successful
+    if !current_user_videos && user_video.save
+      flash[:success] = "Bookmark added to your dashboard!"
+    end
+  end
+
+  def create_catch_duplicate_if_duplicate
+    if current_user_videos
+      flash[:error] = "Already in your bookmarks"
+    end
+  end
 
   def user_video_params
     params.permit(:user_id, :video_id)
