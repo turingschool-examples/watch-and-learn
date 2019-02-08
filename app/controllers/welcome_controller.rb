27db2e58f13344b0a145422c3db31984e76d3246
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-07 18:41:15 -0700

    Removes tag condition in index method because it doesn't seem to be doing anything.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    elsif current_user
+    if current_user
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
     else
       @tutorials = Tutorial.no_classroom_content.paginate(:page => params[:page], :per_page => 5)
