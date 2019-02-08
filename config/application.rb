43ad5d210dbf72abbf1ac3d583758cfe41915ac3
abenetka <ali.benetka@gmail.com>
2019-02-07 17:07:30 -0700

    Updates config files for emails

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,17 @@ module PersonalProject
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      domain:               'localhost:3000',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+
   end
 end
