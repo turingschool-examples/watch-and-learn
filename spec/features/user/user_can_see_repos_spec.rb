3342e01beda558f295b45f347ed894b1b949232c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-19 17:11:23 -0600

    Added expectation for repo link's routes
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- spec/features/user/user_can_see_repos_spec.rb
+++ spec/features/user/user_can_see_repos_spec.rb
@@ -16,13 +16,29 @@ feature 'As a logged in user' do
         fill_in 'session[password]', with: user.password
 
         click_on 'Log In'
-
+save_and_open_page
         expect(page).to have_content("Github")
         expect(page).to have_css(".github")
         expect(page).to have_css(".repo", count: 5)
-        within(first(".repo")) do
+        within(page.all(".repo")[0]) do
           expect(page).to have_css(".repo-name")
-          expect(page).to have_link("little_shop")
+          expect(page).to have_link("little_shop", href: "https://github.com/aprildagonese/little_shop")
+        end
+        within(page.all(".repo")[1]) do
+          expect(page).to have_css(".repo-name")
+          expect(page).to have_link("book_club", href: "https://github.com/n-flint/book_club")
+        end
+        within(page.all(".repo")[2]) do
+          expect(page).to have_css(".repo-name")
+          expect(page).to have_link("activerecord-obstacle-course", href: "https://github.com/PeregrineReed/activerecord-obstacle-course")
+        end
+        within(page.all(".repo")[3]) do
+          expect(page).to have_css(".repo-name")
+          expect(page).to have_link("activerecord_exploration", href: "https://github.com/PeregrineReed/activerecord_exploration")
+        end
+        within(page.all(".repo")[4]) do
+          expect(page).to have_css(".repo-name")
+          expect(page).to have_link("apollo_14", href: "https://github.com/PeregrineReed/apollo_14")
         end
       end
     end
