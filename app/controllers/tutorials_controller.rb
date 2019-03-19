24656b0a4e26302a2e5f1dafd73f6139b3147fe7
plapicola <p.lapicola@gmail.com>
2019-03-19 17:54:36 -0600

    Adds test for tutorials index facade
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,4 +1,8 @@
 class TutorialsController < ApplicationController
+  def index
+    facade = TutorialsIndexFacade.new(current_user)
+  end
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
