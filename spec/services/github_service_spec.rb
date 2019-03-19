7e92ec0134fa2fa0a229a800902e586204df349c
Tim Allen <tnelsallen3@gmail.com>
2019-03-19 17:38:25 -0600

    Made Follower model test
    Co-authored by: Julia Marco <marcootj@gmail.com>

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -9,13 +9,24 @@ describe GithubService do
 
   describe "instance methods" do
     it "get repositories" do
-      VCR.use_cassette("github service") do
+      VCR.use_cassette("github service repos") do
         service = GithubService.new
 
         result = service.get_repos
         expect(result).to be_a(Array)
         expect(result[0]).to be_a(Hash)
         expect(result[0]).to have_key(:name)
+      end
+    end
+
+    it "get followers" do
+      VCR.use_cassette("github service followers") do
+        service = GithubService.new
+
+        result = service.get_followers
+        expect(result).to be_a(Array)
+        expect(result[0]).to be_a(Hash)
+        expect(result[0]).to have_key(:login)
       end
     end
   end
