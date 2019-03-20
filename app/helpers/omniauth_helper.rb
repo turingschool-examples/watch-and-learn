79e7bc89110fe0215f482e387aa47b693d73f1b4
plapicola <p.lapicola@gmail.com>
2019-03-20 15:37:27 -0600

    Adds testing for github connection from dashboard
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

---
+++ app/helpers/omniauth_helper.rb
@@ -0,0 +1,15 @@
+module OmniauthHelper
+  def stub_github
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github'.
+      extra: {
+
+      },
+      credentials: {
+        token: 'github_token_goes_here'
+      }
+      })
+  end
+end
--- spec/features/user/user_sees_github_on_dashboard_spec.rb
+++ spec/features/user/user_sees_github_on_dashboard_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe'when I visit my dashboard' do
   context 'as a user' do
     before :each do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      @user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       allow_any_instance_of(User).to receive(:github_token).and_return(ENV['GITHUB_API_KEY'])
     end
 
@@ -77,6 +77,35 @@ describe'when I visit my dashboard' do
             expect(page).to have_link('plapicola')
           end
         end
+      end
+    end
+
+    context 'As a user without a github token' do
+      it 'I see a link on my dashboard to connect to github' do
+        allow_any_instance_of(User).to receive(:github_token).and_return(nil)
+
+        visit dashboard_path
+
+        expect(page).to have_button 'Connect to GitHub'
+        expect(page).to_not have_content 'Following'
+        expect(page).to_not have_content 'Followers'
+        expect(page).to_not have_content 'battleship'
+      end
+
+      it 'I can connect my dashboard to github and see my information' do
+        # Login cannot be stubbed
+        visit login_path
+        fill_in :username, with: @user.email
+        fill_in :password, with: @user.password
+        click_button 'Log In'
+        stub_github
+
+        click_button 'Connect to GitHub'
+
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_content 'Following'
+        expect(page).to have_content 'Follers'
+        expect(page).to have_content 'battleship'
       end
     end
   end
