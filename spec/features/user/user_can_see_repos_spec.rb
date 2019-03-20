9a204479e8a6b50014e9d770a5263018657c59b1
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-19 18:13:03 -0600

    Added tests for edge cases for issue #34
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- spec/features/user/user_can_see_repos_spec.rb
+++ spec/features/user/user_can_see_repos_spec.rb
@@ -2,9 +2,27 @@ require 'rails_helper'
 
 feature 'As a logged in user' do
   context 'when visiting /dashboard' do
+
+    it 'sees a GitHub section only if it has a token' do
+      user = create(:user, token: nil)
+
+      visit '/'
+
+      click_on "Sign In"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
+
+      click_on 'Log In'
+
+      expect(page).to_not have_content("GitHub")
+    end
+
     it 'sees a list of five repos' do
       VCR.use_cassette('github_current_users_repos') do
-        user = create(:user)
+        user = create(:user, token: 'token')
 
         visit '/'
 
@@ -16,8 +34,8 @@ feature 'As a logged in user' do
         fill_in 'session[password]', with: user.password
 
         click_on 'Log In'
-save_and_open_page
-        expect(page).to have_content("Github")
+
+        expect(page).to have_content("GitHub")
         expect(page).to have_css(".github")
         expect(page).to have_css(".repo", count: 5)
         within(page.all(".repo")[0]) do
@@ -40,6 +58,36 @@ save_and_open_page
           expect(page).to have_css(".repo-name")
           expect(page).to have_link("apollo_14", href: "https://github.com/PeregrineReed/apollo_14")
         end
+      end
+    end
+
+    xit 'sees only five repos of current user' do
+      user = create(:user, token: 'token')
+      user_2 = create(:user, token: 'other')
+
+      user_response = File.open("./fixtures/user_github_mock_repos.json")
+      stub_request(:get, "https://api.github.com/user/repos").with(headers: {Authorization: "token #{user.token}"}).to_return(status: 200, body: user_response)
+      user = create(:user)
+
+      other_response = File.open("./fixtures/other_github_mock_repos.json")
+      stub_request(:get, "https://api.github.com/user/repos").with(headers: {Authorization: "token #{user_2.token}"}).to_return(status: 200, body: other_response)
+
+      visit '/'
+
+      click_on "Sign In"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
+
+      click_on 'Log In'
+      counter = 0
+      5.times do
+        within(page.all(".repo")[counter]) do
+          expect(page).to have_link("activerecord_exploration", href: "https://github.com/PeregrineReed/activerecord_exploration")
+        end
+        counter += 1
       end
     end
   end
