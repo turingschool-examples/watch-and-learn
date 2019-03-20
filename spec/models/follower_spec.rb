6bb58be4fea257a1496442b59824fac16caf556e
nagerz <znager@gmail.com>
2019-03-20 10:10:41 -0600

    Cleaned up merge conflicts. Consolidated to github user for followers and following.
    Co-authored-by: Teresa M Knowles <teresa07moreno@gmail.com>

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
-
-RSpec.describe Follower do
-  it 'has attributes' do
-    attributes = {login: 'plapicola',
-                  html_url: 'https://github.com/plapicola'}
-
-    follower = Follower.new(attributes)
-
-    expect(follower.name).to eq(attributes[:login])
-    expect(follower.address).to eq(attributes[:html_url])
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe Follower do
+#   it 'has attributes' do
+#     attributes = {login: 'plapicola',
+#                   html_url: 'https://github.com/plapicola'}
+#
+#     follower = Follower.new(attributes)
+#
+#     expect(follower.name).to eq(attributes[:login])
+#     expect(follower.address).to eq(attributes[:html_url])
+#   end
+# end
