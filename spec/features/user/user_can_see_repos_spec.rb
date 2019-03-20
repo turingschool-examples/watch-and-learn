f21d76030e8cf5238cb3799ddae085306e582999
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-19 21:00:39 -0600

    Refactored spec for user only seeing their own repos

--- spec/features/user/user_can_see_repos_spec.rb
+++ spec/features/user/user_can_see_repos_spec.rb
@@ -22,7 +22,7 @@ feature 'As a logged in user' do
 
     it 'sees a list of five repos' do
       VCR.use_cassette('github_current_users_repos') do
-        user = create(:user, token: 'token')
+        user = create(:user, token: ENV[])
 
         visit '/'
 
@@ -61,33 +61,36 @@ feature 'As a logged in user' do
       end
     end
 
-    xit 'sees only five repos of current user' do
-      user = create(:user, token: 'token')
-      user_2 = create(:user, token: 'other')
+    it 'sees only five repos of current user' do
+      VCR.use_cassette('github_other_users_repos') do
+        user_2 = create(:user, token: ENV['github_other_token'])
 
-      user_response = File.open("./fixtures/user_github_mock_repos.json")
-      stub_request(:get, "https://api.github.com/user/repos").with(headers: {Authorization: "token #{user.token}"}).to_return(status: 200, body: user_response)
-      user = create(:user)
+        visit '/'
 
-      other_response = File.open("./fixtures/other_github_mock_repos.json")
-      stub_request(:get, "https://api.github.com/user/repos").with(headers: {Authorization: "token #{user_2.token}"}).to_return(status: 200, body: other_response)
+        click_on "Sign In"
 
-      visit '/'
+        expect(current_path).to eq(login_path)
 
-      click_on "Sign In"
+        fill_in 'session[email]', with: user_2.email
+        fill_in 'session[password]', with: user_2.password
 
-      expect(current_path).to eq(login_path)
+        click_on 'Log In'
 
-      fill_in 'session[email]', with: user.email
-      fill_in 'session[password]', with: user.password
-
-      click_on 'Log In'
-      counter = 0
-      5.times do
-        within(page.all(".repo")[counter]) do
-          expect(page).to have_link("activerecord_exploration", href: "https://github.com/PeregrineReed/activerecord_exploration")
+        within(page.all(".repo")[0]) do
+          expect(page).to have_link("little_shop", href: "https://github.com/aprildagonese/little_shop")
         end
-        counter += 1
+        within(page.all(".repo")[1]) do
+          expect(page).to_not have_link("book_club", href: "https://github.com/n-flint/book_club")
+        end
+        within(page.all(".repo")[2]) do
+          expect(page).to_not have_link("activerecord-obstacle-course", href: "https://github.com/PeregrineReed/activerecord-obstacle-course")
+        end
+        within(page.all(".repo")[3]) do
+          expect(page).to_not have_link("activerecord_exploration", href: "https://github.com/PeregrineReed/activerecord_exploration")
+        end
+        within(page.all(".repo")[4]) do
+          expect(page).to_not have_link("apollo_14", href: "https://github.com/PeregrineReed/apollo_14")
+        end
       end
     end
   end
