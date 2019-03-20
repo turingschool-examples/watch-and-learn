f4db5fef5edd45a4f7144fe6d30a509b7c8ba00b
April Dagonese <adagonese@gmail.com>
2019-03-20 11:10:18 -0600

    Refactors test for get_repos

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -7,16 +7,19 @@ describe GithubService do
     expect(service).to be_a(GithubService)
   end
 
-  describe ".get_repos" do
-    it "gets user's repos" do
+  describe "Instance Methods" do
+    it "#get_repos" do
       VCR.use_cassette("services/get_repos") do
         repos = GithubService.new.get_repos
+
+        expect(repos[0]).to be_a(Hash)
+        expect(repos[0]).to have_key(:name)
 
         expect(repos[0][:name]).to eq("activerecord-obstacle-course")
         expect(repos[1][:name]).to eq("apollo_14")
         expect(repos[2][:name]).to eq("backend_prework")
         expect(repos[3][:name]).to eq("battleship")
-        expect(repos[4][:name]).to eq("book_club")
+        expect(repos[4][:name]).to eq("books")
       end
     end
   end
