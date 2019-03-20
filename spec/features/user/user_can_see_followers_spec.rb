cc51d9eded7c572f7332b5ec974e93b043394940
Chris Lewis <13chlewis@gmail.com>
2019-03-20 10:23:54 -0600

    Stubbed API calls working for user dashboard
    Co-authored-by: Manoj Panta <34468256+manojpanta@users.noreply.github.com>

--- spec/features/user/user_can_see_followers_spec.rb
+++ spec/features/user/user_can_see_followers_spec.rb
@@ -4,8 +4,13 @@ describe 'A registered user' do
   context 'visiting /dashboard' do
     it 'can see a list of all of their github followers' do
       user = create(:user, github_token: ENV["github_key"])
+
       filename = 'user_followers.json'
       url = "https://api.github.com/user/followers"
+      stub_get_json(url, filename)
+
+      filename = 'user_repos.json'
+      url = "https://api.github.com/user/repos"
       stub_get_json(url, filename)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
