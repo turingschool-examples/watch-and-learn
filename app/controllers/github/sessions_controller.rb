9569b0df44def271628ad1f96a3764383989f924
plapicola <p.lapicola@gmail.com>
2019-03-20 15:15:09 -0600

    Adds omniauth gem for github
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

---
+++ app/controllers/github/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Github::SessionsController < ApplicationController
+  before_action :require_login
+
+  def create
+    current_user.update(github_extraction)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  # Might want to change to model method?
+  def github_extraction
+    github_info = {}
+    omniauth_info = request.env['omniauth.auth']
+    github_info[:uid] = omniauth_info.uid
+    github_info[:github_token] = omniauth_info.credentials.token
+    github_info
+  end
+end
