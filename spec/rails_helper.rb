b7b63794f39358c6a4efc92af83cc994f94dc386
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-20 15:58:03 -0600

    Alters environment variable to reflect new token.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
   config.filter_sensitive_data("<GITHUB_API_KEY>") { ENV['GITHUB_API_KEY'] }
+  config.filter_sensitive_data("<MF_GITHUB_TOKEN>") { ENV['MF_GITHUB_TOKEN'] }
 end
 
 
