48c05bd255ed6e9987b508f850c5ee5816514c4e
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-20 18:40:31 -0600

    Changes syntax of VCR in test to - , :vcr do - .

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -12,19 +12,17 @@ describe GithubService do
   end
 
   describe "Instance Methods" do
-    it "#get_repos" do
-      VCR.use_cassette("services/get_repos") do
-        repos = GithubService.new(@user.github_token).get_repos
+    it "#get_repos", :vcr do
+      repos = GithubService.new(@user.github_token).get_repos
 
-        expect(repos[0]).to be_a(Hash)
-        expect(repos[0]).to have_key(:name)
+      expect(repos[0]).to be_a(Hash)
+      expect(repos[0]).to have_key(:name)
 
-        expect(repos[0][:name]).to eq("activerecord-obstacle-course")
-        expect(repos[1][:name]).to eq("apollo_14")
-        expect(repos[2][:name]).to eq("backend_prework")
-        expect(repos[3][:name]).to eq("battleship")
-        expect(repos[4][:name]).to eq("books")
-      end
+      expect(repos[0][:name]).to eq("activerecord-obstacle-course")
+      expect(repos[1][:name]).to eq("apollo_14")
+      expect(repos[2][:name]).to eq("backend_prework")
+      expect(repos[3][:name]).to eq("battleship")
+      expect(repos[4][:name]).to eq("books")
     end
   end
 end
