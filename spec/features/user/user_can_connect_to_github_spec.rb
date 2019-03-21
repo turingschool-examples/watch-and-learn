957770aaed117ef0b2c609eeb9d2324acafeab3c
Chris Lewis <13chlewis@gmail.com>
2019-03-21 17:01:51 -0600

    All tests passing, moved test authorization token to application.yml and removed unnecessary data from OAuth mock in rails helper
    Co-authored-by: Manoj Panta <34468256+manojpanta@users.noreply.github.com>

--- spec/features/user/user_can_connect_to_github_spec.rb
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe 'A registered user without a github token' do
   context 'visiting /dashboard' do
     it 'can connect their account to github' do
-      WebMock.disable!
       user = create(:user)
+      login_as(user)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit dashboard_path
       expect(page).to_not have_content("Followers")
       expect(page).to_not have_content("Following")
+
+      mock_user_dashboard_github
 
       click_on 'Connect To Github'
 
