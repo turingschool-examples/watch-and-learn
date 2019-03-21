4fd41e7b7b0978a8b867214ea2edaf56c2606f5a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-20 22:17:53 -0600

    Adds get_followers method to GithubService utilizing /followers uri.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -7,6 +7,10 @@ class GithubService
     get_json('repos')
   end
 
+  def get_followers
+    get_json('followers')
+  end
+
   def get_json(uri)
     response = conn.get(uri)
     JSON.parse(response.body, symbolize_names: true)
