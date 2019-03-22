02e25d4400e52898a5faff38b07d3b44a6e272b8
plapicola <p.lapicola@gmail.com>
2019-03-21 21:35:40 -0600

    Adds validation tests to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,17 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
     it {should validate_presence_of(:password)}
+    it {should validate_length_of(:first_name).
+        is_at_least(1)
+    }
+    it {should validate_length_of(:last_name).
+        is_at_least(1)
+    }
+    it {should validate_length_of(:email).
+        is_at_least(1)
+    }
   end
 
   describe 'roles' do
