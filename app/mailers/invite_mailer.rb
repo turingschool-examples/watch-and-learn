7178378c0d91dc76d2155a61efb29e0099e9f20e
Ty Mazey <tymazey@gmail.com>
2019-03-23 12:53:13 -0600

    adds test to InviteFacade for known methods
    Co-authored-by: Peter Lapicola <p.lapicola@gmail.com>

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,7 +2,7 @@ class InviteMailer < ApplicationMailer
 
   def invite(user, recipient)
     recipient = GithubService.new.get_user(recipient)
-    @invite = InviteFacade.new(user, recipient
+    @invite = InviteFacade.new(user, recipient)
     mail(to: @invite.to_address, subject: "Someone's Invited You!").send_now
   end
 end
