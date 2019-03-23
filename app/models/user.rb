eec52f94c3d198172e4c4a5b2cbe5104a55549f3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-23 12:16:51 -0600

    Adds connect to github method on user. Co-authored-by: April Dagonese <41272635+aprildagonese@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
+
+  def connect_github(data)
+    self.update!(github_token: data.credentials.token)
+  end
 end
