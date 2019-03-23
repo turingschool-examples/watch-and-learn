a1f9308ed30cba8faacd92f1245d1885d3e4b45d
Ty Mazey <tymazey@gmail.com>
2019-03-23 12:40:31 -0600

    fleshes out InviteMailer controller
    Co-authored-by: Peter Lapicola <p.lapicola@gmail.com>

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,6 +1,8 @@
 class InviteMailer < ApplicationMailer
 
   def invite(user, recipient)
-    email = GithubService.new.get_email(recipient)
+    recipient = GithubService.new.get_user(recipient)
+    @invite = InviteFacade.new(user, recipient
+    mail(to: @invite.to_address, subject: "Someone's Invited You!").send_now
   end
 end
