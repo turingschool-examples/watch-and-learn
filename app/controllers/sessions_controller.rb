b7ea44ae0ffafa2b24ee7869b39395702047f129
Chris Lewis <13chlewis@gmail.com>
2019-03-23 12:43:42 -0600

    Tested and implemented functionality that doesn't allow users to connect multiple accounts to the same GitHub account
    Co-authored-by: Manoj Panta <34468256+manojpanta@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     if current_user && !current_user.github_token
-      current_user.update(github_token: request.env['omniauth.auth']['credentials']['token'])
       current_user.update(uid: request.env['omniauth.auth']['uid'])
-      redirect_to dashboard_path
+      if User.find(current_user.id).uid
+        current_user.update(github_token: request.env['omniauth.auth']['credentials']['token'])
+        redirect_to dashboard_path
+      else
+        flash[:error] = "This GitHub account is already connected to another user's profile"
+        redirect_to dashboard_path
+      end
     else
       user = User.find_by(email: params[:session][:email])
       if user && user.authenticate(params[:session][:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, if: :password
   validates_presence_of :first_name
+  validates_uniqueness_of :uid, :allow_nil => true
   enum role: [:default, :admin]
   has_many :friendships
   has_many :friends, through: :friendships
   has_secure_password
-
 end
