52aa3745d95d085a290608b3d4f1be6d44cdbef7
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-24 15:57:31 -0600

    Adds test for a user adding friends. Co-authored-by: April Dagonese <41272635+aprildagonese@users.noreply.github.com>

--- spec/features/user/user_can_add_friends.rb
+++ spec/features/user/user_can_add_friends.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "As a registered user connected to Github" do
   before :each do
-    @april = create(:user, email: "test@email.com", password: "test", github_token: ENV['GITHUB_API_KEY'], github_uid: "41272635")
-    @mackenzie = create(:user, email: "mackenzie@email.com", password: "test", github_token: ENV['MF_GITHUB_TOKEN'], github_uid: "42525195")
-    @zach = create(:user, email: "zach@email.com", password: "test", github_token: "faketoken", github_uid: "34927114")
+    @april = create(:user, email: "test@email.com", password: "test", github_token: ENV['GITHUB_API_KEY'], github_uid: "41272635", github_handle: 'aprildagonese', github_url: 'https://github.com/aprildagonese')
+    @mackenzie = create(:user, email: "mackenzie@email.com", password: "test", github_token: ENV['MF_GITHUB_TOKEN'], github_uid: "42525195", github_handle: 'Mackenzie-Frey', github_url: 'https://github.com/Mackenzie-Frey')
+    @zach = create(:user, email: "zach@email.com", password: "test", github_token: "faketoken", github_uid: "34927114", github_handle: 'nagerz', github_url: 'https://github.com/nagerz')
 
     repos_json_response = File.open('fixtures/user_repos.rb')
     stub_request(:get, "https://api.github.com/user/repos").to_return(status: 200, body: repos_json_response)
@@ -66,9 +66,28 @@ describe "As a registered user connected to Github" do
       expect(page).to_not have_content("My Friends")
       expect(page).to_not have_css(".friend")
       expect(page).to_not have_css(".friend_handle_nagerz")
-      expect(page).to_not have_link("nagerz")
 
-      within "following_handle_nagerz" do
+      within ".following_handle_nagerz" do
+        click_button("Add Friend")
+      end
+
+      within ".my_friends" do
+        expect(page).to have_content("My Friends")
+        expect(page).to have_css(".friend")
+        expect(page).to have_css(".friend_handle_nagerz")
+        expect(page).to have_link("nagerz")
+      end
+    end
+
+    it "I see my friends under My Friends" do
+      visit dashboard_path
+
+      expect(page).to_not have_css(".my_friends")
+      expect(page).to_not have_content("My Friends")
+      expect(page).to_not have_css(".friend")
+      expect(page).to_not have_css(".friend_handle_nagerz")
+
+      within ".following_handle_nagerz" do
         click_button("Add Friend")
       end
 
