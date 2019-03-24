1dae6b6a9db539795885a45806c7543d1d594536
plapicola <p.lapicola@gmail.com>
2019-03-24 12:54:55 -0600

    Adds testing for uid return from github users
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -9,9 +9,10 @@ describe GithubUser do
   end
 
   it 'has attributes' do
-    follower = GithubUser.new({login: 'login', html_url: 'url'})
+    follower = GithubUser.new({login: 'login', html_url: 'url', uid: 12})
 
     expect(follower.handle).to eq('login')
     expect(follower.url).to eq('url')
+    expect(follower.uid).to eq(12)
   end
 end
