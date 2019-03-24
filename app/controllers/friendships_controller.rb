3b46a61f47747f235979bd07bef13d8f701a7ed5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-24 15:59:04 -0600

    Adds Friendships Controller and create method for add_friend_path. Co-authored-by: April Dagonese <41272635+aprildagonese@users.noreply.github.com>

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,7 @@
+class FriendshipsController < ApplicationController
+  def create
+    friend = User.find_by(github_uid: params['friend_github_uid'])
+    Friendship.create!(user_id: current_user.id, friend_user_id: friend.id)
+    redirect_to dashboard_path
+  end
+end
