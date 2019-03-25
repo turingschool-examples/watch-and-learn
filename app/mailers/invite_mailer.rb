3a4389ed492809fcbba0bac274ef82798cfe6903
Teresa M Knowles <teresa07moreno@gmail.com>
2019-03-25 17:35:20 -0600

    Delete invite_mailer.rb

--- app/mailers/invite_mailer.rb
+++
@@ -1,9 +0,0 @@
-class InviteMailer < ApplicationMailer
-
-  def invite(user, github_user)
-    @user = user
-    mail(to: github_user, subject: "#{user.name} wants to invite you to Brownfield of Dreams.")
-  end
-
-  
-end
