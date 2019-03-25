240a30eb6e865df83ca7416789bd3979bb806951
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-25 17:28:06 -0600

    adds custom validation that tests if a user and friend uid are the same

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+  validate :friend_themselves_check
+
+  private
+
+  def friend_themselves_check
+    if user && friend && user.uid == friend.uid
+      errors.add(:friendship, "You cannot add yourself as a friend.")
+    end
+  end
 end
