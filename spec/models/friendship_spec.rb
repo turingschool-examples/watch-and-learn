a1c273ff0030c687d1ff06e1c060f8c108c0b956
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-25 17:27:41 -0600

    adds spec test that makes sure a friendship cannot be created with the same user as the user and the friend

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Friendship, type: :model do
     it {should belong_to(:user)}
     it {should belong_to(:friend)}
   end
+
+  describe 'instance methods' do
+    it 'friend_themselves_check' do
+      @user1 = create(:user, uid: '12')
+      create(:github_token, user: @user1, token: ENV['USER_1_GITHUB_TOKEN'])
+
+      @user2 = create(:user, uid: '34')
+
+      friendship = Friendship.new(user: @user1, friend:@user1)
+
+      expect(friendship.save).to eq(false)
+      expect(friendship.errors.messages[:friendship].first).to eq("You cannot add yourself as a friend.")
+
+    end
+  end
 end
