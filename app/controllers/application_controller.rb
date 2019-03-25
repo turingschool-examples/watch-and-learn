a7fde7744bd29d3d5827fdccf7679d303e9da890
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-25 16:56:56 -0600

    Adds user to seed to enable Mackenzie to have a ability to see Add Friend button. Comments out protect from forgery. Getting error due to loosing current user in connect_github method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :null_session
   helper_method :current_user
   helper_method :find_bookmark
   helper_method :list_tags
