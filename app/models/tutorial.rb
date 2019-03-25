2c945404912ff6e08c0691b396783784326a35fb
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-25 01:01:49 -0600

    tutorial model spec

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,5 +1,8 @@
 class Tutorial < ApplicationRecord
   validates :title, presence: true, length: {minimum: 1}
+  validates :description, presence: true, length: {minimum: 1}
+  validates :thumbnail, presence: true, length: {minimum: 1}
+  validates_presence_of :classroom
   has_many :videos, ->  { order(position: :ASC) }, dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
