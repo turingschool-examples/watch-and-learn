c94cf6fe96c3035767bc63c5b64219607f44e574
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-26 14:37:40 -0600

    invite working on development mode, tested with mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
+  # config.action_mailer.default_url_options = {host: "localhost", port: 3000, protocol: 'http'}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
