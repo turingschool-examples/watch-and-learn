a0a0805a5ff873e7cf4ca17735007773bcefc024
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-26 13:53:22 -0600

    changes invitation mailer github service

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -1,13 +1,13 @@
 class InvitationController < ApplicationController
 
   def new
-    
+
   end
 
   def create
     github_service = GithubService.new(current_user)
-    email = github_service.get_email(params[:handle])["email"]
-
+    email = github_service.get_email(params[:handle])[:email]
+    binding.pry
     if InviteMailer.invite(current_user, params[:email]).deliver_now
       flash[:notice] = "Successfully sent invite!"
     else
