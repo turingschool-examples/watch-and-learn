2b84a3578d6525c778d4f67153cafbf6e1ea7833
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-26 13:33:03 -0600

    adds flash messages

---
+++ app/controllers/invitation_controller.rb
@@ -0,0 +1,14 @@
+class InvitationController < ApplicationController
+
+  def create
+    github_service = GithubService.new(current_user)
+    email = github_service.get_email(params[:handle])["email"]
+
+    if InviteMailer.invite(current_user, params[:email]).deliver_now
+      flash[:notice] = "Successfully sent invite!"
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+    end
+    redirect_to dashboard_path
+  end
+end
