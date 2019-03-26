84f5be2244dfed2d6237a007f11b9df3597457f6
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-26 14:41:23 -0600

    Conditional for github_user.email. If there's no email, it flashes an error

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -8,8 +8,9 @@ class InvitationController < ApplicationController
     github_service = GithubService.new(current_user)
     user = github_service.get_user(params[:handle])
     github_user = GithubUser.new(user)
-
-    if InviteMailer.invite(current_user, github_user).deliver_now
+    
+    if github_user.email
+      InviteMailer.invite(current_user, github_user).deliver_now
       flash[:notice] = "Successfully sent invite!"
     else
       flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
