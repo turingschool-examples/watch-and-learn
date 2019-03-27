1cef49407274d15a02ed9073bfb926452828e57a
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-27 14:56:01 -0600

    Delete commented out development env config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,6 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = { :address => "http://localhost:3000", :port => 1025 }
 
-  # config.action_mailer.default_url_options = {host: "localhost", port: 3000, protocol: 'http'}
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
