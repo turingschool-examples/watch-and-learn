6ecc455a0f16a47eaba9d65caf4fe35b0a753d06
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-27 14:54:49 -0600

    Got rid of verification of current_user.github_token in create action of Invitation Controller per PR feedback

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -8,7 +8,7 @@ class InvitationController < ApplicationController
     user = GithubService.new(current_user).get_user(params[:handle])
     github_user = GithubUser.new(user)
 
-    if github_user.email && current_user.github_token
+    if github_user.email 
       InviteMailer.invite(current_user, github_user).deliver_now
       flash[:notice] = "Successfully sent invite!"
     else
