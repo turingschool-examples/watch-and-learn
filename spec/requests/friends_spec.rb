f79209f347f9936b375b926d7dcb9e6f1e7ef870
April Dagonese <adagonese@gmail.com>
2019-03-27 15:24:56 -0600

    Adds sad path test for Add Friend

--- spec/requests/friends_spec.rb
+++ spec/requests/friends_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 describe 'Add Friend Call' do
-  xit 'A logged in user cannot post with an invalid friend ID' do
+  it 'A logged in user cannot post with an INVALID friend ID' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    post "/friendships", {:params => {:friend_user_id => 'title'}}
+    post "/friendships", {:params => {:friend_user_id => '123456'}}
 
     expect(response).to_not be_successful
   end
+
+  it 'A logged in user cannot post with a VALID friend ID' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    post "/friendships", {:params => {:friend_user_id => '42525195'}}
+
+    expect(response).to_not be_successful
+  end
+
+
 end
