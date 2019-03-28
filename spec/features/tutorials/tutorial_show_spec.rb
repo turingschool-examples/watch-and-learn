76962d36f8d47804c937e11933e09b758b4d64cf
April Dagonese <adagonese@gmail.com>
2019-03-28 08:06:19 -0600

    Adds logic to display correctly when tutorial has no videos

--- spec/features/tutorials/tutorial_show_spec.rb
+++ spec/features/tutorials/tutorial_show_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Tutorial show page" do
   it "displays all videos" do
+    tut1 = create(:tutorial)
+    vid1, vid2 = create_list(:video, 2, tutorial: tut1)
+
+    visit tutorial_path(tut1)
+
+    expect(current_path).to eq(tutorial_path(tut1))
+    expect(page).to have_content(tut1.title)
+    expect(page).to have_content(vid1.title)
+    expect(page).to have_content(vid2.title)
   end
 
   it "displays correctly when it has no videos" do
@@ -12,9 +21,6 @@ RSpec.describe "Tutorial show page" do
     visit tutorial_path(tut2)
 
     expect(current_path).to eq(tutorial_path(tut2))
-    expect(page).to have_content(tut1.title)
-    expect(page).to have_content(vid1.title)
-    expect(page).to have_content(vid2.title)
-    expect(page).to have_content(tut2.title)
+    expect(page).to have_content("This tutorial doesn't have any videos yet, but we're working on it!")
   end
 end
