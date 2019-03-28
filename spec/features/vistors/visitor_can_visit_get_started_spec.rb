11ff2a845700b9a5b5d78404886899072c9b8d23
Chris Lewis <13chlewis@gmail.com>
2019-03-28 12:03:00 -0600

    Created test for About page

--- spec/features/vistors/visitor_can_visit_get_started_spec.rb
+++ spec/features/vistors/visitor_can_visit_get_started_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'When a visitor visits Get Started' do
-  it 'they see information about the site' do
+  it 'they see information on how to use' do
     visit '/get_started'
 
     expect(page).to have_content('Get Started')
