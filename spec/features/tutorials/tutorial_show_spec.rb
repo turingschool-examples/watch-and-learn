f721137af116143faf963f8b00a2ebcada7a8200
April Dagonese <adagonese@gmail.com>
2019-03-28 07:38:50 -0600

    Adds test for showing vidoes

---
+++ spec/features/tutorials/tutorial_show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Tutorial show page" do
+  it "displays all videos" do
+  end
+
+  it "displays correctly when it has no videos" do
+    tut1 = create(:tutorial)
+    vid1, vid2 = create_list(:video, 2, tutorial: tut1)
+    tut2 = create(:tutorial)
+
+    visit tutorial_path(tut2)
+
+    expect(current_path).to eq(tutorial_path(tut2))
+    expect(page).to have_content(tut1.title)
+    expect(page).to have_content(vid1.title)
+    expect(page).to have_content(vid2.title)
+    expect(page).to have_content(tut2.title)
+  end
+end
