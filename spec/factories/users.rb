6282e22eed4105010febab9ce63d27eb0fe4ecee
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-06 17:48:43 -0600

    Adds token to users table creates initial test to show Repositories on dashboard updates git ignore
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     last_name { Faker::Artist.name }
     password { Faker::Color.color_name }
     role { :default }
+    token {"8e08129aa359391e0f54b1192be5a9c3bb819e09"}
   end
 
   factory :admin, parent: :user do
