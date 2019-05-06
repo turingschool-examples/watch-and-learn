bb079470e6b981298318786952102c74f289ff03
Ethan Grab <ethangrab@gmail.com>
2019-05-06 16:44:31 -0600

    Ran rubocop -a to autocorrect issues thrown by rubocop
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>

--- app/services/tutorial_sequencer.rb
+++ app/services/tutorial_sequencer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TutorialSequencer
   def initialize(tutorial, sequenced_video_ids)
     @tutorial = tutorial
@@ -9,6 +11,7 @@ class TutorialSequencer
   end
 
   private
+
   attr_reader :tutorial, :sequenced_video_ids
 
   def videos
@@ -21,9 +24,7 @@ class TutorialSequencer
         video.id == video_id.to_i
       end
 
-      if video.position != index
-        video.update(position: index)
-      end
+      video.update(position: index) if video.position != index
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :tutorials, only:[:show, :index]
-      resources :videos, only:[:show]
+      resources :tutorials, only: %i[show index]
+      resources :videos, only: [:show]
     end
   end
 
@@ -11,22 +13,22 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
 
   namespace :admin do
-    get "/dashboard", to: "dashboard#show"
-    resources :tutorials, only: [:create, :edit, :update, :destroy, :new] do
+    get '/dashboard', to: 'dashboard#show'
+    resources :tutorials, only: %i[create edit update destroy new] do
       resources :videos, only: [:create]
     end
-    resources :videos, only: [:edit, :update, :destroy]
+    resources :videos, only: %i[edit update destroy]
 
     namespace :api do
       namespace :v1 do
-        put "tutorial_sequencer/:tutorial_id", to: "tutorial_sequencer#update"
+        put 'tutorial_sequencer/:tutorial_id', to: 'tutorial_sequencer#update'
       end
     end
   end
 
-  get '/login', to: "sessions#new"
-  post '/login', to: "sessions#create"
-  delete '/logout', to: "sessions#destroy"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'users#show'
   get '/about', to: 'about#show'
@@ -35,11 +37,11 @@ Rails.application.routes.draw do
   # Is this being used?
   get '/video', to: 'video#show'
 
-  resources :users, only: [:new, :create, :update, :edit]
+  resources :users, only: %i[new create update edit]
 
-  resources :tutorials, only: [:show, :index] do
-    resources :videos, only: [:show, :index]
+  resources :tutorials, only: %i[show index] do
+    resources :videos, only: %i[show index]
   end
 
-  resources :user_videos, only:[:create, :destroy]
+  resources :user_videos, only: %i[create destroy]
 end
