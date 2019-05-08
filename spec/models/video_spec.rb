9b241626fb3589244577557899d8bd04851a6177
tnodland <42710030+tnodland@users.noreply.github.com>
2019-05-08 08:33:09 -0600

    Update video_spec.rb
    Added a few space to appease hound ci

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Video, type: :model do
   describe 'relationships' do
     it { should have_many :user_videos }
     it { should have_many(:users).through :user_videos }
-    it { should belong_to :tutorial}
+    it { should belong_to :tutorial }
   end
 
   describe 'validations' do
-    it {should validate_numericality_of :position }
+    it { should validate_numericality_of :position }
   end
 end
