00837a49fadca43adce4e0f9b64d4dba09a88c72
Ethan Grab <ethangrab@gmail.com>
2019-05-08 15:03:14 -0600

    API method calls for followers and following
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -2,17 +2,32 @@ require 'rails_helper'
 
 describe GithubService do
   context "instance methods" do
-    it "gets github information" do
+    it "gets repository information" do
       VCR.use_cassette("services/find_repositories") do
-        user = create(:user, github_token: ENV["GITHUB_API_KEY"])
-        service = GithubService.new(user)
+        token = ENV["GITHUB_API_KEY"]
+        service = GithubService.new(token)
 
-        result = service.github_info
+        result = service.repository_info
         repository = result.first
 
         expect(repository).to be_a(Hash)
         expect(repository[:name]).to eq("Bookclub")
       end
     end
+
+    it "gets follower information" do
+      VCR.use_cassette("services/find_followers") do
+        token = ENV["GITHUB_API_KEY"]
+        service = GithubService.new(token)
+
+        result = service.follower_info
+        follower = result.first
+
+        expect(follower).to be_a(Hash)
+        expect(follower[:login]).to eq("chitasan")
+      end
+    end
+
+
   end
 end
