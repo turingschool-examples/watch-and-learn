05111e5d53f5b84861b04b7c262495c79da51044
Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>
2019-05-08 08:38:38 -0600

    Update get_started_spec.rb
    Makes adjustments to appease the hounds

--- spec/features/get_started_spec.rb
+++ spec/features/get_started_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Get started page" do
-  context "as any type of user" do
-    it "can see links and information on the get started page" do
+RSpec.describe 'Get started page' do
+  context 'as any type of user' do
+    it 'can see links and information on the get started page' do
       visit get_started_path
 
-      expect(page).to have_content("Register to bookmark segments")
-      expect(page).to have_link("homepage")
+      expect(page).to have_content('Register to bookmark segments')
+      expect(page).to have_link('homepage')
 
-      within ".started-main" do
-        expect(page).to have_link("Sign in")
+      within '.started-main' do
+        expect(page).to have_link('Sign in')
       end
     end
   end
