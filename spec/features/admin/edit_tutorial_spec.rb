08945905be32a33e1b4b497e3874d9afb1a8df1d
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-09 16:14:31 -0600

    Edited the tests

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -14,7 +14,7 @@ describe 'An Admin can edit a tutorial' do
     click_on 'Add Video'
 
     fill_in 'video[title]', with: 'How to tie your shoes.'
-    fill_in 'video[description]', 
+    fill_in 'video[description]',
             with: 'Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through.'
     fill_in 'video[video_id]', with: 'J7ikFUlkP_k'
     click_on 'Create Video'
--- spec/features/user/user_sees_github_repos_spec.rb
+++ spec/features/user/user_sees_github_repos_spec.rb
@@ -12,12 +12,14 @@ require 'rails_helper'
 describe 'when logged in user visits root path' do
   it 'he can see github repos' do
     user = User.create!(first_name: 'Earl', last_name: 'Stephens',
-       email: 'sethreader@hotmail.com', password: 'password',
-       username: 'earl-stephens',
-       github_token: ENV['token'])
+                        email: 'sethreader@hotmail.com',
+                        password: 'password',
+                        username: 'earl-stephens',
+                        github_token: ENV['token'])
 
     allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
+      .to receive(:current_user)
+      .and_return(user)
 
     visit dashboard_path
 
@@ -28,12 +30,14 @@ describe 'when logged in user visits root path' do
 
   it 'tests for different user' do
     user = User.create!(first_name: 'Deonte', last_name: 'Cooper',
-      email: '45864171+djc00p@users.noreply.github.com',
-      password: 'password', username: 'djc00p',
-      github_token: ENV['Deonte_token'])
+                        email: '45864171+djc00p@users.noreply.github.com',
+                        password: 'password',
+                        username: 'djc00p',
+                        github_token: ENV['Deonte_token'])
 
     allow_any_instance_of(ApplicationController)
-    .to receive(:current_user).and_return(user)
+      .to receive(:current_user)
+      .and_return(user)
 
     visit dashboard_path
     # save_and_open_page
@@ -43,17 +47,20 @@ describe 'when logged in user visits root path' do
   end
 
   it 'he cannot see github repos if he doesnt have a token' do
-    user = User.create!(first_name: 'Earl', last_name: 'Stephens',
-      email: 'sethreader@hotmail.com', password: 'password',
-      username: 'earl-stephens')
+    user = User.create!(first_name: 'Earl',
+                        last_name: 'Stephens',
+                        email: 'sethreader@hotmail.com',
+                        password: 'password',
+                        username: 'earl-stephens')
 
     allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
+      .to receive(:current_user)
+      .and_return(user)
 
     visit dashboard_path
 
     within '#github-section' do
-      expect(page).to_not have_content('github.com')
+      expect(page).to_not have_content('Github')
     end
   end
 end
