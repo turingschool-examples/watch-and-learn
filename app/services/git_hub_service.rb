b78919c6a3b1cc7a4696a793ccea2ce395430634
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-09 16:37:08 -0600

    Fixed some more houndci errors

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -9,24 +9,24 @@ class GitHubService
   end
 
   def repos
-    get_json("repos")
+    get_json('repos')
   end
 
   def followers
-    get_json("followers")
+    get_json('followers')
   end
 
   private
 
-    def get_json(url)
-      response = conn.get(url)
-      JSON.parse(response.body, symbolize_names: true)
-    end
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-    def conn
-      Faraday.new("https://api.github.com/user/") do |f|
-        f.params["access_token"] = @github_token
-        f.adapter Faraday.default_adapter
-      end
+  def conn
+    Faraday.new('https://api.github.com/user/') do |f|
+      f.params['access_token'] = @github_token
+      f.adapter Faraday.default_adapter
     end
+  end
 end
