91d3f4b01e949d3817b063776a6cc29924856bec
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-08 22:32:11 -0600

    Moves webmock to before :each block

--- spec/features/user/user_can_see_their_git_repos_spec.rb
+++ spec/features/user/user_can_see_their_git_repos_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 
 describe 'as a logged in user on my dashboard' do
-  it 'sees a section for github and their collection of repos' do
+  before :each do
     json_response = File.open("./fixtures/user_repos.json")
     stub_request(:get, "https://api.github.com/user/repos").
       to_return(status: 200, body: json_response)
+  end
 
+  it 'sees a section for github and their collection of repos' do
     user = create(:user, git_key: "bananas")
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
 
     expect(page).to have_css('.github')
-
     expect(page).to have_css('.repository', count: 5)
 
     within first ".repository" do
-      expect(page).to have_css('.link')
+      expect(page).to have_css('.repo-link')
     end
   end
 
   it 'does not see a section for github, or any repos if missing github token' do
-    json_response = File.open("./fixtures/user_repos.json")
-    stub_request(:get, "https://api.github.com/user/repos").
-      to_return(status: 200, body: json_response)
-
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
