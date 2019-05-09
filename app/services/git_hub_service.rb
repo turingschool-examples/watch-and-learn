cf447c4aa3068b19be3cb5a81a0f7ed11564a1a3
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-09 16:06:16 -0600

    Added service file for github

---
+++ app/services/git_hub_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Service PORO for github
+
+class GitHubService
+  def initialize(user_name, github_token)
+    @user_name = user_name
+    @github_token = github_token
+  end
+
+  def repos
+    get_json("repos")
+  end
+
+  def followers
+    get_json("followers")
+  end
+
+  private
+
+    def get_json(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def conn
+      Faraday.new("https://api.github.com/user/") do |f|
+        f.params["access_token"] = @github_token
+        f.adapter Faraday.default_adapter
+      end
+    end
+end
