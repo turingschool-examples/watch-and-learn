fe610173a29d407064106f407ef04f242958da14
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-08 21:59:56 -0600

    Adds initial test for user seeing followers on their profile

---
+++ spec/features/user/user_can_see_followers_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'as a logged in user' do
+  before :each do
+  json_response = File.open("./fixtures/user_followers.json")
+  stub_request(:get, "https://api.github.com/user/followers").
+    to_return(status: 200, body: json_response)
+  end
+
+  context 'when I visit my dashboard - in the Github section' do
+    it 'shows a followers section, with my followers handles which link to their profiles' do
+      user = create(:user, git_key: "bananas")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      expect(page).to have_css('.github')
+      expect(page).to have_css('.followers')
+
+      within first('.followers') do
+        expect(page).to have_css('.profile-link')
+      end
+    end
+  end
+
+end
