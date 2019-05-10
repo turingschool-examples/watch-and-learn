9b165f01d74424604c4c2eb436f33db4d5c5800a
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-10 08:39:26 -0600

    Adds new tutorial spec and admin/tutorial#create method
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -4,6 +4,8 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   def create
+    Tutorial.create(tutorial_params)
+    redirect_to admin_dashboard_path
   end
 
   def new
@@ -12,14 +14,18 @@ class Admin::TutorialsController < Admin::BaseController
 
   def update
     tutorial = Tutorial.find(params[:id])
-    if tutorial.update(tutorial_params)
+    if tutorial.update(tutorial_tag_params)
       flash[:success] = "#{tutorial.title} tagged!"
     end
     redirect_to edit_admin_tutorial_path(tutorial)
   end
 
   private
-  def tutorial_params
+  def tutorial_tag_params
     params.require(:tutorial).permit(:tag_list)
+  end
+
+  def tutorial_params
+    params.require(:tutorial).permit(:title,:description,:thumbnail)
   end
 end
