db6afa941878ee08b41ef9d1dfd1ecbc7b646070
earl-stephens <stephensearl@hotmail.com>
2019-05-11 11:36:12 -0600

    added tests for admin adding a tutorial

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -11,14 +11,13 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.new
   end
 
-  # def create
-  #   @tutorial = Tutorial.new(tutorial_params)
-  #   binding.pry
-  #   if @tutorial.save
-  #     flash[:success] = "#{tutorial.title} created!"
-  #     redirect_to edit_admin_tutorial_path(@tutorial)
-  #   end
-  # end
+  def create
+    @tutorial = Tutorial.new(tutorial_params)
+    if @tutorial.save
+      flash[:success] = "#{@tutorial.title} created!"
+      redirect_to edit_admin_tutorial_path(@tutorial)
+    end
+  end
 
   def update
     tutorial = Tutorial.find(params[:id])
---
+++ spec/features/admin/can_add_video_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe 'an admin' do
+  it 'can add a video' do
+    admin = create(:admin)
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(admin)
+
+    visit new_admin_tutorial_path
+
+    fill_in "Title", with: "Bugs Bunny"
+    fill_in "Description", with: "Saturday morning cartoons"
+    fill_in "Thumbnail", with: 'https://youtu.be/uYBce9Gsz7g'
+
+    click_on 'Save'
+
+    tutorial = Tutorial.last
+    expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+  end
+end
