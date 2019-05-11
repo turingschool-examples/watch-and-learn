318d47e6b578800e8f2ee882ea2bb916b7939e67
Earl Stephens <stephensearl@hotmail.com>
2019-05-11 15:40:36 -0600

    Update user_can_use_omniauth_spec.rb
    Removed extra blank space and aligned some elements to fix HoundCI errors.

--- spec/features/user/user_can_use_omniauth_spec.rb
+++ spec/features/user/user_can_use_omniauth_spec.rb
@@ -5,16 +5,15 @@ describe 'User logs in through github omniauth' do
   it 'shows github info on dashboard' do
     VCR.use_cassette('cassettes/can_see_github_info4') do
       user = User.create!(first_name: 'Deonte',
-        last_name: 'Cooper',
-        email: '45864171+djc00p@users.noreply.github.com',
-        password: 'password')
+                          last_name: 'Cooper',
+                          email: '45864171+djc00p@users.noreply.github.com',
+                          password: 'password')
 
       allow_any_instance_of(ApplicationController)
         .to receive(:current_user)
         .and_return(user)
 
       visit dashboard_path
-
 
       OmniAuth.config.test_mode = true
 
