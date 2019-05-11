d5aed2c15eb868ad72d7af5929f52c7c8ebafa04
earl-stephens <stephensearl@hotmail.com>
2019-05-11 11:17:40 -0600

    added tests for get started and about links

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -24,5 +24,42 @@ describe 'Visitor' do
         expect(page).to have_content(tutorial1.description)
       end
     end
+
+    it 'shows an about page' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      click_on 'About'
+
+      expect(current_path).to eq(about_path)
+      expect(page).to have_content("This application is designed to pull in youtube information")
+    end
+
+    it 'shows a get started page' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      click_on 'Get Started'
+
+      expect(current_path).to eq(get_started_path)
+      within '.started-main' do
+        expect(page).to have_content('Browse tutorials from')
+        expect(page).to have_link('Sign in')
+      end
+    end
   end
 end
