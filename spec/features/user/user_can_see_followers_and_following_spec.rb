75b57950bc1543b502e9aea4ed85c5121102c4c2
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-11 12:56:19 -0600

    builds test for card 14, user can see users they follow
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- spec/features/user/user_can_see_followers_and_following_spec.rb
+++ spec/features/user/user_can_see_followers_and_following_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "as a logged in user" do
-  context "when i visit the user dashboard" do
+  context "when i visit the user dashboard", :vcr do
+    before :each do
+      @user = create(:user)
+    end
+    
     it "shows a list of users i follow" do
-      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit dashboard_path
+
+      within '.following' do
+        expect(page).to have_content('People you Follow')
+        expect(page).to have_link('plapicola')
+        expect(page).to have_link('tnodland')
+      end
     end
   end
 end
