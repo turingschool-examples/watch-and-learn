83daedd2c8de53c3bac3ab65abcf84af7a80fcca
Earl Stephens <stephensearl@hotmail.com>
2019-05-11 16:17:03 -0600

    Update schema.rb
    Changed the line length on lines 26, 29 and 34 due to HoundCI errors

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,16 @@ ActiveRecord::Schema.define(version: 2019_05_08_163444) do
     t.string 'context', limit: 128
     t.datetime 'created_at'
     t.index ['context'], name: 'index_taggings_on_context'
-    t.index ['tag_id', 'taggable_id', 'taggable_type', 'context', 'tagger_id', 'tagger_type'], name: 'taggings_idx', unique: true
+    t.index %w[tag_id taggable_id taggable_type context tagger_id tagger_type], 
+            name: 'taggings_idx', unique: true
     t.index ['tag_id'], name: 'index_taggings_on_tag_id'
-    t.index %w[taggable_id taggable_type context], name: 'index_taggings_on_taggable_id_and_taggable_type_and_context'
+    t.index %w[taggable_id taggable_type context], 
+            name: 'index_taggings_on_taggable_id_and_taggable_type_and_context'
     t.index %w[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
     t.index ['taggable_id'], name: 'index_taggings_on_taggable_id'
     t.index ['taggable_type'], name: 'index_taggings_on_taggable_type'
-    t.index ['tagger_id', 'tagger_type'], name: 'index_taggings_on_tagger_id_and_tagger_type'
+    t.index ['tagger_id', 'tagger_type'], 
+            name: 'index_taggings_on_tagger_id_and_tagger_type'
     t.index ['tagger_id'], name: 'index_taggings_on_tagger_id'
   end
 
