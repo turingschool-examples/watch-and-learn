f10c7f950fc6e171be0a8fe0df51b61d080b6229
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-13 14:38:25 -0600

    Specifies which css classes in test
    Co-authored-by: Erin King <44042415+erin-king@users.noreply.github.com>
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_through_github_connect_spec.rb
+++ spec/features/user/user_can_add_friends_through_github_connect_spec.rb
@@ -10,14 +10,28 @@ describe 'as a logged in user who has connected with github' do
    visit dashboard_path
    expect(page).to_not have_content("Your Friends")
 
-   within '#jtaylor522' do
-     expect(page).to have_button("Add as Friend")
-     click_button("Add as Friend")
-     expect(current_path).to eq(dashboard_path)
+   within ".github-followers" do
+     within '#jtaylor522' do
+       expect(page).to have_button("Add as Friend")
+       click_button("Add as Friend")
+       expect(current_path).to eq(dashboard_path)
+     end
+   end
+
+   within ".github-followers" do
+     within first '#jtaylor522' do
+       expect(page).to_not have_button("Add as Friend")
+     end
+   end
+
+   within ".github-followings" do
+     save_and_open_page
+     within '#jtaylor522' do
+       expect(page).to_not have_button("Add as Friend")
+     end
    end
 
    within '.friends' do
-
      expect(page).to have_content("Your Friends")
      expect(page).to have_content("jtaylor522")
    end
