50cf8ec60ce5f8db48abdf29ba78ed118fa0c8ad
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-13 14:02:59 -0600

    Adjusts strings and spacing to appease the hounds
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- spec/features/user/user_can_invite_people_to_the_site_spec.rb
+++ spec/features/user/user_can_invite_people_to_the_site_spec.rb
@@ -3,28 +3,21 @@ require 'rails_helper'
 RSpec.describe 'As a user', type: :feature do
   describe 'when I visit my dashboard', :vcr do
     it 'allows me to invite people to the website' do
-      # As a registered user
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      # When I visit /dashboard
       visit dashboard_path
-      # And I click "Send an Invite"
 
-      click_link "Send an Invite"
+      click_link 'Send an Invite'
 
-      # Then I should be on /invite
       expect(current_path).to eq(invite_path)
       expect(page).to have_content("Enter Your Friend's GitHub Usename and we'll invite them to join!")
 
-      # And when I fill in "Github Handle" with <A VALID GITHUB HANDLE>
-      fill_in :github_handle, with: "test-freind"
-      # And I click on "Send Invite"
-      click_button "Send Invite"
-      # Then I should be on /dashboard
+      fill_in :github_handle, with: 'test-freind'
+      click_button 'Send Invite'
+
       expect(current_path).to eq(dashboard_path)
-      # And I should see a message that says "Successfully sent invite!" (if the user has an email address associated with their github account)
-      expect(page).to have_content("Successfully sent invite!")
+      expect(page).to have_content('Successfully sent invite!')
     end
 
     it 'will show an error if there is no email attached to their github account' do
@@ -36,8 +29,8 @@ RSpec.describe 'As a user', type: :feature do
 
       expect(current_path).to eq(invite_path)
 
-      fill_in :github_handle , with: "tnodland"
-      click_button "Send Invite"
+      fill_in :github_handle , with: 'tnodland'
+      click_button 'Send Invite'
       failure = "The Github user you selected doesn't have an email address associated with their account."
 
       expect(current_path).to eq(dashboard_path)
@@ -53,8 +46,8 @@ RSpec.describe 'As a user', type: :feature do
 
       expect(current_path).to eq(invite_path)
 
-      fill_in :github_handle , with: "lbivlodfbnvolidarbvlhdrsbvlksdanv"
-      click_button "Send Invite"
+      fill_in :github_handle , with: 'lbivlodfbnvolidarbvlhdrsbvlksdanv'
+      click_button 'Send Invite'
       failure = "The Github user you selected doesn't have an email address associated with their account."
 
       expect(current_path).to eq(dashboard_path)
