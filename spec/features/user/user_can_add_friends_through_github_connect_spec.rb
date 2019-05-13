296f7a8915f7a84a9f2590d2e0addd31e36a0cc4
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-13 12:33:17 -0600

    Fixes Hound issues
    Co-authored-by: Erin King <44042415+erin-king@users.noreply.github.com>
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_through_github_connect_spec.rb
+++ spec/features/user/user_can_add_friends_through_github_connect_spec.rb
@@ -5,8 +5,7 @@ describe 'as a logged in user who has connected with github' do
    user_1 = create(:user, access_token: ENV['GITHUB_TOKEN_KEY'])
    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-   user_2 = create(:user, access_token: ENV['TEST_KEY'], github_login: 'jtaylor522')
-
+   create(:user, access_token: ENV['TEST_KEY'], github_login: 'jtaylor522')
 
    visit dashboard_path
    expect(page).to_not have_content("Your Friends")
@@ -21,6 +20,5 @@ describe 'as a logged in user who has connected with github' do
      expect(page).to have_content("Your Friends")
      expect(page).to have_content("jtaylor522")
    end
-
  end
 end
