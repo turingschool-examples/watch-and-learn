5292e8cd32446da28315bbe2f8c1858f34b7a170
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-13 08:08:32 -0600

    Migrates new column for github login
    Co-authored-by: Erin King <44042415+erin-king@users.noreply.github.com>
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_through_github_connect_spec.rb
+++ spec/features/user/user_can_add_friends_through_github_connect_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'as a logged in user who has connected with github' do
  it 'can add registered users as friends', :vcr do
-   user_1 = create(:user, access_token: ENV['GITHUB_TOKEN_KEY'], github_login: "jalena-penaligon")
+   user_1 = create(:user, access_token: ENV['GITHUB_TOKEN_KEY'])
    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
    user_2 = create(:user, access_token: ENV['TEST_KEY'], github_login: 'jtaylor522')
