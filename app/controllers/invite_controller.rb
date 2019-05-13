6f7ba53179fabde7c21be28dde0b85f456174b3b
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-13 14:02:59 -0600

    Adds sad pathing for sending emails, adds production configuration and updates cassettes all tests passing
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -5,9 +5,14 @@ class InviteController < ApplicationController
   def create
     service = GithubService.new(current_user)
     response = service.get_email(params[:github_handle])
-    github_user = GithubUser.new(response)
-    InviteSenderMailer.invite(current_user, github_user).deliver_now
-    flash[:success] = "Successfully sent invite!"
-    redirect_to dashboard_path
+    if response[:email]
+      github_user = GithubUser.new(response)
+      InviteSenderMailer.invite(current_user, github_user).deliver_now
+      flash[:success] = "Successfully sent invite!"
+      redirect_to dashboard_path
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+      redirect_to dashboard_path
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => "https://brown-field-of-dreams.herokuapp.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'brown-field-of-dreams.herokuapp.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
 end
