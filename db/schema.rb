0709791b08c750037976a3755a33a27da85cb5a5
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-13 08:42:04 -0600

    Migrates friendships table belonging to user
    Co-authored-by: Erin King <44042415+erin-king@users.noreply.github.com>
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_140747) do
+ActiveRecord::Schema.define(version: 2019_05_13_144110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "friend_id"
+    t.string "friend_login"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
@@ -82,6 +89,7 @@ ActiveRecord::Schema.define(version: 2019_05_13_140747) do
     t.index ["tutorial_id"], name: "index_videos_on_tutorial_id"
   end
 
+  add_foreign_key "friendships", "users"
   add_foreign_key "user_videos", "users"
   add_foreign_key "user_videos", "videos"
 end
