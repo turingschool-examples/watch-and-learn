91e38f8349a9350c901f58eaefd3a9e468c294aa
earl-stephens <stephensearl@hotmail.com>
2019-05-13 18:28:16 -0600

    worked on friendships.  able to add friends.  still need to display all friends.
    Co-authored-by: Deonte Cooper <45864171+djc00p@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  has_many :user_friendships, class_name: 'Friendship'
+  has_many :friendships, through: :user_friendships,
+            source: :friendship
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name
   enum role: %i[default admin]
@@ -25,10 +29,28 @@ class User < ApplicationRecord
   end
 
   def bookmarked_vids
-    Video.joins(:users)
-         .where(users: {id: self.id})
-         .group(:id)
-         .order(:tutorial_id)
-         .order(:position)
+  #   vids = Video.joins(:users).includes(:tutorial)
+  #        .select('tutorials.title as tutorial_title')
+  #        .group(:id, 'tutorial.id')
+  #        .order(:tutorial_id)
+  #        .order(:position)
+         # Tutorial.joins(videos: :user_videos).includes(:videos).where(user_videos: {user_id: self.id}).group(:id)
+         # binding.pry
+  end
+
+  def github_user(username)
+    User.find_by(username: username)
+  end
+
+  def friendship?(username)
+    user = self.github_user(username)
+    if user != nil
+      friends = Friendship.where(user_id: self.id, friendship_id: user.id)[0]
+      if friends != nil
+        true
+      end
+    else
+      nil
+    end
   end
 end
