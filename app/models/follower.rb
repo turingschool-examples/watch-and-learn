fc2bc40a727709c66e157cb21e2aa02a94022ee7
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-14 10:17:03 -0600

    adds follower_a_registered_user and not_friend_or_user? methods to follower poro

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -5,4 +5,16 @@ class Follower
     @url  = data[:html_url]
     @git_id = data[:id]
   end
+
+  def not_friend_or_user?
+    if !follower_a_registered_user || Friendship.find_by(friended_user_id: follower_a_registered_user.id)
+      false
+    else
+      true
+    end
+  end
+
+  def follower_a_registered_user
+    User.find_by(github_id: @git_id)
+  end
 end
