c7208a1efc763fa3cc3f7e2316ce7ecafedd3b48
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-14 10:49:04 -0600

    finishes card 10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,14 @@
 class WelcomeController < ApplicationController
   def index
     if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      if current_user
+        @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      else
+        @tutorials = Tutorial.non_class_content.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      end
     else
       if current_user
-        @tutorials = Tutorial.class_content.paginate(:page => params[:page], :per_page => 5)
+        @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
       else
         @tutorials = Tutorial.non_class_content.paginate(:page => params[:page], :per_page => 5)
       end
--- spec/features/user/only_users_can_see_classroom_content_spec.rb
+++ spec/features/user/only_users_can_see_classroom_content_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'on the root path' do
   before :each do
     @non_class_tut = create(:tutorial)
+    @non_class_tut2 = create(:tutorial)
     @class_tut = create(:tutorial, classroom: true)
+    @class_tut2 = create(:tutorial, classroom: true)
   end
 
   context 'as a visitor' do
@@ -11,7 +13,23 @@ RSpec.describe 'on the root path' do
       visit '/'
 
       expect(page).to have_link(@non_class_tut.title)
+      expect(page).to have_link(@non_class_tut2.title)
       expect(page).to_not have_link(@class_tut.title)
+      expect(page).to_not have_link(@class_tut2.title)
+    end
+  end
+
+  context 'as a logged in user' do
+    it 'can see all types of tutorials' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/'
+
+      expect(page).to have_link(@non_class_tut.title)
+      expect(page).to have_link(@non_class_tut2.title)
+      expect(page).to have_link(@class_tut.title)
+      expect(page).to have_link(@class_tut2.title)
     end
   end
 end
