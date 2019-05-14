4cb55e14843b4cbf9e7c97786280c3ce5d8f32d6
Ethan Grab <ethangrab@gmail.com>
2019-05-14 11:27:37 -0600

    Feature test for add a friend
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>

--- spec/features/user/user_can_add_a_friend_spec.rb
+++ spec/features/user/user_can_add_a_friend_spec.rb
@@ -2,21 +2,41 @@ require 'rails_helper'
 
 describe 'as a logged in User with github connected' do
   before :each do
-    @user_1 = create(:user, github_id: 2)
-    @user_2 = create(:user, github_id: 1)
+    @user = create(:user, github_token: ENV["GITHUB_API_KEY"], github_id: 40487417)
+    @friend_user = create(:user, github_id: 44073660)
   end
     it 'I can add a follower as a friend' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      VCR.use_cassette("services/find_followers") do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      visit dashboard_path
+        visit dashboard_path
 
-      expect(page).to have_content("Followers")
-      expect(page).to have_css(".follower")
+        expect(page).to have_content("Followers")
+        expect(page).to have_css(".follower")
 
-      within first ".follower" do
-        expect(page).to have_link("jaxtestingaccount")
-        expect(page).to have_link("Add as friend")
-        click_button("Add as friend")
+        within first ".follower" do
+          expect(page).to have_link("chitasan")
+          expect(page).to have_button("Add as Friend")
+        end
       end
     end
+
+    it 'I cannot add a follower as a friend if already friended' do
+      VCR.use_cassette("services/find_followers") do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+        Friendship.create!(user_id: @user.id, friended_user_id: @friend_user.id)
+
+        visit dashboard_path
+
+        expect(page).to have_content("Followers")
+        expect(page).to have_css(".follower")
+
+        within first ".follower" do
+          expect(page).to have_link("chitasan")
+          expect(page).to_not have_button("Add as Friend")
+        end
+      end
+    end
+
 end
