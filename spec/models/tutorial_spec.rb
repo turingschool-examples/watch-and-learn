3ec6b3f0ceffeb4d88f6aeee9bf9eff833c5ec9e
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-14 10:40:28 -0600

    adds class_content class method to tutorial model

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'class methods' do
+    it '.class_content' do
+      non_class_tut = create(:tutorial)
+      class_tut = create(:tutorial, classroom: true)
+      class_tut2 = create(:tutorial, classroom: true)
+
+      expect(Tutorial.class_content).to eq([class_tut, class_tut2])
+    end
+  end
 end
