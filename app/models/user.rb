a3f4389694d429d432283fc7f6baa1b37e4abfdc
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-14 10:17:49 -0600

    adds relationships to friendships and self referential relationships to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friended_users, through: :friendships
   has_many :friends, foreign_key: :friended_user_id, class_name: 'Friendship'
-  has_many :friended_users, through: :friendships, source: :user
+  has_many :users_who_friended, through: :friendships, source: :user
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name
