d01312bc12e9b12750b8806a541bb21aab60f978
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-14 14:24:28 -0600

    builds out initial test for adding friends (card 12)
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

---
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "as a logged in user that has linked a github" do
+  context "when I visit my dashboard" do
+    it "can add other users who have linked their github as friends from their followers and followed", :vcr do
+      user = create(:user)
+      friend_user = create(:user, github_name: 'tnodland', token: ENV['GITHUB_FRIEND_TOKEN'])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      within '.following' do
+        expect(page.all('.li').first).to_not have_link("Add as Friend")
+        expect(page.all('.li').last).to have_link("Add as Friend")
+      end
+
+      within '.followers' do
+        expect(page.all('.li').first).to_not have_link("Add as Friend")
+        expect(page.all('.li').last).to have_link("Add as Friend")
+      end
+    end
+  end
+end
