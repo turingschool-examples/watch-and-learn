70498157e02f042f413c38abb5bcd48aae3e1906
Ethan Grab <ethangrab@gmail.com>
2019-05-14 10:52:22 -0600

    added relationship tests
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe Friendship do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:friended_user_id) }
   end
+
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:friended_user) }
+  end
 end
