9a3d178630cb42afffe433138c180bd0d0543336
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-14 14:39:04 -0600

    adds to github/sessions_controller to update the users github name upon linking their account
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -3,7 +3,8 @@ class Github::SessionsController < ApplicationController
     omniauth_handshake = request.env['omniauth.auth']
     user = current_user
     token = omniauth_handshake.credentials.token
-    user.update(token: token)
+    name = omniauth_handshake.extra.raw_info.login
+    user.update(token: token, github_name: name)
     redirect_to dashboard_path
   end
 end
