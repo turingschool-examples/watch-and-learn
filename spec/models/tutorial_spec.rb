64e32b18c9f08bcb0eb79f85ed00ac4b6758f5a4
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-16 07:32:57 -0600

    Add space at end of curly braces to make HoundCI happy

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Tutorial, type: :model do
 
   describe 'relationships' do
-    it { should have_many :videos}
+    it { should have_many :videos }
   end
 
   describe 'validations' do
-    it { should validate_presence_of :title}
-    it { should validate_presence_of :description}
-    it { should validate_presence_of :thumbnail}
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :thumbnail }
   end
 end
