a5bff4bc0695a1b5f9a8c7f89f2a7dc25b987aac
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-16 08:08:12 -0600

    Adds spec for inviting friends by email
    Co-authored-by: Erin King <44042415+erin-king@users.noreply.github.com>
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

---
+++ spec/features/user/user_can_invite_friends_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe "As a registered, logged in user", :vcr do
+  it "allows me to invite friends by email" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+    click_link "Send an Invite"
+
+    expect(current_path).to eq(invite_path)
+
+    expect(page).to have_content("Enter valid Github Handle")
+    fill_in "github[:handle]", with: "jtaylor522"
+    click_button "Send Invite"
+    expect(page).to have_content("Successfully sent invite!")
+    expect(current_path).to eq(dashboard_path)
+  end
+
+  it "does not allow me to invite invalid users", :vcr do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+    click_link "Send an Invite"
+
+    expect(current_path).to eq(invite_path)
+
+    expect(page).to have_content("Enter valid Github Handle")
+    fill_in "github[:handle]", with: "iamnotavalidgithubname"
+    click_button "Send Invite"
+    error = "The github user you selected doesn't have a valid email address associated with their account."
+    expect(page).to have_content(error)
+    expect(current_path).to eq(invite_path)
+  end
+end
