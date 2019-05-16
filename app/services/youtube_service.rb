5d4ad44df25ca278699be85c881ec776ef831a13
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-15 21:04:12 -0600

    Add line to rails_helper to allow http connection when no cassette

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -1,13 +1,15 @@
 class YoutubeService
   def video_info(id)
+    id = id.slice(-11, 11)
     params = {part: "snippet,contentDetails,statistics", id: id}
-    # binding.pry .slice!(-11, 11)
+    # binding.pry #.slice!(-11, 11)
     get_json("youtube/v3/videos", params)
   end
 
   private
 
   def get_json(url, params)
+    # binding.pry
     response = conn.get("youtube/v3/videos", params)
     JSON.parse(response.body, symbolize_names: true)
   end
