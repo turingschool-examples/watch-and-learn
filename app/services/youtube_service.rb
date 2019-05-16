428bbf56b82b8c42dd9e25a9a1582795d2da47e7
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-15 21:08:35 -0600

    Change #get_json to accept a url as an argument and to take that argument in the response line

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -2,15 +2,13 @@ class YoutubeService
   def video_info(id)
     id = id.slice(-11, 11)
     params = {part: "snippet,contentDetails,statistics", id: id}
-    # binding.pry #.slice!(-11, 11)
     get_json("youtube/v3/videos", params)
   end
 
   private
 
   def get_json(url, params)
-    # binding.pry
-    response = conn.get("youtube/v3/videos", params)
+    response = conn.get(url, params)
     JSON.parse(response.body, symbolize_names: true)
   end
 
