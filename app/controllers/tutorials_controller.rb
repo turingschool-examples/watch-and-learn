4bfaa0a837b385027325a2152031134d74d1b17c
Ethan Grab <ethangrab@gmail.com>
2019-05-16 01:32:58 -0600

    reroute if user is not logged in

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -3,6 +3,10 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if current_user.nil? && tutorial.classroom?
+      redirect_to root_path
+    else
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    end
   end
 end
