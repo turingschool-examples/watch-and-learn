33a6b272e85b526e73d9471175f3c2e3e324aa4a
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-15 21:32:04 -0600

    Remove fields_for :videos in the new tutorial page

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -6,5 +6,4 @@ class Tutorial < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :thumbnail
-  # validates :image_url, format: {with: /\.(png|jpg)\Z/i}
 end
