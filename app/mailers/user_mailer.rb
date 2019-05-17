51c133f197c7a02d853d64ad8b9066f92b9cd58e
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-17 12:29:24 -0600

    removes name from to: block in registration_confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
     @user = user
-    mail(:to => "#{user.first_name} #{user.email}", :subject => "Registration Confirmation")
+    mail(:to => "#{user.email}", :subject => "Registration Confirmation")
   end
 end
