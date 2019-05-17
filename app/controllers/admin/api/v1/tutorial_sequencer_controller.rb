7c8eb67a3e2d8a85c62cabeeeb4ea0fd20831e5e
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-17 16:01:15 -0600

    Deleteed contents in the file was being test on  mnodel level

--- app/controllers/admin/api/v1/tutorial_sequencer_controller.rb
+++ app/controllers/admin/api/v1/tutorial_sequencer_controller.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
 class Admin::Api::V1::TutorialSequencerController < Admin::Api::V1::BaseController
-  def update
-    tutorial = Tutorial.find(params[:tutorial_id])
-    TutorialSequencer.new(tutorial, ordered_video_ids).run!
 
-    render json: tutorial
-  end
-
-  private
-
-  def ordered_video_ids
-    params[:tutorial_sequencer][:_json]
-  end
 end
