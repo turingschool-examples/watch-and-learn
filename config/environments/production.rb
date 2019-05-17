b72da487da6ede871babe34d183eb46b601b3e6f
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-17 12:49:08 -0600

    changes default host and domain to http instead of https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,13 +73,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://fast-wave-23899.herokuapp.com/',
+    :domain => 'http://fast-wave-23899.herokuapp.com/',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { host: "https://fast-wave-23899.herokuapp.com/", port: 587 }
+  config.action_mailer.default_url_options = { host: "http://fast-wave-23899.herokuapp.com/", port: 587 }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
