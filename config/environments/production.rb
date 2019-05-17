7d4aa996562801fd0a892b8aec6afefc5d42d36c
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-17 11:51:20 -0600

    adds actionmailer environment config from heroku docs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "personal_project_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'https://fast-wave-23899.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
