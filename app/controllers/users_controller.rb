7b9f805c6f95eff926b125b4a87cd39b02605e3b
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-16 20:30:10 -0600

    finishes writing test for card 7, adds flash messages for a new user upon registering
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = "Logged in as #{user.first_name}"
+      flash[:notice] = 'This account has not yet been activated. Please check your email.'
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
