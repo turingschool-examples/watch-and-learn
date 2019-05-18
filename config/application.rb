e0ed61a6ab0f8dfd146528ba5f29937570b0e20f
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-17 18:49:55 -0600

    Added Send Grid info for heroku

--- config/application.rb
+++ config/application.rb
@@ -34,12 +34,12 @@ module PersonalProject
     config.action_mailer.delivery_method = :smtp
 
    config.action_mailer.smtp_settings = {
-     address:              ‘smtp.sendgrid.net’,
-     port:                 ‘587’,
-     domain:               ‘example.com’,
-     user_name:            ENV[“SENDGRID_USERNAME”],
-     password:             ENV[“SENDGRID_PASSWORD”],
-     authentication:       ‘plain’,
+     address:              'smtp.sendgrid.net',
+     port:                 '587',
+     domain:               'example.com',
+     user_name:            ENV['SENDGRID_USERNAME'],
+     password:             ENV['SENDGRID_PASSWORD'],
+     authentication:       'plain',
      enable_starttls_auto: true
    }
   end
