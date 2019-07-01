a0e939e39f59e8df26dbfafe7558b23444e56237
billy urrutia <billyurrutia@gmail.com>
2019-07-01 16:32:15 -0600

    Initial commit
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,10 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
