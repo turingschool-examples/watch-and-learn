4578aeceb5de80c81946f028541290a7c5f0de31
Noah Flint <noah.flint24@gmail.com>
2019-07-02 16:50:47 -0600

    created test for displaying following users

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -3,28 +3,34 @@ require 'webmock/rspec'
 
 describe "As a logged in user, on /dashboard" do
   before :each do
-    @user = User.create(email: "john@gmail.com", first_name: "John", last_name: "smith", token: "790f5a98275d53160a72ff956ad8a4b171635419")
+    @user = User.create(email: "john@gmail.com", first_name: "John", last_name: "smith", token: ENV['GITHUB_API_KEY'])
     @user_2 = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
     json_repo_response = File.open("./fixtures/user_repos.json")
     stub_request(:get, "https://api.github.com/user/repos").
          with(
            headers: {
        	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'Authorization'=>'token 790f5a98275d53160a72ff956ad8a4b171635419',
+           'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'User-Agent'=>'Faraday v0.15.4'
            }).
          to_return(status: 200, body: json_repo_response, headers: {})
 
+    json_following_response = File.open("./fixtures/user_following.json")
+      stub_request(:get, "https://api.github.com/user/following").
+           with(
+             headers: {
+         	  'Accept'=>'*/*',
+         	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         	  'User-Agent'=>'Faraday v0.15.4'
+             }).
+           to_return(status: 200, body: json_following_response, headers: {})
+           allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit dashboard_path
+
   end
   context "There is a section for 'Github'" do
     it "Displays list of repository names each as links to their repo" do
-
-
-      visit dashboard_path
-
       within(".github") do
         within(".github-repos") do
           expect(page).to have_css(".repo", count: 5)
@@ -39,10 +45,22 @@ describe "As a logged in user, on /dashboard" do
 
     it "User without token does not see github section" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
-
       visit dashboard_path
 
       expect(page).to_not have_css(".github")
     end
+
+    it "Displays list of github users the current user follows, handels are links to profile" do
+      within(".github") do
+        within(".github-following") do
+          expect(page).to have_link("n-flint")
+          expect(page).to have_link("earl-stephens")
+          expect(page).to have_link("ryanmillergm")
+          expect(page).to have_link("Loomus")
+        end
+      end
+    end
   end
 end
+
+
