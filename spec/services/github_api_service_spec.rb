476c1585a2810059f7e3de53dfcf41dd2cf56621
Noah Flint <noah.flint24@gmail.com>
2019-07-02 16:52:43 -0600

    added #following tet to github service test

--- spec/services/github_api_service_spec.rb
+++ spec/services/github_api_service_spec.rb
@@ -3,18 +3,28 @@ require 'webmock/rspec'
 
 describe GithubApiService do
   before :each do
-    @user = User.create(email: "john@gmail.com", first_name: "John", last_name: "smith", token: "790f5a98275d53160a72ff956ad8a4b171635419")
+    @user = User.create(email: "john@gmail.com", first_name: "John", last_name: "smith", token: ENV['GITHUB_API_KEY'])
       @service = GithubApiService.new(@user.token)
+
       json_repo_response = File.open("./fixtures/user_repos.json")
       stub_request(:get, "https://api.github.com/user/repos").
            with(
              headers: {
          	  'Accept'=>'*/*',
          	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-         	  'Authorization'=>'token 790f5a98275d53160a72ff956ad8a4b171635419',
          	  'User-Agent'=>'Faraday v0.15.4'
              }).
            to_return(status: 200, body: json_repo_response, headers: {})
+
+      json_following_response = File.open("./fixtures/user_following.json")
+      stub_request(:get, "https://api.github.com/user/following").
+           with(
+             headers: {
+         	  'Accept'=>'*/*',
+         	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         	  'User-Agent'=>'Faraday v0.15.4'
+             }).
+           to_return(status: 200, body: json_following_response, headers: {})
 
     end
 
@@ -28,6 +38,16 @@ describe GithubApiService do
       expect(repo_data).to be_a Hash
       expect(repo_data).to have_key :name
       expect(repo_data).to have_key :html_url
+    end
+  end
+
+  context "#following" do
+    it "returns a given number of github users, current user is following" do
+      following = @service.following[0]
+
+      expect(following).to be_a Hash
+      expect(following).to have_key :login
+      expect(following).to have_key :html_url
     end
   end
 end
