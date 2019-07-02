37f563babf277b05e5999a9760f2faee1fad665a
James Cape <james.cape@gmail.com>
2019-07-02 13:50:16 -0600

    Runs rubocop and adds webdrivers to gemfile
    Co-Authored-By: Martin Mercer <m-mrcr@users.noreply.github.com>

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class YoutubeService
   def video_info(id)
-    params = {part: "snippet,contentDetails,statistics", id: id}
+    params = { part: 'snippet,contentDetails,statistics', id: id }
 
-    get_json("youtube/v3/videos", params)
+    get_json('youtube/v3/videos', params)
   end
 
   private
 
-  def get_json(url, params)
-    response = conn.get("youtube/v3/videos", params)
+  def get_json(_url, params)
+    response = conn.get('youtube/v3/videos', params)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(url: "https://www.googleapis.com") do |f|
-      f.adapter  Faraday.default_adapter
+    Faraday.new(url: 'https://www.googleapis.com') do |f|
+      f.adapter Faraday.default_adapter
       f.params[:key] = ENV['YOUTUBE_API_KEY']
     end
   end
