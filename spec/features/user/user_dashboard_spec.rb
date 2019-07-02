e76eb139c64bf1fe43c7e0a74a6cc7fe688d4148
David Hardy <davehardy632@gmail.com>
2019-07-01 20:08:33 -0600

    completed card 16

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 describe "As a logged in user, on /dashboard" do
   before :each do
     @user = User.create(email: "john@gmail.com", first_name: "John", last_name: "smith", token: "4505955c23675653cff57fddbde04c05d6594db7")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    visit dashboard_path
+    @user_2 = create(:user)
   end
   context "There is a section for 'Github'" do
     it "Displays list of repository names each as links to their repo" do
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit dashboard_path
 
       within(".github") do
         within(".github-repos") do
@@ -19,6 +22,14 @@ describe "As a logged in user, on /dashboard" do
           expect(page).to have_link("activerecord-obstacle-course-1")
         end
       end
+    end
+
+    it "User without token does not see github section" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
+
+      visit dashboard_path
+
+      expect(page).to_not have_css(".github")
     end
   end
 end
