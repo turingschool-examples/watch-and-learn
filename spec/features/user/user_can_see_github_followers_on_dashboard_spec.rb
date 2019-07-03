a38fe787e66f92570bd81e0905651463edded186
Ryan Miller <ryanmillergm@gmail.com>
2019-07-03 11:29:06 -0600

    Adds user github followers test and view

---
+++ spec/features/user/user_can_see_github_followers_on_dashboard_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+# As a logged in user
+# When I visit /dashboard
+# Then I should see a section for "Github"
+# And under that section I should see another section titled "Followers"
+# And I should see list of all followers with their handles linking to their Github profile
+
+describe 'As a logged in user' do
+  describe 'Visiting the dashboard' do
+    describe 'I see a section for github' do
+
+      it 'shows a list of github followers' do
+        user = create(:user, github_token: ENV["GITHUB_PAT"])
+
+        visit '/'
+
+        click_link 'Sign In'
+
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+
+        click_on "Log In"
+
+        within "#followers" do
+          expect(page).to have_content("Followers")
+          expect(page).to have_all_of_selectors("#follower-1", "#follower-2", "#follower-3", "#follower-4", "#follower-5")
+        end
+      end
+    end
+  end
+end
