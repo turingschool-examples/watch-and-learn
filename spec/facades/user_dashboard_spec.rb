eef4deb34a24b441dce8e4815994d4ef6653c390
davisc0801 <davisc1014@gmail.com>
2019-07-02 18:34:14 -0600

    Add VCR cassette call to facade test
    Co-Authored-By: Rob Stringer <Mycobee@users.noreply.github.com>

--- spec/facades/user_dashboard_spec.rb
+++ spec/facades/user_dashboard_spec.rb
@@ -8,9 +8,11 @@ describe UserDashboardFacade do
   end
 
   it "#repos" do
-    test_facade = UserDashboardFacade.new(ENV["GITHUB_API_KEY"])
-    expect(test_facade.repos(2)).to be_a(Array)
-    expect(test_facade.repos(2).first).to be_a(Repository)
-    expect(test_facade.repos(2).first.name).to eq("book_club")
+    VCR.use_cassette("github/github_repositories") do
+      test_facade = UserDashboardFacade.new(ENV["GITHUB_API_KEY"])
+      expect(test_facade.repos(2)).to be_a(Array)
+      expect(test_facade.repos(2).first).to be_a(Repository)
+      expect(test_facade.repos(2).first.name).to eq("book_club")
+    end
   end
 end
