4f3dfd5591523568dce97b1125e0ae8d090455cf
earl-stephens <stephensearl@hotmail.com>
2019-07-03 10:20:43 -0600

    created github service test
    Co-authored-by: Billy Urrutia <billyurrutia@gmail.com>

---
+++ spec/services/github_service_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe GithubService  do
+  it 'exists' do
+    user = create(:user)
+    github = GithubService.new(user)
+
+    expect(github).to be_a GithubService
+  end
+
+  context '#get_repos' do
+    it 'returns repo data' do
+      VCR.use_cassette('grab_github_service_repo') do
+        user = create(:user, username: 'CosmicSpagetti', github_token: ENV['GITHUB_TOKEN'])
+        github = GithubService.new(user)
+        repos = github.repos
+
+        expect(repos).to be_a Array
+      end
+    end
+  end
+end
