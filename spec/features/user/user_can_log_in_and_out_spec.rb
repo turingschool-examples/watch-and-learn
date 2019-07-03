5a81c7539df7f952bc87da22633249c63ba281e6
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-03 10:12:48 -0600

    fixes issues with user logging out by deleting cassette.
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/features/user/user_can_log_in_and_out_spec.rb
+++ spec/features/user/user_can_log_in_and_out_spec.rb
@@ -20,29 +20,31 @@ describe 'User' do
       expect(page).to have_content(user.email)
       expect(page).to have_content(user.first_name)
       expect(page).to have_content(user.last_name)
-    end 
+    end
   end
 
   it 'can log out', :js do
-    user = create(:user)
+    VCR.use_cassette("user_log_out") do
+      user = create(:user)
 
-    visit login_path
+      visit login_path
 
-    fill_in'session[email]', with: user.email
-    fill_in'session[password]', with: user.password
+      fill_in'session[email]', with: user.email
+      fill_in'session[password]', with: user.password
 
-    click_on 'Log In'
+      click_on 'Log In'
 
-    click_on 'Profile'
+      click_on 'Profile'
 
-    expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(dashboard_path)
 
-    click_on 'Log Out'
+      click_on 'Log Out'
 
-    expect(current_path).to eq(root_path)
-    expect(page).to_not have_content(user.first_name)
-    expect(page).to have_content('SIGN IN')
-  end
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_content(user.first_name)
+      expect(page).to have_content('SIGN IN')
+    end
+  end 
 
   it 'is shown an error when incorrect info is entered' do
     user = create(:user)
