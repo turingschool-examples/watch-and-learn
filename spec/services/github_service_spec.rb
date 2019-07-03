17b9bff6db57a23232ac81f7a4100cfa5acf985b
earl-stephens <stephensearl@hotmail.com>
2019-07-03 11:04:32 -0600

    created service test for github service
    Co-authored-by: Billy Urrutia <billyurrutia@gmail.com>

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -13,6 +13,7 @@ describe GithubService  do
       VCR.use_cassette('grab_github_service_repo') do
         user = create(:user, username: 'CosmicSpagetti', github_token: ENV['GITHUB_TOKEN'])
         github = GithubService.new(user)
+        
         repos = github.repos
 
         expect(repos).to be_a Array
