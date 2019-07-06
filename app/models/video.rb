a2f2c83687de47fdc3a71b91c047d43cfb977ca6
Ryan Miller <ryanmillergm@gmail.com>
2019-07-06 14:36:04 -0600

    Add update

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class Video < ApplicationRecord
+  validates_presence_of :position
+
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+  after_create :update_position
+
+  def update_position
+    binding.pry
+  end
+
 end
