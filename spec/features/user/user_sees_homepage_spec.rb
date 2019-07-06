85ad1f3e07f0d7b610afeb0657e4ce38a612a7ec
James Cape <james.cape@gmail.com>
2019-07-06 16:39:07 -0600

    Adds tutorial model logic to prevent visitors from seeing classroom tutorials
    Co-Authored-By: Martin Mercer <m-mrcr@users.noreply.github.com>

--- spec/features/user/user_sees_homepage_spec.rb
+++ spec/features/user/user_sees_homepage_spec.rb
@@ -5,6 +5,19 @@ require 'rails_helper'
 describe 'Visitor' do
   describe 'on the home page' do
     it 'can see a list of tutorials' do
+      user = create(:user)
+
+      visit '/'
+
+      click_on 'Sign In'
+
+      expect(current_path).to eq(login_path)
+
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
+
+      click_on 'Log In'
+
       tutorial1 = create(:tutorial, classroom: false)
       tutorial2 = create(:tutorial, classroom: true)
 
@@ -15,15 +28,13 @@ describe 'Visitor' do
 
       visit root_path
 
-      expect(page).to have_css('.tutorial', count: 1)
+      expect(page).to have_css('.tutorial', count: 2)
 
       within(first('.tutorials')) do
         expect(page).to have_css('.tutorial')
         expect(page).to have_css('.tutorial-description')
         expect(page).to have_content(tutorial1.title)
         expect(page).to have_content(tutorial1.description)
-        expect(page).to_not have_content(tutorial2.title)
-        expect(page).to_not have_content(tutorial2.description)
       end
     end
   end
