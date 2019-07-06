af70f037452a1052e3cf9a7ca97b6d71b47b0f29
Ryan Miller <ryanmillergm@gmail.com>
2019-07-06 12:41:04 -0600

    Adds admin can add new tutorial test

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -5,7 +5,8 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.find(params[:id])
   end
 
-  def create; end
+  def create;
+  end 
 
   def new
     @tutorial = Tutorial.new
