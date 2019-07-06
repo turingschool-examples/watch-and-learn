dc823b040e75b33cf3898dbcc8de3f40a327f8cb
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-06 17:20:24 -0600

    Builds rake task for updating video position, validation for same
    Co-Authored-By: James Cape <james-cape@users.noreply.github.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Video < ApplicationRecord
+  validates_numericality_of :position
+
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
