d17f38b0bfe24a857afaa219f7fcc71b02a1426b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-07 14:56:07 -0600

    Created user_can_see_followers_spec and cleaned up repo spec
    Co-Authored-By: Chris Davis <44012054+DavisC0801@users.noreply.github.com>

--- spec/facades/user_dashboard_spec.rb
+++ spec/facades/user_dashboard_spec.rb
@@ -1,18 +1,21 @@
 require "rails_helper"
 
 describe UserDashboardFacade do
+	before :each do 
+		token =	Token.create(github_token: ENV["GITHUB_API_KEY"])
+    @test_facade = UserDashboardFacade.new(token)
+	end
+
   it "exists" do
-    test_facade = UserDashboardFacade.new(ENV["GITHUB_API_KEY"])
     #TODO update with Oauth token
-    expect(test_facade).to be_a(UserDashboardFacade)
+    expect(@test_facade).to be_a(UserDashboardFacade)
   end
 
   it "#repos" do
     VCR.use_cassette("github/github_repositories", :allow_playback_repeats => true) do
-      test_facade = UserDashboardFacade.new(ENV["GITHUB_API_KEY"])
-      expect(test_facade.repos(2)).to be_a(Array)
-      expect(test_facade.repos(2).first).to be_a(Repository)
-      expect(test_facade.repos(2).first.name).to eq("book_club")
+      expect(@test_facade.repos(2)).to be_a(Array)
+      expect(@test_facade.repos(2).first).to be_a(Repository)
+      expect(@test_facade.repos(2).first.name).to eq("book_club")
     end
   end
 end
---
+++ spec/features/user/user_can_see_followers_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "User" do
+    before :each do
+      user = create(:user)
+			user.token = Token.create(github_token: ENV["GITHUB_API_KEY"])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+		end
+
+	context "As a logged in user when I visit my dashboard " do
+		it 'shows a section of followers with handles as links to github' do
+      visit dashboard_path
+      expect(page).to have_selector(".github_section")
+				
+	  	within ".github_section" do
+				expect(page).to have_link("user_name")
+			end
+		end	
+	end
+end
