337cb3669f5aa772e918c3f0ec2b2f37b8f106f1
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-07 16:09:36 -0600

    Adds github following feature spec
    Co-Authored-By: Chris Davis <44012054+DavisC0801@users.noreply.github.com>

---
+++ spec/features/user/user_can_see_following_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "User" do
+    before :each do
+      user = create(:user)
+			user.token = Token.create(github_token: ENV["GITHUB_API_KEY"])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+		end
+
+	context "As a logged in user when I visit my dashboard " do
+		it 'shows a section of following with handles as links to github' do
+			VCR.use_cassette("github/following", allow_playback_repeats: true) do
+      	visit dashboard_path
+      	expect(page).to have_selector(".github_section")
+					
+	  		within ".github_section" do
+					expect(page).to have_link("DragonQuills")
+				end
+			end
+		end	
+	end
+end
+
