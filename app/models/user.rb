6da86ab0ae1ac602555674588bbe6c503f7a57af
davisc0801 <davisc1014@gmail.com>
2019-07-08 17:43:04 -0600

    Add relations to user table
    Co-Authored-By: Rob Stringer <Mycobee@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_one :token
   has_many :user_videos
   has_many :videos, through: :user_videos
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
