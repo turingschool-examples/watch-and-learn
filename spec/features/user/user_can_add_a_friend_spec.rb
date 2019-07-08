b4496557784190c5ce001b35abcbd18241b3ffeb
davisc0801 <davisc1014@gmail.com>
2019-07-08 17:19:21 -0600

    Add spec for showing add to friend list buttons in view
    Co-Authored-By: Rob Stringer <Mycobee@users.noreply.github.com>

---
+++ spec/features/user/user_can_add_a_friend_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe "As a user on dashboard path" do
+  before :each do
+    @user = create(:user)
+    @user.update(html_url: "https://github.com/DavisC0801")
+    @user.token = Token.new(github_token: ENV["GITHUB_API_KEY"])
+    @user_2 = create(:user)
+    @user_2.update(html_url: "https://github.com/Mycobee")
+    @user_2.token = Token.new(github_token: ENV["GITHUB_API_KEY_2"])
+  end
+
+  scenario "Links show up next to followers that have accounts in our system." do
+    VCR.use_cassette("github/dashboard") do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
+      visit dashboard_path
+      within(".github_followers_section") do
+        expect(page).to have_button("Add to Friends")
+      end
+    end
+  end
+
+  scenario "Links show up next to following that have accounts in our system." do
+    VCR.use_cassette("github/dashboard") do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit dashboard_path
+      within(".github_following_section") do
+        expect(page).to have_button("Add to Friends")
+      end
+    end
+  end
+end
