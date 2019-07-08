8118bf60476a22d993024c555c8b5738446289d0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-07 18:33:22 -0600

    Backfilled oauth tests with omniauth mocks
    Co-Authored-By: Chris Davis <44012054+DavisC0801@users.noreply.github.com>

--- spec/features/user/user_github_oauth_spec.rb
+++ spec/features/user/user_github_oauth_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
-feature "" do
-	xit " " do
+feature "github oauth" do
+	before :each do
+		OmniAuth.config.test_mode = true
+		OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+			:provider => 'github',
+			:credentials => {token: ENV['GITHUB_API_KEY']}
+		})
+		@user = create(:user)
 		
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+	end
+
+	it "Starts without github access and grants after oauth" do
+		VCR.use_cassette('github/oauth') do
+			visit dashboard_path
+
+			expect(page).to_not have_selector(".github_section")
+
+			click_link("Connect to Github")
+
+			expect(current_path).to eq(dashboard_path)
+
+			expect(page).to have_selector(".github_section")
+		end
 	end
 end
 
