2b881b8f1739dd909600bd2362c03a6d78966b79
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-09 10:01:41 -0600

    Add html url to user model, and dependent destroy to user token.
    Co-Authored-By: Chris Davis <44012054+DavisC0801@users.noreply.github.com>

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,6 +2,7 @@ class GithubController < ApplicationController
 	def create
 		token	= auth_hash['credentials']['token']
 		current_user.token = Token.find_or_create_by(github_token: token)
+		current_user.update(html_url: auth_hash["info"]["urls"]["GitHub"])
 		redirect_to dashboard_path
 	end
 
