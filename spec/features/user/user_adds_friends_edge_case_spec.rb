cbf88183a1db93525821e28c8df3af43a90393f0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-08 19:13:36 -0600

    Adds edge case functionality testing

--- spec/features/user/user_adds_friends_edge_case_spec.rb
+++ spec/features/user/user_adds_friends_edge_case_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
-describe "Add friend edge case" do
+describe "Add friend edge case", type: :request do
   it "shows a message if an invalid user is sent" do
-    post(friendships_path, { params: {friend_html: "test"}})
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Invalid Friend.")
+		post '/friendships', { params: {friend_html: "test"}}
+
+    expect(response).to redirect_to(dashboard_path)
+		expect(flash[:message]).to be_present
   end
 end
