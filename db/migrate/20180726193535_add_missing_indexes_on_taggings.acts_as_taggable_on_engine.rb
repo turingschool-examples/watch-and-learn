28a7f9d9f4b0fb61fd3fc77af57b56c48daabf27
James Cape <james.cape@gmail.com>
2019-07-09 15:14:38 -0600

    Consolidates find_index lines per rubocop

--- db/migrate/20180726193535_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb
+++ db/migrate/20180726193535_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb
@@ -8,11 +8,7 @@ else
 end
 AddMissingIndexesOnTaggings.class_eval do
   def change
-    add_index :taggings, :tag_id unless index_exists? :taggings, :tag_id
-    add_index :taggings, :taggable_id unless index_exists? :taggings, :taggable_id
-    add_index :taggings, :taggable_type unless index_exists? :taggings, :taggable_type
-    add_index :taggings, :tagger_id unless index_exists? :taggings, :tagger_id
-    add_index :taggings, :context unless index_exists? :taggings, :context
+    add_indexes
 
     unless index_exists? :taggings, %i[tagger_id tagger_type]
       add_index :taggings, %i[tagger_id tagger_type]
@@ -21,5 +17,13 @@ AddMissingIndexesOnTaggings.class_eval do
     unless index_exists? :taggings, %i[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
       add_index :taggings, %i[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
     end
+  end
+
+  def add_indexes
+    add_index :taggings, :tag_id unless index_exists? :taggings, :tag_id
+    add_index :taggings, :taggable_id unless index_exists? :taggings, :taggable_id
+    add_index :taggings, :taggable_type unless index_exists? :taggings, :taggable_type
+    add_index :taggings, :tagger_id unless index_exists? :taggings, :tagger_id
+    add_index :taggings, :context unless index_exists? :taggings, :context
   end
 end
