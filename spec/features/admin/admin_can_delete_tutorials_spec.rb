46cf5f6e5df21fa0e8bfa806ad04d7f62fe3351e
Vince Carollo <vincecarollo@gmail.com>
2019-07-10 19:50:51 -0600

    Add spec to test tutorial videos being deleting when tutorial is deleted by admin

--- spec/features/admin/admin_can_delete_tutorials_spec.rb
+++ spec/features/admin/admin_can_delete_tutorials_spec.rb
@@ -18,5 +18,19 @@ RSpec.describe 'As an admin on the admin' do
       expect(page).to_not have_content(tutorial.title)
     end
 
+    it "deletes all tutorial videos when deleted" do
+      tutorial = create(:tutorial)
+      admin = create(:user, role: 1)
+      create(:video, tutorial: tutorial)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_path
+
+      click_link 'Destroy'
+
+      expect(Video.all).to be_empty
+    end
+
   end
 end
