577b2fb9b432c02db2e1e0140b1129c4306d4734
Vince Carollo <vincecarollo@gmail.com>
2019-07-11 22:38:53 -0600

    Add to feature spec for video thumbnails being created

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -20,6 +20,8 @@ describe 'An Admin can edit a tutorial' do
       click_on 'Create Video'
 
       expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+      expect(Video.last.thumbnail).to eq("https://i.ytimg.com/vi/J7ikFUlkP_k/hqdefault.jpg")
+      
       within(first('.video')) do
         expect(page).to have_content('How to tie your shoes.')
       end
