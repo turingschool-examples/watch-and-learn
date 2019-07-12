8a0c2ffa25dac19cf1d4356f05c6e10b5f38646b
Vince Carollo <vincecarollo@gmail.com>
2019-07-11 21:52:57 -0600

    Add spec for more specific video creation errors

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -7,7 +7,7 @@ describe 'An Admin can edit a tutorial' do
   let(:admin)    { create(:admin) }
 
   describe 'by adding a video' do
-    it 'works as intended', :vcr do
+    it 'works as intended' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit edit_admin_tutorial_path(tutorial)
@@ -25,7 +25,7 @@ describe 'An Admin can edit a tutorial' do
       end
     end
 
-    it 'Blocks invalid videos', :vcr do
+    it 'Blocks videos with no youtube id' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit edit_admin_tutorial_path(tutorial)
@@ -34,11 +34,40 @@ describe 'An Admin can edit a tutorial' do
 
       fill_in 'video[title]', with: 'How to tie your shoes.'
       fill_in 'video[description]', with: 'Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through.'
-      fill_in 'video[video_id]', with: ''
       click_on 'Create Video'
 
       expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
-      expect(page).to have_content('Unable to create video')
+      expect(page).to have_content('Youtube ID is required')
+    end
+
+    it 'Blocks videos with no title' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit edit_admin_tutorial_path(tutorial)
+
+      click_on 'Add Video'
+
+      fill_in 'video[description]', with: 'Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through.'
+      fill_in 'video[video_id]', with: 'J7ikFUlkP_k'
+      click_on 'Create Video'
+
+      expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+      expect(page).to have_content("Title can't be blank")
+    end
+
+    it 'Blocks videos with no description' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit edit_admin_tutorial_path(tutorial)
+
+      click_on 'Add Video'
+
+      fill_in 'video[title]', with: 'How to tie your shoes.'
+      fill_in 'video[video_id]', with: 'J7ikFUlkP_k'
+      click_on 'Create Video'
+
+      expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+      expect(page).to have_content("Description can't be blank")
     end
   end
 end
