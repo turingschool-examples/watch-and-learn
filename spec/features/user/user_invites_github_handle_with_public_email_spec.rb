ac3c6cef8c6e127ba69c07e5750b61e27d9383ff
James Cape <james.cape@gmail.com>
2019-07-12 18:17:01 -0600

    Changes email test sleep from 10s to 1s

--- spec/features/user/user_invites_github_handle_with_public_email_spec.rb
+++ spec/features/user/user_invites_github_handle_with_public_email_spec.rb
@@ -24,7 +24,7 @@ describe 'User logs in with Github' do
 
       fill_in 'invitee_github_handle', with: 'WHomer'
 
-      expect { click_button 'Send Invite'; sleep 10 }
+      expect { click_button 'Send Invite'; sleep 1 }
         .to change { ActionMailer::Base.deliveries.count }.by(1)
 
       expect(current_path).to eq(dashboard_path)
