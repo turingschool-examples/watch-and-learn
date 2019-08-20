ebfd2eaccc9e29a13fe6d403a45b81041fd87742
Jake Miller <jake0miller@gmail.com>
2019-08-20 15:48:15 -0600

    Add followers to github service spec
    Co-Authored-By: jori.peterson <48265011+JoriPeterson@users.noreply.github.com>

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -19,5 +19,17 @@ describe GithubService do
         expect(member_data).to have_key :name
       end
     end
+
+    context "#follower_data" do
+      it "returns followers", :vcr do
+        search = subject.follower_data
+        expect(search).to be_a Array
+        expect(search.count).to eq 1
+        expect(search[0]).to be_an Hash
+        follower_data = search[0]
+
+        expect(follower_data).to have_key :login
+      end
+    end
   end
 end
