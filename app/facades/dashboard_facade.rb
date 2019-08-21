d72348639cd1f63951d09e2d821da67f72edabc3
Sarah Tatro <statrox4@gmail.com>
2019-08-20 18:21:00 -0600

    User Dashboard: Github View & Facade
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

---
+++ app/facades/dashboard_facade.rb
@@ -0,0 +1,18 @@
+class DashboardFacade
+  def initialize(user)
+    @service = GithubService.new(user.token)
+  end
+
+  def repos
+    @_repos ||= get_repos
+  end
+
+  private
+  attr_reader :service
+
+  def get_repos
+    service.fetch_repos.take(5).map do |raw_repo|
+      Repo.new(raw_repo)
+    end
+  end
+end
