23aaf86cb5b2fadf2abe2ba516becf65db0c3e28
Sarah Tatro <statrox4@gmail.com>
2019-08-20 18:20:35 -0600

    User Dashboard Github Spec
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

---
+++ spec/features/user/user_dashboard_github_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Github Repos' do
+  scenario 'User can see 5 repos' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    VCR.use_cassette("synopsis") do
+      visit '/dashboard'
+    end
+
+    expect(current_path).to eq(dashboard_path)
+
+
+    within('#repos') do
+      expect(page).to have_css(".repo", count: 5)
+      within(first(".repo")) do
+        expect(page).to have_link("www.github.com/repo")
+      end
+    end
+  end
+end
