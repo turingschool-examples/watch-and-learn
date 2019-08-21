2cfa9faa2d3707c904d6db0246d8df479ba1a34f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-20 20:04:02 -0600

    Turns on VCR
    Co-authored-by: Stella Mainar <mainars09@alumni.hanover.edu>

--- spec/features/user/can_see_github_on_dashboard_spec.rb
+++ spec/features/user/can_see_github_on_dashboard_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 describe "User on dashboard" do
   it "can see 5 github repositories" do
-    # VCR.use_cassette('github_auth', record: :new_episodes) do
-WebMock.allow_net_connect!
-VCR.turn_off!
+    VCR.use_cassette('user_sees_repos', record: :new_episodes) do
+
       user = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -20,6 +19,6 @@ VCR.turn_off!
       within(first(".github-repos")) do
         expect(page).to have_link("brownfield-of-dreams")
       end
-    # end
+    end
   end
 end
