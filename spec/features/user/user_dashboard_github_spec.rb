7792ec8fe7464b6de9593f0e7ac0c666e73c3c95
Sarah Tatro <statrox4@gmail.com>
2019-08-21 15:45:48 -0600

    User Dashboard: Repo Spec
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

--- spec/features/user/user_dashboard_github_spec.rb
+++ spec/features/user/user_dashboard_github_spec.rb
@@ -5,18 +5,23 @@ feature 'Github Repos' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    VCR.use_cassette("synopsis") do
+    #VCR.use_cassette("synopsis") do
       visit '/dashboard'
-    end
+    #end
 
     expect(current_path).to eq(dashboard_path)
-
 
     within('#repos') do
       expect(page).to have_css(".repo", count: 5)
       within(first(".repo")) do
-        expect(page).to have_link("www.github.com/repo")
+        expect(page).to have_link("JoriPeterson/battleship")
       end
     end
+
+#     scenario 'I see another section titled Followers' do
+#       scenario 'And I should see list of all followers with their handles linking to their Github profile' do
+#
+#       end
+#     end
   end
 end
