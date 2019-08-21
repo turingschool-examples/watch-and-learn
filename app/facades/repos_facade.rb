fe4840d30455ad07731ebce01622422f132bfbb5
username <wnt925@gmail.com>
2019-08-21 14:15:02 -0600

    Add Repos facade

---
+++ app/facades/repos_facade.rb
@@ -0,0 +1,25 @@
+class Repo
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
+  def all_repos
+      conn = Faraday.new(url: 'https://api.github.com/user')
+      conn.basic_auth(@username, @password)
+      conn.get('/repos')
+  end
+end
+
+#
+#       faraday.adapter Faraday.default_adapter
+#     end
+#
+#     response = conn.get("/congress/v1/members/house/#{state}/current.json")
+#
+#     JSON.parse(response.body, symbolize_names: true)[:results]
+#   end
+#
+#   private
+#   attr_reader :state
+# end
