655bebc435e17a7e3a4615b5604fc75879af79f6
Aurie <Myrdden42@gmail.com>
2019-08-22 12:37:58 -0600

    Add Follower/Following to Dashboard Facade

--- app/facades/dashboard_facade.rb
+++ app/facades/dashboard_facade.rb
@@ -1,10 +1,19 @@
 class DashboardFacade
   def initialize(user)
     @service = GithubService.new(user.token)
+    @friendships = User.friendship_uids
   end
 
   def repos
     @_repos ||= get_repos
+  end
+
+  def followers
+    @_followers ||= get_followers
+  end
+  
+  def following
+    @_following ||= get_following
   end
 
   private
@@ -13,6 +22,18 @@ class DashboardFacade
   def get_repos
     service.fetch_repos.take(5).map do |raw_repo|
       Repo.new(raw_repo)
+    end
+  end
+
+  def get_followers
+    service.fetch_followers.map do |raw_follower|
+      GithubHandle.new(raw_follower, @friendships)
+    end
+  end
+
+  def get_following
+    service.fetch_following.map do |raw_following|
+      GithubHandle.new(raw_following, @friendships)
     end
   end
 end
