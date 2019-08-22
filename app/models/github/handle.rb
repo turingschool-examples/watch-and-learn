d37ad8f6ee24d51b40a73a83ba4881b2725fc3d5
Aurie <Myrdden42@gmail.com>
2019-08-22 15:11:11 -0600

    Add Github API Models

--- app/models/github/handle.rb
+++ app/models/github/handle.rb
@@ -1,10 +1,12 @@
-class GithubHandle
-  attr_reader :path, :name
-  def initialize(data, user_friendships)
-    @path = data[:html_url]
-    @name = data[:login]
-    @friend = user_friendships.include? data[:id]
-  end
+module Github
+  class Handle
+    attr_reader :path, :name
+    def initialize(data, user_friendships)
+      @path = data[:html_url]
+      @name = data[:login]
+      @friend = user_friendships.include? data[:id]
+    end
 
-  def friend?; @friend end
+    def friend?; @friend end
+  end
 end
--- app/models/github/repo.rb
+++ app/models/github/repo.rb
@@ -1,8 +1,10 @@
-class Repo
-  attr_reader :full_path, :path
+module Github
+  class Repo
+    attr_reader :full_path, :path
 
-  def initialize(data)
-    @full_path = data[:html_url]
-    @path = data[:full_name]
+    def initialize(data)
+      @full_path = data[:html_url]
+      @path = data[:full_name]
+    end
   end
 end
