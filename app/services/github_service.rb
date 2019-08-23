f1d0340e651eb64bc0e36619be7ba4185c55ed24
Jake Miller <jake0miller@gmail.com>
2019-08-23 09:46:02 -0600

    Add string interpolation of github token inside github service

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,6 @@
 class GithubService
 	def initialize(token)
-		@token = token
+		@token = token.nil? ? nil : "token " + token
 	end
 
   def repository_data
@@ -18,13 +18,8 @@ class GithubService
   private
 
   def conn
-<<<<<<< HEAD
-    @_conn ||= Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers["Authorization"] = ENV['GITHUB_API_KEY']
-=======
     @_conn = Faraday.new(url: "https://api.github.com") do |faraday|
       faraday.headers["Authorization"] = @token
->>>>>>> 6d7e861dec7ef7ab66b413241fa70b19f7d4c8aa
       faraday.headers["Accept"] = "application/vnd.github.v3+json"
       faraday.adapter Faraday.default_adapter
     end
