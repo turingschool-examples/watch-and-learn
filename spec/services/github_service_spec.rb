4ba8a77be0e41c2556edf1b1d76702526cc4f3d2
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-24 10:16:58 -0600

    Add followers and following to github service spec

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -6,11 +6,25 @@ describe GithubService do
       it "returns github data" do
         stub_dashboard_api_calls
         repos = subject.repos
+        followers = subject.followers
+        following = subject.following
 
         expect(repos).to be_a Array
         expect(repos[0]).to be_a Hash
         expect(repos.count).to eq(30)
         expect(repos[0]).to have_key :name
+
+        expect(followers).to be_a Array
+        expect(followers[0]).to be_a Hash
+        expect(followers.count).to eq(4)
+        expect(followers[0]).to have_key :login
+        expect(followers[0]).to have_key :html_url
+
+        expect(following).to be_a Array
+        expect(following[0]).to be_a Hash
+        expect(following.count).to eq(3)
+        expect(following[0]).to have_key :login
+        expect(following[0]).to have_key :html_url
       end
     end
   end
