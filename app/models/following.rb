9e9525353a4aa10b0081c139d0165aac426526ed
Jori Peterson <joripeterson@gmail.com>
2019-08-24 16:59:34 -0600

    Add in_system to attr_reader
    Co-Authored-By: Jake Miller <jake0miller@gmail.com>

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,8 +1,9 @@
 class Following
-	attr_reader :login, :url
+	attr_reader :login, :url, :in_system
 
 	def initialize(data = {})
 		@login = data[:login]
 		@url = data[:html_url]
+		@in_system = UserCredential.exists?(nickname: @login)
 	end
 end
