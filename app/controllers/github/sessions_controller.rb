ec11b48ec02f7ed7642b7103767e2cdcad115d46
Jori Peterson <joripeterson@gmail.com>
2019-08-24 17:26:09 -0600

    Refactor github sessions controller for user info
    Co-Authored-By: Jake Miller <jake0miller@gmail.com>

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -1,9 +1,7 @@
 class Github::SessionsController < ApplicationController
 	def create
 		user_info = request.env['omniauth.auth']
-		token = user_info["credentials"]["token"]
-		nickname = user_info["info"]["nickname"]
-		current_user.add_credential("github", token, nickname)
+		current_user.add_credential("github", user_info)
 		redirect_to dashboard_path
 	end
 end
