dea817fc63e8483cf2d48bff5603ed7625006266
Jori Peterson <joripeterson@gmail.com>
2019-08-24 16:58:59 -0600

    Refactor in_system for followers and following
    Co-Authored-By: Jake Miller <jake0miller@gmail.com>

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,14 +1,13 @@
 class Follower
-  attr_reader :login, :url
+  attr_reader :login, :url, :in_system
 
   def initialize(attributes = {})
     @login = attributes[:login]
 		@url = attributes[:html_url]
+		@in_system = UserCredential.exists?(nickname: @login)
   end
 
-	def friendable?(login)
-		 !UserCredential.where("nickname = ?", login).empty?		 
-	end
-
-
+	# def friendable?(login)
+	# 	 !UserCredential.where("nickname = ?", login).empty?
+	# end
 end
