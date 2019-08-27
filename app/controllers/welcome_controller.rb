c48c781b951cd6ec868c79758c667b7c197bf944
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-27 13:43:23 -0600

    Add current user into conditional statement

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    binding.pry
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    elsif current_user.nil?
+      @tutorials = Tutorial.where(classroom: false)
     else
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
     end
