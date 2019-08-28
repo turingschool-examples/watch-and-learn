c85ce6bfa629b41e4710a97d15169ec7c9a46074
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-27 20:10:55 -0600

    Add a default video method to controller

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,16 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
+    if params[:video_id] == nil
+      params[:video_id] = default_video.id
+    end
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
+
+  private
+
+  # def default_video
+  #   binding.pry
+  #   Video.first
+  # end
 end
--- spec/features/tutorials/show_spec.rb
+++ spec/features/tutorials/show_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 feature 'tutorial show page' do
   scenario 'does not error out if there are no videos' do
     t1 = create(:tutorial)
+    video = create(:video)
 
     visit tutorial_path(t1)
 
   end
 end
 
-# If a tutorial doesn't have any videos and we visit its show page
-# an error occurs when trying to call current_video.title since
-# current_video is nil.
-#
 # A possible easy fix is to default to Video.new if there aren't
 # any videos associated with the tutorial.
