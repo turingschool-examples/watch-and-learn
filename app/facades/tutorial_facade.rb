9f78f53238f0e208bac64caa18f324c50d4d465f
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-27 21:01:54 -0600

    Add multiple videos into tutorials show to avoid any errors if tutorial does not have a video

--- app/facades/tutorial_facade.rb
+++ app/facades/tutorial_facade.rb
@@ -5,13 +5,17 @@ class TutorialFacade < SimpleDelegator
   end
 
   def current_video
-    if videos.length == 0
-      Video.new(title: "Sorry", description: "No video yet", video_id: 0, tutorial_id: 0, position: 0)
+    if videos == []
+      Video.new(title: "Sorry", description: "No video", video_id: "0", thumbnail: "https://cdn.pixabay.com/photo/2016/02/08/17/43/sorry-1186962__340.jpg", tutorial_id: self.id, position: 1)
     elsif @video_id
       videos.find(@video_id)
     else
       videos.first
     end
+  end
+
+  def multiple_videos?
+    videos != []
   end
 
   def next_video
@@ -29,7 +33,6 @@ class TutorialFacade < SimpleDelegator
   end
 
   def maximum_video_position
-    binding.pry
     videos.max_by { |video| video.position }.position
   end
 end
--- spec/features/tutorials/show_spec.rb
+++ spec/features/tutorials/show_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'tutorial show page' do
   scenario 'does not error out if there are no videos' do
     t1 = create(:tutorial)
-    video = create(:video)
 
     visit tutorial_path(t1)
 
