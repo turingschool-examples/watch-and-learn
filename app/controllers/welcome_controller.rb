14abd74dc4363c620d200ddf6212ff677269607b
Alec Wells <alec.wells47@gmail.com>
2019-10-08 13:14:01 -0600

    Finish Rubocop setup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,8 @@
 class WelcomeController < ApplicationController
   def index
     if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+      @tutorials = Tutorial.tagged_with(params[:tag])
+        .paginate(page: params[:page], per_page: 5)
     else
       @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
     end
