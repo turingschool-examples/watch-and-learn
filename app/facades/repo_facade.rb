96b8d1ad98869bfd325c8401f4c53273fcaa00b6
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-09 17:05:02 -0600

    Include current user to pass through to service in repo facade
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>

--- app/facades/repo_facade.rb
+++ app/facades/repo_facade.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class RepoFacade
-  def create_repos
-    get_repo_data.map do |repo|
+  def create_repos(current_user)
+    get_repo_data(current_user).map do |repo|
       Repo.new(repo)
     end[0..4]
   end
 
-  def get_repo_data
-    GithubSearchResults.new.repos
+  def get_repo_data(current_user)
+    GithubSearchResults.new(current_user).repos
+
   end
 end
