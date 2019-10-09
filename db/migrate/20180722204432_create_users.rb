6369ad4b306274748cda9b1c0b2aa47a673cbf25
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-09 17:00:02 -0600

    Create migration and update schema so that users table has uid and github token columns
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>

--- db/migrate/20180722204432_create_users.rb
+++ db/migrate/20180722204432_create_users.rb
@@ -8,7 +8,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string  :last_name
       t.string  :password_digest
       t.integer :role, default: 0
-
+      t.string  :github_token
+      t.string :uid
       t.timestamps
     end
   end
