e2a1770f56012370d7cb228287b1793135acb07f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-09 16:59:20 -0600

    Update show action in user controller to have access to current user and the user's github token
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@
 
 class UsersController < ApplicationController
   def show
+    @current_user = current_user
+    if @current_user.github_token?
     render locals: {
-      repos: RepoFacade.new.create_repos
+      repos: RepoFacade.new.create_repos(@current_user)
     }
+    end
   end
 
   def new
