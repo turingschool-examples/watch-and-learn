02e1b47e5b2eaa85c2b50b2bd72a4e349fa5465e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-10 16:43:20 -0600

    Rename feature test for accessing github repos to include all tests for getting github repos, followers, and followed accounts
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
    
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- spec/features/user/user_sees_github_information_spec.rb
+++ spec/features/user/user_sees_github_information_spec.rb
@@ -7,7 +7,7 @@ describe 'A registered user' do
     json_response = File.open('./fixtures/github_repo_data.json')
     stub_request(:get, 'https://api.github.com/user/repos').to_return(status: 200, body: json_response)
 
-    user = create(:user, github_token: ENV["github_api_key"])
+    user = create(:user, github_token: ENV["GITHUB_API_KEY"])
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -19,6 +19,40 @@ describe 'A registered user' do
       expect(page).to have_css('.repo_name_and_link')
     end
   end
+
+  it 'I can see list of my github followers with their names as links to their profiles', :vcr do
+    json_response = File.open('./fixtures/github_followers_data.json')
+    stub_request(:get, 'http://api.github.com/user/followers').to_return(status: 200, body: json_response)
+
+    user = create(:user, github_token: ENV["GITHUB_API_KEY"])
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    expect(page).to have_content('Github Section')
+
+    within('.github') do
+      expect(page).to have_css('.followers_name_and_link')
+    end
+  end
+
+    it 'I can see list of accounts that I follow with name of each account I follow linked to their account page on github if I have a github_token', :vcr do
+      json_response = File.open('./fixtures/github_following_data.json')
+      stub_request(:get, 'https://api.github.com/user/following').to_return(status: 200, body: json_response)
+
+      user = create(:user, github_token: ENV["GITHUB_API_KEY"])
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+
+      expect(page).to have_content('Github Section')
+
+      within('.github') do
+        expect(page).to have_css('.following_name_and_link')
+      end
+    end
 
   it 'I cannot see list of five github repos if I do not have a github_token', :vcr do
     json_response = File.open('./fixtures/github_repo_data.json')
