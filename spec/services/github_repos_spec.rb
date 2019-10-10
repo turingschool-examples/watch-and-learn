502048373e3d01bcfd9bf54d4a42d1f38f6d02ef
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-10 16:56:28 -0600

    Modify and cassettes for testing and add service tests to verify that github api is returning followers and followed account information

--- spec/services/github_repos_spec.rb
+++ spec/services/github_repos_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'github api service' do
   it 'returns repos', :vcr do
-    service = GithubSearchResults.new(create(:user, github_token: ENV['github_api_key']))
+    service = GithubSearchResults.new(create(:user, github_token: ENV['GITHUB_API_KEY']))
     array_of_hashes_data = service.repos
 
     expect(service).to be_an_instance_of(GithubSearchResults)
@@ -11,5 +11,27 @@ RSpec.describe 'github api service' do
     expect(array_of_hashes_data.first).to have_key(:name)
     expect(array_of_hashes_data.first).to have_key(:url)
     expect(array_of_hashes_data.count).to eq(30)
+  end
+
+  it 'returns followers', :vcr do
+    service = GithubSearchResults.new(create(:user, github_token: ENV['GITHUB_API_KEY']))
+    array_of_hashes_data = service.followers
+
+    expect(service).to be_an_instance_of(GithubSearchResults)
+
+    expect(array_of_hashes_data).to be_a(Array)
+    expect(array_of_hashes_data.first).to have_key(:login)
+    expect(array_of_hashes_data.first).to have_key(:html_url)
+  end
+
+  it 'returns followed accounts', :vcr do
+    service = GithubSearchResults.new(create(:user, github_token: ENV['GITHUB_API_KEY']))
+    array_of_hashes_data = service.followed_accounts
+
+    expect(service).to be_an_instance_of(GithubSearchResults)
+
+    expect(array_of_hashes_data).to be_a(Array)
+    expect(array_of_hashes_data.first).to have_key(:login)
+    expect(array_of_hashes_data.first).to have_key(:html_url)
   end
 end
