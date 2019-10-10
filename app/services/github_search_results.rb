37c04af22cb4f3a9de50cdf70f2ece04c2a6530b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-10 16:46:46 -0600

    Add followers and followed_accounts methods to github search results service to initiate get requests from github for a users followers and followed accounts on github
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
    
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- app/services/github_search_results.rb
+++ app/services/github_search_results.rb
@@ -7,8 +7,18 @@ class GithubSearchResults
   end
 
   def repos
-    response = conn.get('/user/repos', access_token: @current_user.github_token) # .json ???
+    response = conn.get('/user/repos', access_token: @current_user.github_token)
     repo_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def followers
+    response = conn.get('/user/followers', access_token: @current_user.github_token)
+    follower_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def followed_accounts
+    response = conn.get('/user/following', access_token: @current_user.github_token)
+    followed_accounts_data = JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
