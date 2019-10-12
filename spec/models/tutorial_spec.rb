ca529f29dabbbb234301ea471bf8549fb6a74356
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-12 13:18:29 -0600

    Adds relationship test to tutorials and adds a test to determine that when a tutorial is destroyed, its videos are removed from the database.
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,4 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+
+  describe 'relationships' do
+    it { should have_many(:videos) }
+  end
+
+  describe 'deleting a tutorial' do
+    it "deletes videos when the tutorial is deleted" do
+      tutorial = Tutorial.create
+      video1 = create(:video, tutorial_id: tutorial.id)
+
+      expect { tutorial.destroy }.to change { Video.count }.by(-1)
+    end
+  end
 end
