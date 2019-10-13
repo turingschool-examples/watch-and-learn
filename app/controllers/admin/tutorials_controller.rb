79a4837505d3e684244261ac3797c849457b0d49
Alec Wells <alec.wells47@gmail.com>
2019-10-13 11:11:46 -0600

    Start work on add new tutorial

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -5,7 +5,9 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.find(params[:id])
   end
 
-  def create; end
+  def create
+     tut = Tutorial.create!(tutorial_params)
+  end
 
   def new
     @tutorial = Tutorial.new
@@ -22,6 +24,6 @@ class Admin::TutorialsController < Admin::BaseController
   private
 
   def tutorial_params
-    params.require(:tutorial).permit(:tag_list)
+    params.require(:tutorial).permit(:tag_list, :title, :description, :thumbnail)
   end
 end
