783ca1e7e3bf96f36a4e26cbd823e00b8984af4d
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-13 15:46:44 -0600

    Writes tests for dashboard_path to have video titles that have been bookmarked
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <joshsherwood1@users.noreply.github.com>

--- spec/features/user/user_can_bookmark_a_video_spec.rb
+++ spec/features/user/user_can_bookmark_a_video_spec.rb
@@ -33,4 +33,44 @@ describe 'A registered user' do
     click_on 'Bookmark'
     expect(page).to have_content('Already in your bookmarks')
   end
+
+  it "shows a list of all bookmarked segments under the Bookmarked Segments section, organized by which tutorial they're a part of and ordered by their position" do
+    tutorial = create(:tutorial)
+    tutorial_2 = create(:tutorial)
+    tutorial_3 = create(:tutorial)
+    video = create(:video, tutorial_id: tutorial.id)
+    video_2 = create(:video, tutorial_id: tutorial_2.id)
+    video_3 = create(:video, tutorial_id: tutorial_3.id)
+    video_4 = create(:video, tutorial_id: tutorial_3.id)
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/tutorials/#{tutorial.id}?video_id=#{video.id}"
+    click_on 'Bookmark'
+
+    visit "/tutorials/#{tutorial_2.id}?video_id=#{video_2.id}"
+    click_on 'Bookmark'
+
+    visit "/tutorials/#{tutorial_3.id}?video_id=#{video_3.id}"
+    click_on 'Bookmark'
+    visit "/tutorials/#{tutorial_3.id}?video_id=#{video_4.id}"
+    click_on 'Bookmark'
+
+    visit dashboard_path
+
+    expect(page).to have_content(video.title)
+    expect(page).to have_content(video_2.title)
+    expect(page).to have_content(video_3.title)
+    expect(page).to have_content(video_4.title)
+
+    within "tutorial" do
+      expect(page).to have_content(video.title)
+    end
+
+    within "tutorial_3" do
+      expect(page).to have_content(video_3.title)
+      expect(page).to have_content(video_4.title)
+    end
+  end
 end
