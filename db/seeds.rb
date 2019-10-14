50f8109e4fef06dad633d532df2280b2f4f624e5
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-14 17:54:54 -0600

    Update seeds so that one user is already connected to github
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,5 +117,5 @@ m3_tutorial.videos.create!(
 )
 
 User.create!(email: 'josh@example.com', first_name: 'Josh', last_name: 'Sherwood', password: 'password')
-User.create!(email: 'leiya@example.com', first_name: 'Leiya', last_name: 'Kenney', password: 'password')
+User.create!(email: 'leiya@example.com', first_name: 'Leiya', last_name: 'Kenney', password: 'password', github_token: ENV["LEIYA_GITHUB_API_KEY"], github_username: 45922590)
 User.create!(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password: 'password', role: :admin)
