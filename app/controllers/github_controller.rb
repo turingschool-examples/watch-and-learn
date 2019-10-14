cea4812dc705ef1b1c3f7fb25f504a3d9e69203f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-14 15:39:10 -0600

    Add functionality to github controller so that a users uid is also stored in user table during omniauth process
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
    
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -3,7 +3,9 @@ class GithubController < ApplicationController
   def create
     @current_user = current_user
     token = auth_hash["credentials"]["token"]
+    uid = auth_hash["uid"]
     @current_user.update_attribute(:github_token, token)
+    @current_user.update_attribute(:github_username, uid)
     @current_user.save
     redirect_to '/dashboard'
   end
