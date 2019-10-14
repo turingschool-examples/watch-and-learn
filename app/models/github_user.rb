f0bcfe7c774c30f51bbfaa42ac3ed45467d41215
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-14 14:46:45 -0600

    Some pseudo code for if user's already friends
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -2,7 +2,6 @@ class GithubUser
   attr_reader :login, :html_url
 
   def initialize(github_user_hash)
-
     @github_id = github_user_hash[:id]
     @login = github_user_hash[:login]
     @html_url = github_user_hash[:html_url]
@@ -11,4 +10,8 @@ class GithubUser
   def is_user?
     User.find_by(github_id: @github_id)
   end
+
+  # def already_friends?
+  #   Friendship.find_by????
+  # end
 end
