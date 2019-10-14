11ba2358e1cbb51c96d2817a507d36f538873680
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-14 15:33:45 -0600

    Add feature test for verifying that users who are also friends on github have link to add each other on their dashboard
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
    
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

---
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+describe 'A registered user' do
+  it 'can see a default video on a tutorial show page when a tutorial was created without any videos', :vcr do
+    json_response = File.open('./fixtures/github_repo_data.json')
+    stub_request(:get, 'https://api.github.com/user/repos').to_return(status: 200, body: json_response)
+
+    json_response = File.open('./fixtures/github_followers_data.json')
+    stub_request(:get, 'http://api.github.com/user/followers').to_return(status: 200, body: json_response)
+
+    json_response = File.open('./fixtures/github_following_data.json')
+    stub_request(:get, 'https://api.github.com/user/following').to_return(status: 200, body: json_response)
+
+    #tutorial = create(:tutorial, title: 'How to Tie Your Shoes', classroom: false)
+    user = create(:user, github_token: ENV["GITHUB_API_KEY"], github_username: 49769068)
+    user_2 = create(:user, github_token: ENV["LEIYA_GITHUB_API_KEY"], github_username: 45922590)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit "/dashboard"
+    expect(page).to have_content("leiyakenney || Add as Friend")
+    expect(page).to have_link("Add as Friend")
+  end
+end
