7d0c688cd61efb4aa6553c4724b1be276cbe258a
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-13 18:45:15 -0600

    Creates self.ordered_grouped_videos method
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <joshsherwood1@users.noreply.github.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,4 +5,10 @@ class Video < ApplicationRecord
   has_many :users, through: :user_videos
   belongs_to :tutorial
   validates_presence_of :position
+
+  def self.ordered_grouped_videos
+    self.select("videos.tutorial_id, videos.title, videos.position")
+    .order(:position)
+    .group_by(&:tutorial_id)
+  end
 end
