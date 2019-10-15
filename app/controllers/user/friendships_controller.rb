baefa684519f294314b27feb35663f4e2fb15467
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-15 11:39:35 -0600

    Update user friendships controller with conditional to account for is person is already friends with someone and tries to add them as a friend again
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>

--- app/controllers/user/friendships_controller.rb
+++ app/controllers/user/friendships_controller.rb
@@ -1,9 +1,14 @@
 class User::FriendshipsController < ApplicationController
 
   def create
-    current_user.friendships.create!(user_id: params["id"], friend_id: params["friend_id"])
-    friend_user = User.find(params["friend_id"])
-    friend_user.friendships.create!(user_id: params["friend_id"], friend_id: params["id"])
-    redirect_to "/dashboard"
+    if (current_user.friendships != []) && (current_user.friendships.any? { |friendship| friendship.friend.id.to_s == params["friend_id"] })
+      flash[:error] = "You are already friends with this person"
+      redirect_to "/dashboard"
+    else
+      current_user.friendships.create!(user_id: params["id"], friend_id: params["friend_id"])
+      friend_user = User.find(params["friend_id"])
+      friend_user.friendships.create!(user_id: params["friend_id"], friend_id: params["id"])
+      redirect_to "/dashboard"
+    end
   end
 end
