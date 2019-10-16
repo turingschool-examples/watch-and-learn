e7583b423905f6870b690cd053eaa10aaf8db577
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-15 21:12:13 -0600

    Intial setup for displaying user's bookmarks

---
+++ spec/features/user/user_can_see_bookmarked_videos_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+# As a logged in user
+# When I visit '/dashboard'
+# Then I should see a list of all bookmarked segments under the Bookmarked Segments section
+# And they should be organized by which tutorial they are a part of
+# And the videos should be ordered by their position
+
+
+describe "invite guest" do
+  it "can invite guest using github handle" do
+    user = create(:user, github_id: 123, github_token: ENV["GITHUB_API_KEY"])
+
+    tutorial_1 = create(:tutorial, title: "title_1")
+      video_1 = create(:video, title: "video_1", position: 1)
+      video_2 = create(:video, title: "video_2", position: 2)
+      video_3 = create(:video, title: "video_3", position: 3)
+
+    tutorial_2 = create(:tutorial, title: "title_2")
+      video_4 = create(:video, title: "video_4", position: 1)
+      video_5 = create(:video, title: "video_5", position: 2)
+      video_6 = create(:video, title: "video_6", position: 3)
+
+    user_videos_1 = create(:user_video, user_id: user.id, video_id: video_1.id)
+    user_videos_2 = create(:user_video, user_id: user.id, video_id: video_2.id)
+    user_videos_3 = create(:user_video, user_id: user.id, video_id: video_3.id)
+    user_videos_4 = create(:user_video, user_id: user.id, video_id: video_4.id)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    # /tutorials/3?video_id=5
+
+    within ".bookmark" do
+      within "#tutorial-#{tutorial_1.id}" do
+        expect(page).to have_link(video_1.name)
+        expect(page).to have_link(video_2.name)
+        expect(page).to have_link(video_3.name)
+      end
+    end
+
+    within ".bookmark" do
+      within "#tutorial-#{tutorial_2.id}" do
+        expect(page).to have_link(video_4.name)
+        expect(page).to_not have_link(video_5.name)
+        expect(page).to_not have_link(video_6.name)
+      end
+    end
+
+    within ".bookmark" do
+      within "#tutorial-#{tutorial_1.id}" do
+        click_link(video_1.name)
+      end
+    end
+
+    expect(current_path).to eq("/tutorials/#{tutorial_1.id}?video_id=#{video_1.id}")
+  end
+end
