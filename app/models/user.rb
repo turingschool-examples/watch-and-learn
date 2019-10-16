6d812464d90cf195d0a07d9452e5c93c2bbc0198
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-15 22:02:57 -0600

    Updated models with relationship to user and tutorials

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,9 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friendship_users, through: :friendships
 
+  has_many :videos, through: :user_videos
+  has_many :tutorials, through: :user_videos
+
   # has_many :friendships, foreign_key: :friendship_user, class_name: 'Friendship'
   # has_many :friendship_users, through: :friendships, source: :user
 end
