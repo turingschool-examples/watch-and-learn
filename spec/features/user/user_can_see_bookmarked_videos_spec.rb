8f919801b3f9ba934f6aa070f95669d5da9b3438
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-16 13:17:53 -0600

    removed save_and_open_page

--- spec/features/user/user_can_see_bookmarked_videos_spec.rb
+++ spec/features/user/user_can_see_bookmarked_videos_spec.rb
@@ -9,17 +9,18 @@ require 'rails_helper'
 
 describe "invite guest" do
   it "can invite guest using github handle" do
+    
     user = create(:user, github_id: 123, github_token: ENV["GITHUB_API_KEY"])
 
     tutorial_1 = create(:tutorial, title: "title_1")
-      video_1 = create(:video, title: "video_1", position: 1)
-      video_2 = create(:video, title: "video_2", position: 2)
-      video_3 = create(:video, title: "video_3", position: 3)
+      video_1 = create(:video, title: "video_1", position: 1, tutorial_id: tutorial_1.id)
+      video_2 = create(:video, title: "video_2", position: 2, tutorial_id: tutorial_1.id)
+      video_3 = create(:video, title: "video_3", position: 3, tutorial_id: tutorial_1.id)
 
     tutorial_2 = create(:tutorial, title: "title_2")
-      video_4 = create(:video, title: "video_4", position: 1)
-      video_5 = create(:video, title: "video_5", position: 2)
-      video_6 = create(:video, title: "video_6", position: 3)
+      video_4 = create(:video, title: "video_4", position: 1, tutorial_id: tutorial_2.id)
+      video_5 = create(:video, title: "video_5", position: 2, tutorial_id: tutorial_2.id)
+      video_6 = create(:video, title: "video_6", position: 3, tutorial_id: tutorial_2.id)
 
     user_videos_1 = create(:user_video, user_id: user.id, video_id: video_1.id)
     user_videos_2 = create(:user_video, user_id: user.id, video_id: video_2.id)
@@ -29,8 +30,6 @@ describe "invite guest" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
-
-    save_and_open_page
 
     within ".bookmark" do
       within "#tutorial-#{tutorial_1.id}" do
