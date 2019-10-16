f454c869fbc18788c5bf001de1ac99cd87d0daa7
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-16 17:20:32 -0600

    Minor spacing change

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -23,7 +23,6 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   private
-
   def tutorial_params
     params.require(:tutorial).permit(:tag_list, :title, :description, :thumbnail, :playlist_id, video_attributes: [:title, :description, :video_id, :thumbnail])
   end
