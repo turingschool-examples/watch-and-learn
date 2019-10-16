90e06ce05929e3c1c1c81a853c1cd883cd9e15d4
Alec Wells <alec.wells47@gmail.com>
2019-10-16 06:08:49 -0600

    Refactor

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,19 @@ module PersonalProject
     config.generators.system_tests = nil
   end
 end
+
+module BrownfieldV1
+  class Application < Rails::Application
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      domain:               'example.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+  end
+end
