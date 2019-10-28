6172c3d52cd3949e2882fbadf31449013b5efc6d
TheMindset <mulezig2014@outlook.fr>
2019-10-28 10:49:14 +0100

    build github service

---
+++ app/services/github_service.rb
@@ -0,0 +1,21 @@
+class GithubService
+
+  def repository_data
+    get_json("/user/repos")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = ENV['CLIENT_ID']
+      faraday.adapter(Faraday.default_adapter)
+    end
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+end
