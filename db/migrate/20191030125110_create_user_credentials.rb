64810770101eb14814f67eeb0cb20328106a4d24
TheMindset <mulezig2014@outlook.fr>
2019-10-30 15:11:26 +0100

    create  credentials table  for user

---
+++ db/migrate/20191030125110_create_user_credentials.rb
@@ -0,0 +1,9 @@
+class CreateUserCredentials < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_credentials do |t|
+      t.string :website
+      t.string :token
+      t.references :user, foreign_key: true
+    end
+  end
+end
