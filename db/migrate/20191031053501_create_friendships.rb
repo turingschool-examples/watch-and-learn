91422d6f82ecce9c7100079dc976260071937d0b
TheMindset <mulezig2014@outlook.fr>
2019-10-31 07:30:51 +0100

    update friendships table

--- db/migrate/20191031053501_create_friendships.rb
+++ db/migrate/20191031053501_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, foreigny_key: true
-      t.bigint :friend_id
+      t.bigint :friend_id, index: true
 
       t.timestamps
     end
