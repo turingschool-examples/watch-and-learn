85a89103a7cc585d6030ed7819ebb8102afec950
TheMindset <mulezig2014@outlook.fr>
2019-11-01 07:13:54 +0100

    new file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_125110) do
+ActiveRecord::Schema.define(version: 2019_11_01_052459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
@@ -87,6 +96,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_125110) do
     t.index ["tutorial_id"], name: "index_videos_on_tutorial_id"
   end
 
+  add_foreign_key "friendships", "users"
   add_foreign_key "user_credentials", "users"
   add_foreign_key "user_videos", "users"
   add_foreign_key "user_videos", "videos"
