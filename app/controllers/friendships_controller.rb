212b37ba5410ad3e50b7b022526fe640f5cfeeba
TheMindset <mulezig2014@outlook.fr>
2019-11-02 16:42:33 +0100

    frendly navigation

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,13 @@
 
 class FriendshipsController < ApplicationRecord
   def create
-    current_user.friendships.create!(friend_id: params[:id].to_i)
-    redirect_to dashboard_path
+
+    friend = current_user.friendships.build(friend_id: params[:id].to_i)
+    if friend.save
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Something happen please retry!!"
+      redirect_to dashboard_path
+    end
   end
 end
