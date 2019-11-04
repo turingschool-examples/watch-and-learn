6e7e41ae60407bfca1fc765d2266a8dba524c549
TheMindset <mulezig2014@outlook.fr>
2019-11-04 10:23:39 +0100

    limit authorization for tutorial index

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class TutorialsController < ApplicationController
+  def index
+    @tutorials = if current_user
+                   Tutorial.all
+                 else
+                   Tutorial.where("classroom = false")
+                 end
+  end
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
