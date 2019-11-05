3178f191d02c9af0ac1de42cacd4f1f9fffc3bfa
TheMindset <mulezig2014@outlook.fr>
2019-11-05 07:50:15 +0100

    redirect to the page for add video if tutorial empty

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -10,7 +10,9 @@ class TutorialsController < ApplicationController
   end
 
   def show
-    tutorial = Tutorial.find(params[:id])
+    tutorial = Tutorial.includes(:videos).find(params[:id])
+    redirect_to new_admin_tutorial_video_path(tutorial) if tutorial.videos.empty?
+
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
 end
