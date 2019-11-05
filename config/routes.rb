ee94582db421e9806105f7966e01e3bf5dd784c1
TheMindset <mulezig2014@outlook.fr>
2019-11-05 07:45:21 +0100

    organize routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,24 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'welcome#index'
-  get 'tags/:tag', to: 'welcome#index', as: :tag
-  get '/register', to: 'users#new'
+  namespace :api do
+    namespace :v1 do
+      put "tutorial_sequencer/:tutorial_id", to: "tutorial_sequencer#update"
+    end
+  end
 
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
     resources :tutorials, only: [:create, :edit, :update, :destroy, :new] do
-      resources :videos, only: [:create]
+      resources :videos, only: [:create, :new]
     end
     resources :videos, only: [:edit, :update, :destroy]
-
-    namespace :api do
-      namespace :v1 do
-        put "tutorial_sequencer/:tutorial_id", to: "tutorial_sequencer#update"
-      end
-    end
   end
+
+  root 'welcome#index'
+  get 'tags/:tag', to: 'welcome#index', as: :tag
+  get '/register', to: 'users#new'
+
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
@@ -36,6 +37,7 @@ Rails.application.routes.draw do
 
   # Is this being used?
   get '/video', to: 'video#show'
+
 
   resources :users, only: [:new, :create, :update, :edit]
 
