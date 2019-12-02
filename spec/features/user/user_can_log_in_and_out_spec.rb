e9b1f6ee6234ccb0c21e1c8950c583fc57aa1147
Laura Schulz <lrs8810@gmail.com>
2019-12-02 15:36:26 -0700

    Add cassette and vcr modules

--- spec/features/user/user_can_log_in_and_out_spec.rb
+++ spec/features/user/user_can_log_in_and_out_spec.rb
@@ -21,7 +21,7 @@ describe 'User' do
     expect(page).to have_content(user.last_name)
   end
 
-  it 'can log out', :js do
+  it 'can log out', :js, :vcr do
     user = create(:user)
 
     visit login_path
