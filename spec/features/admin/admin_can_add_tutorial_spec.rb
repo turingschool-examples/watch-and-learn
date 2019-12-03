4588cbe3d30526df1d095fdfc59ee250a916dea2
Laura Schulz <lrs8810@gmail.com>
2019-12-03 16:52:28 -0700

    Add admin can add tutorial spec
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

---
+++ spec/features/admin/admin_can_add_tutorial_spec.rb
@@ -0,0 +1,38 @@
+# When I visit '/admin/tutorials/new'
+# And I fill in 'title' with a meaningful name
+# And I fill in 'description' with a some content
+# And I fill in 'thumbnail' with a valid YouTube thumbnail
+# And I click on 'Save'
+# Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
+# And I should see a flash message that says "Successfully created tutorial."
+
+require 'rails_helper'
+
+RSpec.describe  'As an admin' do
+  before(:each) do
+    admin = create(:admin)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/admin/tutorials/new'
+  end
+
+  it 'I can add a new tutorial' do
+
+    fill_in "tutorial[title]", with: 'Baking - The Basics'
+    fill_in "tutorial[description]", with: "Venture with Laura into her kitchen as she shows you the baking basics."
+    fill_in "tutorial[thumbnail]", with: 'https://www.1training.org/wp-content/uploads/2019/05/Advanced-Diploma-in-Cake-Baking-and-Ornamenting-460x306.jpg'
+    click_button 'Save'
+
+    tutorial = Tutorial.last
+
+    expect(current_path).to eq("/tutorials/#{tutorial.id}")
+    expect(page).to have_content('Successfully created tutorial!')
+  end
+
+  it "it won't create the tutorial with incomplete fields" do
+    click_button 'Save'
+
+    expect(page).to have_content("Title can't be blank, Description can't be blank, and Thumbnail can't be blank")
+  end
+end
