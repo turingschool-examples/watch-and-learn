040ed1f78c76948c8a9812514adde23e438347b0
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-03 15:28:09 -0700

    Add restriction for classroom content in WelcomeController
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,28 @@
 class WelcomeController < ApplicationController
   def index
     if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      tagged_tutorials
     else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      tutorials
     end
   end
+   
+  private
+    def tagged_tutorials
+      if current_user
+        @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)     
+      else 
+        all_tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)     
+        @tutorials = all_tutorials.where(classroom: false)
+      end
+    end
+
+    def tutorials
+      if current_user
+        @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      else
+        all_tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+        @tutorials = all_tutorials.where(classroom: false)  
+      end
+    end
 end
