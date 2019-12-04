67db53d97f2a6c7045b29397744788d72fed2d65
Brian Bower <brian@brianbower.com>
2019-12-03 18:45:55 -0700

    Add flash message for when unregistered user tries to add bookmark. User story 13.

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,5 +2,6 @@ class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
+    flash.now[:alert] = "User must login to bookmark videos." if params[:alert].present?
   end
 end
