ae4acb9170c74641ae02175867638245090714c9
Graham Thompson <grwthomps@gmail.com>
2019-12-03 20:35:51 -0700

    Complete test for creating new tutorial as admin, create sad path test
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/features/admin/new_tutorial_spec.rb
+++ spec/features/admin/new_tutorial_spec.rb
@@ -8,13 +8,40 @@ RSpec.describe "When I visit '/admin/tutorials/new'" do
 
     visit '/admin/tutorials/new'
 
-    fill_in "Title", with: "How to brush your teeth"
-    fill_in "Description", with: "Electric toothbrush or toothbrush"
-    fill_in "Thumbnail", with: "https://www.youtube.com/watch?v=bifu6CzomHI"
+    within "#new_tutorial_form" do
+      fill_in "Title", with: "Basic Hygiene Tutorial"
+      fill_in "Description", with: "A series of videos covering basic hygiene."
+      fill_in "Thumbnail", with: "https://images.unsplash.com/photo-1418754356805-b89082b6965e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+    end
+
+    within "#new_video_form" do
+      fill_in "Title", with: "How to brush your teeth"
+      fill_in "Description", with: "Description for how to brush your teeth"
+      fill_in "Video", with: "https://www.youtube.com/watch?v=bifu6CzomHI"
+    end
 
     click_on "Save"
 
     expect(current_path).to eq("/tutorials/#{Tutorial.last.id}")
     expect(page).to have_content("Successfully created tutorial.")
+    expect(page).to have_content("Basic Hygiene Tutorial")
+    expect(page).to have_content("How to brush your teeth")
+    expect(page).to have_content("Description for how to brush your teeth")
+  end
+
+  it "cannot create a tutorial if fields are blank" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/admin/tutorials/new'
+
+    within "#new_tutorial_form" do
+      fill_in "Title", with: "Basic Hygiene Tutorial"
+      fill_in "Description", with: "A series of videos covering basic hygiene."
+      fill_in "Thumbnail", with: "https://images.unsplash.com/photo-1418754356805-b89082b6965e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+    end
+
+    click_on "Save"
+
+    expect(page).to have_content("Please fill in all fields to create this tutorial")
   end
 end
