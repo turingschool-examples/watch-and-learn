dc6ccbe70e0ecda7f40af079b18108ad41cfc5e7
Graham Thompson <grwthomps@gmail.com>
2019-12-04 15:01:50 -0700

    Refactor new tutorial test to allow creating a tutorial without a video
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/features/admin/new_tutorial_spec.rb
+++ spec/features/admin/new_tutorial_spec.rb
@@ -8,25 +8,16 @@ RSpec.describe "When I visit '/admin/tutorials/new'" do
 
     visit '/admin/tutorials/new'
 
-    within "#new_tutorial_form" do
-      fill_in "Title", with: "Basic Hygiene Tutorial"
-      fill_in "Description", with: "A series of videos covering basic hygiene."
-      fill_in "Thumbnail", with: "https://images.unsplash.com/photo-1418754356805-b89082b6965e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-    end
-
-    within "#new_video_form" do
-      fill_in "Title", with: "How to brush your teeth"
-      fill_in "Description", with: "Description for how to brush your teeth"
-      fill_in "Video", with: "https://www.youtube.com/watch?v=bifu6CzomHI"
-    end
+    fill_in "Title", with: "Basic Hygiene Tutorial"
+    fill_in "Description", with: "A series of videos covering basic hygiene."
+    fill_in "Thumbnail", with: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg"
 
     click_on "Save"
 
     expect(current_path).to eq("/tutorials/#{Tutorial.last.id}")
     expect(page).to have_content("Successfully created tutorial.")
     expect(page).to have_content("Basic Hygiene Tutorial")
-    expect(page).to have_content("How to brush your teeth")
-    expect(page).to have_content("Description for how to brush your teeth")
+    expect(page).to have_content("No tutorial videos have been added.")
   end
 
   it "cannot create a tutorial if fields are blank" do
@@ -34,11 +25,9 @@ RSpec.describe "When I visit '/admin/tutorials/new'" do
 
     visit '/admin/tutorials/new'
 
-    within "#new_tutorial_form" do
-      fill_in "Title", with: "Basic Hygiene Tutorial"
-      fill_in "Description", with: "A series of videos covering basic hygiene."
-      fill_in "Thumbnail", with: "https://images.unsplash.com/photo-1418754356805-b89082b6965e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-    end
+    fill_in "Title", with: "Basic Hygiene Tutorial"
+    fill_in "Description", with: ""
+    fill_in "Thumbnail", with: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg"
 
     click_on "Save"
 
