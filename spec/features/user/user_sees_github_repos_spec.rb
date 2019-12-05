0da7d9d07cbf98cceb1a460fb20a1b832ac29e63
Ryan Hantak <rhantak@gmail.com>
2019-12-05 13:12:02 -0700

    Add tests for rendering user github repos on dashboard
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

---
+++ spec/features/user/user_sees_github_repos_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'User' do
+  describe "When I visit my dashboard", :vcr do
+    before(:each) do
+      @user = create(:user, token: ENV['USER_GITHUB_TOKEN'])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit '/dashboard'
+    end
+
+    it "I a Github section with 5 of my repos, where each name is a link" do
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content('Github Repositories')
+      expect(page).to have_css('.github-repo', count: 5)
+      # make sure text is not empty in any
+    end
+  end
+end
