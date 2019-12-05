7ffb3ba749fe7ce568b47661919b311df3f30a8e
Michael Cooper <macoop2363@gmail.com>
2019-12-04 18:18:57 -0700

    Add test to return only Tutorials that are classroom content

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,4 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'validations' do
+  end
+
+  describe 'relationships' do
+  end
+
+  describe 'methods' do
+    it 'classroom_content' do
+      tutorial_1 = create(:tutorial)
+      tutorial_2 = create(:tutorial, classroom: true)
+
+      result = Tutorial.classroom_content
+
+      expect(result).to eq([tutorial_2])
+    end
+  end
 end
