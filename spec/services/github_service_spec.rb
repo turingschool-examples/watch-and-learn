a328ed584c1059e45287f773036aa495241185c5
Brian Bower <brian@brianbower.com>
2019-12-05 16:33:28 -0700

    Comment out test

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # require 'rails_helper'
 #
 # describe GithubService do
@@ -7,13 +6,3 @@
 #     expect(search).to be_an Array
 #   end
 # end
-=======
-require 'rails_helper'
-
-describe GithubService do
-  it "returns github data", :vcr do
-    search = subject.repos_by_user
-    expect(search).to be_an Array
-  end
-end
->>>>>>> 8494c042f4f17b4861ccf9a0d017de033838ec32
