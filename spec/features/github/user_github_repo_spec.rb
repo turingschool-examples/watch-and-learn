1a954b2d550729ad2a685cd20a48ac76468ef901
Tyla Devon <tyladevon@gmail.com>
2019-12-04 22:22:27 -0700

    Continue working on test for user github repos.
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- spec/features/github/user_github_repo_spec.rb
+++ spec/features/github/user_github_repo_spec.rb
@@ -4,17 +4,20 @@ require 'rails_helper'
 RSpec.describe "As a logged in user" do
   context "When I visit my dashboard" do
     it "I should see a list of 5 repo's with each Name as a link in 'Github' section" do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      VCR.use_cassette('user_github_repos') do
+        user = create(:user, github_token: '9ba6546442c611a522b341ed40f962ff1febea15')
 
-      visit '/dashboard'
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      within "#Github" do
-        expect(page).to have_link("")
-        expect(page).to have_link("")
-        expect(page).to have_link("")
-        expect(page).to have_link("")
-        expect(page).to have_link("")
+        visit '/dashboard'
+
+        within "#Github" do
+          expect(page).to have_link("")
+          expect(page).to have_link("")
+          expect(page).to have_link("")
+          expect(page).to have_link("")
+          expect(page).to have_link("")
+        end
       end
     end
   end
