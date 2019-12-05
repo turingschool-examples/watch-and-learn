201d09dfb7f3a38cfe2b0b2a304a40feff9257aa
Brian Bower <brian@brianbower.com>
2019-12-05 14:35:27 -0700

    Refactor into helper methods
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,12 +1,19 @@
 class GithubService
   def repos_by_user
-    access_token = '43eddb0d1c2125f11ea3452ffc1454ce0dfb5e37'
-    conn = Faraday.new(url: 'https://api.github.com') do |faraday|
-      faraday.params['access_token'] = access_token
+    get_json('/user/repos')
+  end
+
+  private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true).take(5)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.github.com') do |faraday|
+      faraday.params['access_token'] = ENV['GITHUB_TOKEN']
       faraday.adapter Faraday.default_adapter
     end
-    response = conn.get('user/repos')
-    json = JSON.parse(response.body, symbolize_names: true)
-    repos = json.take(5)
   end
 end
