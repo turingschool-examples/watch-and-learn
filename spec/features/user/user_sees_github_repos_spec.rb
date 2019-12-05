594dc40bd8da6832b98b86538bbda4ab6836561f
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-05 15:21:26 -0700

    Update spec to connect with github using omni-auth
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- spec/features/user/user_sees_github_repos_spec.rb
+++ spec/features/user/user_sees_github_repos_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 describe 'User' do
   describe "When I visit my dashboard", :vcr do
     before(:each) do
-      @user = create(:user, token: ENV['USER_GITHUB_TOKEN'])
+      @user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-      visit '/dashboard'
+      visit dashboard_path
     end
 
-    it "I see a Github section with 5 of my repos, where each name is a link" do
-      expect(current_path).to eq('/dashboard')
+    it "I see a Connect to Github button if not connected" do
+      expect(page).to have_button("Connect to Github")
+    end
+
+    it 'can connect with Github OAuth and see 5 repos' do
+      click_button "Connect to Github"
+  
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(dashboard_path)
       expect(page).to have_content('Github Repositories')
       expect(page).to have_css('.github-repo', count: 5)
     end
