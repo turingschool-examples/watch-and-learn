96720f8e7e135264da84c52e5b30b73a91874914
Michael Cooper <macoop2363@gmail.com>
2019-12-04 19:18:43 -0700

    Add test to ensure that a visitor cannot view a tutorial show page that is marked as 'classroom content'

--- spec/features/vistors/tutorials/show_spec.rb
+++ spec/features/vistors/tutorials/show_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
-describe 'visitor sees a video show' do
-  it 'vistor clicks on a tutorial title from the home page' do
-    tutorial = create(:tutorial)
-    video = create(:video, tutorial_id: tutorial.id)
+describe 'As a visitor' do
+  describe 'when I visit a tutorial show page' do
+    it 'visitor can see a list of videos and the tutorial title' do
+      tutorial = create(:tutorial)
+      video_1 = create(:video, tutorial_id: tutorial.id)
+      video_2 = create(:video, tutorial_id: tutorial.id)
 
-    visit '/'
+      visit tutorial_path(tutorial)
 
-    click_on tutorial.title
+      expect(page).to have_content(tutorial.title)
+      expect(page).to have_content(video_1.title)
+      expect(page).to have_content(video_2.title)
+    end
 
-    expect(current_path).to eq(tutorial_path(tutorial))
-    expect(page).to have_content(video.title)
-    expect(page).to have_content(tutorial.title)
+    it 'visitor cannot view a tutorial show page that is classroom content' do
+      tutorial = create(:tutorial, classroom: true)
+
+      visit tutorial_path(tutorial)
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('You must login or register to view this page!')
+    end
   end
 end
