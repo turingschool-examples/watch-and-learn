06961e78e62f3799edfbe876880345b5ab88fc6f
Ryan Hantak <rhantak@gmail.com>
2019-12-05 13:13:07 -0700

    Add service PORO to make requests to github API
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

---
+++ app/services/github_service.rb
@@ -0,0 +1,26 @@
+class GithubService
+  def initialize(token)
+    @token = token
+  end
+  
+  def fetch_repos
+    response = fetch_data("user/repos")
+    parse_data(response)
+  end
+
+  private
+    def fetch_data(url)
+      conn.get(url)
+    end
+
+    def parse_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def conn
+      Faraday.new(url: 'https://api.github.com') do |faraday|
+        faraday.params["access_token"] = @token
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+end
