282acdcbc605fb092870cff46563dda336140f2e
Ryan Hantak <rhantak@gmail.com>
2019-12-05 13:14:59 -0700

    Add UserDecorator for github functionalities
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

---
+++ app/facades/user_decorator.rb
@@ -0,0 +1,37 @@
+class UserDecorator < SimpleDelegator
+
+  def initialize(user)
+    @user = super(user)
+  end
+
+  # def github_search_result
+  #   GithubSearchResult.new(@user.token)
+  # end
+
+  # def repos
+  #   github_search_result.repos[0..4]
+  # end
+
+  def repos
+    raw_repo_data = get_repo_data
+    create_repos(raw_repo_data)
+  end
+
+
+  private
+    attr_reader :user
+
+    def get_repo_data
+      @raw_repo_data ||= service.fetch_repos
+    end
+
+    def service
+      GithubService.new(user.token)
+    end
+
+    def create_repos(raw_repo_data)
+      raw_repo_data.map do |repo|
+        Repo.new(repo)
+      end[0..4]
+    end
+end
