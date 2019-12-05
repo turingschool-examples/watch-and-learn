2d37040d46c37d6f4ffde98aff026affd037986a
Michael Cooper <macoop2363@gmail.com>
2019-12-04 19:20:16 -0700

    Add check to make sure a visitor cannot view a tutorial that is marked as 'classroom content'

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,17 @@
 class TutorialsController < ApplicationController
+
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    require_user! if tutorial.classroom
+    @facade = TutorialFacade.new(tutorial, params[:video_id]) 
+  end
+
+  private
+
+  def require_user!
+    if !current_user
+      flash[:error] = 'You must login or register to view this page!'
+      redirect_to root_path
+    end
   end
 end
