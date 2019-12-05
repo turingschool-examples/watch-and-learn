f5ae3b8edf2ecbbc7eb7182d43e9947a0ad5b2b8
Tyla Devon <tyladevon@gmail.com>
2019-12-05 14:28:35 -0700

    Rename and refactor facade to include fetching user info.
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

---
+++ app/facades/user_info_facade.rb
@@ -0,0 +1,26 @@
+class UserInfoFacade
+  def initialize(current_user)
+    @current_user = current_user
+  end
+
+  def get_repos
+    service = GithubService.new(@current_user)
+    service.fetch_repos
+  end
+
+  def limit_five
+    raw_repo_data = get_repos
+    raw_repo_data[0..4].map do |data|
+      Repo.new(data[:name], data[:html_url])
+    end
+  end
+
+  def all_followers
+    service = GithubService.new(@current_user)
+    login = service.fetch_user[:login]
+    followers = service.fetch_followers(login)
+    followers.map do |follower|
+      Follower.new(follower[:login], follower[:html_url])
+    end
+  end
+end
