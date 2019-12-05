5e2c541d74a6eee3d135a10936670e1785189752
Laura Schulz <lrs8810@gmail.com>
2019-12-04 19:45:04 -0700

    Add UserFacade with repos, followers, and followings methods
    Co-Authored-By: David Lopex <ldavid.x.lopezl@gmail.com>

---
+++ app/facades/user_facade.rb
@@ -0,0 +1,38 @@
+class UserFacade
+  attr_reader :user
+  
+  def initialize(user)
+    @user = user
+    @git_service = GithubService.new(@user.token)
+  end
+
+  def repos
+    @repos ||= begin
+      raw_repos_data = @git_service.get_repos
+
+      raw_repos_data.map do |repo|
+        Repo.new(repo)
+      end
+    end
+  end
+
+  def followers
+    @followers ||= begin
+      raw_followers_data = @git_service.get_followers
+
+      raw_followers_data.map do |follower|
+        Follower.new(follower)
+      end
+    end
+  end
+
+  def followings
+    @followings ||= begin
+      raw_following_data = @git_service.get_followings
+
+      raw_following_data.map do |following|
+        Following.new(following)
+      end
+    end
+  end
+end
