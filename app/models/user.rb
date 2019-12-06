b2b0e26a92e4c41e53dc336d68728f189b87d091
David Lopez <ldavid.x.lopezl@gmail.com>
2019-12-06 15:29:40 -0700

    Add method for ordered videos hash
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,21 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  
+
   validates_presence_of :first_name,
                         :last_name,
                         :role
 
   enum role: [:default, :admin]
   has_secure_password
+
+  def order_videos
+    videos_ordered = videos.joins(:tutorial).group('tutorials.id, videos.id').order('tutorials.id', 'videos.position')
+    videos_hash = Hash.new
+    videos_ordered.each do |v|
+      videos_hash[v.tutorial] ||= []
+      videos_hash[v.tutorial] << v
+    end
+    videos_hash
+  end
 end
