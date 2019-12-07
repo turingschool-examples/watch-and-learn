161ffa13d7afd5c205d52b23f5c85b4c512155f7
Tyla Devon <tyladevon@gmail.com>
2019-12-07 13:47:47 -0700

    Create routes for Omniauth process.
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'auth/github', as: 'github_login'
+  get '/auth/:github/callback', to: "github#create"
+
   namespace :api do
     namespace :v1 do
       resources :tutorials, only:[:show, :index]
