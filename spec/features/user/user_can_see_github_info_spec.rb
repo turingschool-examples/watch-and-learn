7f358ca7590f5b149ee8384c2b26b5d762c2195a
Scott Regenthal <scottregenthal@gmail.com>
2019-12-07 11:59:10 -0700

    Complete tests for github info view on user dashboard

---
+++ spec/features/user/user_can_see_github_info_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  before :each do
+    user = create(
+      :user, email: 'user_2@example.com',
+      password: 'password',
+      github_token: ENV['GITHUB_TOKEN']
+    )
+
+    visit '/'
+
+    click_on 'Sign In'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log In'
+  end
+
+  it 'can view github repos in github section on dashboard', :vcr do
+    expect(current_path).to eq('/dashboard')
+
+    within(first('.repo')) do
+      expect(page).to have_css('.name')
+    end
+  end
+
+  it 'can view github followings in github section on dashboard', :vcr do
+    expect(current_path).to eq('/dashboard')
+
+    within(first('.following')) do
+      expect(page).to have_css('.login')
+    end
+  end
+
+  it 'can view github followers in github section on dashboard', :vcr do
+    expect(current_path).to eq('/dashboard')
+
+    within(first('.followers')) do
+      expect(page).to have_css('.login')
+    end
+  end
+end
