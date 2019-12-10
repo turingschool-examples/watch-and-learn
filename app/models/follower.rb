207fe1a828e390e9a2667112049c468af1ca33eb
Brian Bower <brian@brianbower.com>
2019-12-09 22:06:14 -0700

    Add id and not_already_a_friend methods with logic

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -6,4 +6,14 @@ class Follower
     @login = attributes[:login]
     @html_url = attributes[:html_url]
   end
+
+  def id
+    User.find_by(handle: @login).id
+  end
+
+  def not_already_a_friend_of(user)
+    follower = User.find_by(handle: @login)
+    !user.friends.include?(follower)
+  end
+
 end
