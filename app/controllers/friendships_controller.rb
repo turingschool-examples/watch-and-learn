9b952a985ad5bda96c9aea7caee96d9bd3e010bd
Brian Bower <brian@brianbower.com>
2019-12-09 22:06:28 -0700

    Change flash notice to flash success

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,7 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
-      flash[:notice] = 'Added Friend!'
+      flash[:success] = 'Added Friend!'
       redirect_to dashboard_path
     else
       flash[:error] = 'Failed to Add Friend!'
