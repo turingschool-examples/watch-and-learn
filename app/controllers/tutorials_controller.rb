a6d414a9651d831ff7ea0bc75f72dd588c2347fa
David Lopez <ldavid.x.lopezl@gmail.com>
2019-12-10 12:02:25 -0700

    Add alternate actions for logged in users
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,12 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if current_user && tutorial.classroom
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    elsif !tutorial.classroom
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    else
+      render file: '/public/404'
+    end
   end
 end
