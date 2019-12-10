1296c96c6a22315391a6466806d5a16e8a1c1742
Zac Isaacson <isaacson.zm@gmail.com>
2019-12-10 14:47:55 -0700

    Add activated migration to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_215359) do
+ActiveRecord::Schema.define(version: 2019_12_10_143634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2019_12_05_215359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "github_token"
+    t.string "handle"
+    t.boolean "activated?", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
