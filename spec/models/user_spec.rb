29bbc69fb9dddc0fc459964f5aced486cb3e94c7
Scott Regenthal <scottregenthal@gmail.com>
2019-12-11 19:31:22 -0700

    Add relationship, validation and instance method tests to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { should have_many(:user_videos) }
+    it { should have_many(:videos).through(:user_videos) }
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:first_name) }
   end
 
@@ -19,6 +27,17 @@ RSpec.describe User, type: :model do
 
       expect(admin.role).to eq('admin')
       expect(admin.admin?).to be_truthy
+    end
+  end
+
+  describe 'instance methods' do
+    it '.status' do
+      user = create(:user)
+      user2 = create(:user, activated?: true)
+
+      expect(user.status).to eq('Inactive')
+
+      expect(user2.status).to eq('Active')
     end
   end
 end
