da2c9854fcf50d5eb6a0bf3657d7f51a3ac5ec4d
Laura Schulz <lrs8810@gmail.com>
2019-12-11 18:50:43 -0700

    Add relationship and validation testing
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- spec/models/user_video_spec.rb
+++ spec/models/user_video_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserVideo, type: :model do
+  describe 'relationships' do
+    it { should belong_to :video }
+    it { should belong_to :user }
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,10 @@ describe Video do
   describe 'validations' do
     it { should validate_presence_of :position }
   end
+
+  describe 'relationships' do
+    it { should have_many :user_videos }
+    it { should have_many(:users).through(:user_videos) }
+    it { should belong_to :tutorial }
+  end
 end
