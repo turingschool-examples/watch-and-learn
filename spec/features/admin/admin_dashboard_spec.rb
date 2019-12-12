b9094bfe1c3afee7fb54afe3716a81ba92bc87e0
Laura Schulz <lrs8810@gmail.com>
2019-12-11 20:34:59 -0700

    Add test only admin can access admin dashboard
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -11,4 +11,14 @@ feature "An admin visiting the admin dashboard" do
 
     expect(page).to have_css(".admin-tutorial-card", count: 2)
   end
+
+  scenario 'only user role to access admin dashboard' do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/admin/dashboard'
+    
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+  end
 end
