9dbd03b822915dbd7e5cbcf9b571ac096f3d818c
Laura Schulz <lrs8810@gmail.com>
2019-12-11 20:32:40 -0700

    Add test for registered users able to see classroom tutorials
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- spec/features/visitors/visitors_cannot_see_classroom_content_spec.rb
+++ spec/features/visitors/visitors_cannot_see_classroom_content_spec.rb
@@ -22,4 +22,26 @@ describe 'Tutorial index page' do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end
   end
+
+  describe 'as a registered user' do
+    it 'I can see classroom tutorials' do
+      tutorial = create(:tutorial)
+      tutorial_2 = create(:tutorial, classroom: true)
+      tutorial_3 = create(:tutorial, classroom: true)
+
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      expect(page).to have_content(tutorial.title)
+      expect(page).to have_content(tutorial_2.title)
+      expect(page).to have_content(tutorial_3.title)
+
+      visit "/tutorials/#{tutorial_2.id}"
+
+      expect(page).to have_content('No videos currently')
+    end
+  end
 end
