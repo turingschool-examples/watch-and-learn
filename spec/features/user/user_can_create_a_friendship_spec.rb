f3d09a6be1f9a3eae798fc7e39748aacb2b3c407
Scott Regenthal <scottregenthal@gmail.com>
2019-12-12 11:33:12 -0700

    Add test for user to create friendships

--- spec/features/user/user_can_create_a_friendship_spec.rb
+++ spec/features/user/user_can_create_a_friendship_spec.rb
@@ -1,7 +1,62 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'As a user' do
-#   it 'can create a friendship' do
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'A registered user' do
+  it 'can add as a friend a github following who has an account', :vcr do
+    user = create(:user, activated?: true, github_token: ENV['GITHUB_TOKEN_1'])
+    user_2 = create(:user, handle: 'freeheeling', activated?: true)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within('.github-following') do
+      click_button('Add as Friend')
+    end
+
+    expect(page).to have_content('Added Friend!')
+
+    user.reload
+
+    visit dashboard_path
+
+    within('.github-following') do
+      expect(page).to_not have_button('Add as Friend')
+    end
+  end
+
+  it 'can add as a friend a github follower who has an account', :vcr do
+    user = create(:user, activated?: true, github_token: ENV['GITHUB_TOKEN_1'])
+    user_2 = create(:user, handle: 'freeheeling', activated?: true)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within('.github-followers') do
+      click_button('Add as Friend')
+    end
+
+    expect(page).to have_content('Added Friend!')
+
+    user.reload
+
+    visit dashboard_path
+
+    within('.github-followers') do
+      expect(page).to_not have_button('Add as Friend')
+    end
+  end
+
+  it 'cannot add a github follower/following as a friend if the user does not have an account', :vcr do
+    user = create(:user, activated?: true, github_token: ENV['GITHUB_TOKEN_1'])
+    user_2 = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within('.github-followers') do
+      expect(page).to_not have_button('Add as Friend')
+    end
+  end
+end
