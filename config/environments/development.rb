d2a44e51c30d35da943ec2dbb865a6f3e6775881
Becky Robran <rerobran@gmail.com>
2020-02-05 16:22:10 -0700

    environment updates
    Co-Authored-By: null <darren2802@users.noreply.github.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -60,4 +60,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.webpacker.check_yarn_integrity = true
 end
