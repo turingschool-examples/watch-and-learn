287b27f533a45b5863c9a14d3b96879ac91875fa
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-05 11:54:01 -0700

    Add method test for github_repos to User model test
    Co-Authored-By: null <msimon42@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,4 +22,31 @@ RSpec.describe User, type: :model do
       expect(admin.admin?).to be_truthy
     end
   end
+
+  describe 'methods' do
+    describe 'instance methods' do
+      describe 'github_repos' do
+        it "returns repo objects for first five repos associated with user's github token", :vcr do
+          @repo_1 = Repo.new(name: 'activerecord-obstacle-course', url: "https://github.com/DanielEFrampton/activerecord-obstacle-course")
+          @repo_2 = Repo.new(name: 'adopt_dont_shop', url: "https://github.com/DanielEFrampton/adopt_dont_shop")
+          @repo_3 = Repo.new(name: 'adopt_dont_shop_paired', url: "https://github.com/DanielEFrampton/adopt_dont_shop_paired")
+          @repo_4 = Repo.new(name: 'backend-curriculum-site', url: "https://github.com/DanielEFrampton/backend-curriculum-site")
+          @repo_5 = Repo.new(name: 'backend_module_0_capstone', url: "https://github.com/DanielEFrampton/backend_module_0_capstone")
+
+          expected = [@repo_1, @repo_2, @repo_3, @repo_4, @repo_5]
+          user = create(:user, github_token: ENV['GITHUB_TOKEN'])
+
+          actual = user.github_repos
+          expect(actual.class).to eq(Array)
+          expect(actual.length).to eq(5)
+
+          actual.each_with_index do |repo, index|
+            expect(repo.class).to eq(Repo)
+            expect(repo.name).to eq(expected[index].name)
+            expect(repo.url).to eq(expected[index].url)
+          end
+        end
+      end
+    end
+  end
 end
