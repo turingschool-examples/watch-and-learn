e792e912195fb3e0b3204ec26a5636835d4fdb95
Tyla Devon <tyladevon@gmail.com>
2020-02-05 12:45:34 -0700

    Refactor Github Service as per Rubocop suggestion and mapping over only 5 repos rather than all.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,24 +4,15 @@ class GithubService
   end
   def conn
     Faraday.new(url: "https://api.github.com") do |f|
-      f.adapter  Faraday.default_adapter
+      f.adapter Faraday.default_adapter
       f.params[:access_token] = @user.github_token
     end
-
   end
-  def get_repos
+  def repos
     response = conn.get("user/repos")
-    repos = JSON.parse(response.body, symbolize_names: true)
-    repos.map do |data|
-      lol = GithubInfo.new(data[:name], data[:html_url])
+    repo_data = JSON.parse(response.body, symbolize_names: true)
+    repo_data[0..4].map do |data|
+      GithubInfo.new(data[:name], data[:html_url])
     end
-  end
-end
-
-class GithubInfo
-  attr_reader :name, :html
-  def initialize(name, url)
-    @name = name
-    @html = url
   end
 end
