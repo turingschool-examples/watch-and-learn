2fa37de9e9a38c674464e4f22899266ed7d840e6
Tyla Devon <tyladevon@gmail.com>
2020-02-05 13:03:36 -0700

    Refactor for whitespace

--- spec/features/user/user_can_see_github_section_spec.rb
+++ spec/features/user/user_can_see_github_section_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "When visting the dashboard as a logged in User " do
+describe "When visting the dashboard as a logged in User" do
   it "can see a section for 'Github' with 5 repo's", :vcr do
     user = create(:user, github_token: ENV['GITHUB_ACCESS_TOKEN'])
 
