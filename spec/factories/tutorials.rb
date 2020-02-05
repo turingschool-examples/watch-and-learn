06d378ed94957c1f93546bf5f52f854f30499d44
Harrison Levin <harrison483@gmail.com>
2020-02-04 18:11:53 -0700

    set_up

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -1,7 +1,9 @@
+require 'faker'
+
 FactoryBot.define do
   factory :tutorial do
     title { Faker::Name.unique.name }
-    description { Faker::HitchhikersGuideToTheGalaxy.marvin_quote }
+    description { Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote }
     thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
     playlist_id { Faker::Crypto.md5 }
     classroom { false }
