01a4a114acf443c540183181520084dc8e761a42
Becky Robran <rerobran@gmail.com>
2020-02-05 18:49:25 -0700

    updated access token so no longer visible
    Co-Authored-By: null <darren2802@users.noreply.github.com>

--- spec/features/user/user_can_see_github_repos_on_dashboard_spec.rb
+++ spec/features/user/user_can_see_github_repos_on_dashboard_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 describe "As a user" do
   it "can see github repos on dashboard" do
-  user = create(:user, token: ENV['GITHUB_TOKEN_LOCAL'])
+  token = ENV["GITHUB_TOKEN_LOCAL"]
+  user = create(:user, token: token)
+
   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
   response = File.read('spec/fixtures/user_github_repos.json')
-  stub_request(:get, "https://api.github.com/user/repos?access_token=0fe02a19242b3e280653bfc5fd3e1ecb567966ba").
+  stub_request(:get, "https://api.github.com/user/repos?access_token=#{user.token}").
   to_return(status: 200, body: response)
   response_hash = JSON.parse(response, symbolize_names: true)
+
   repos = response_hash.map do |data|
     Repo.new(data)
   end
-
+  repos.sample(5)
     visit dashboard_path
 
     expect(current_path).to eq(dashboard_path)
