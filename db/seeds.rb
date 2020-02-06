52a7faf0b9315b2736266f60a3bd4b9a2ed24bdb
Joshua O'bannon <jtobannon@gmail.com>
2020-02-06 14:18:07 -0700

    Update seeds file, include destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Tutorial.destroy_all
+Video.destroy_all
+User.destroy_all
+UserVideo.destroy_all 
+
 prework_tutorial_data = {
   "title"=>"Back End Engineering - Prework",
   "description"=>"Videos for prework.",
