9bb54de2b59521099083adae85afa80bb4c21c38
Becky Robran <rerobran@gmail.com>
2020-02-06 14:52:16 -0700

    Environment Changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data("<GITHUB_TOKEN_LOCAL>") { ENV['GITHUB_TOKEN_LOCAL'] }
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
 end
 
