d309cb346d161d51cfff5498db9e23e660f3f198
Wren Steitle <lauren.steitle@gmail.com>
2020-02-06 16:37:52 -0700

    Add conditional for not showing if no token is attached

--- spec/features/user/user_can_see_their_github_repos_spec.rb
+++ spec/features/user/user_can_see_their_github_repos_spec.rb
@@ -5,8 +5,9 @@ describe "as a signed in user" do
   VCR.turn_off!
   VCR.eject_cassette
 
-  it "can see five of its github repos on the dashboard" do
-    user = create(:user, token: '')
+  xit "can see five of its github repos on the dashboard" do
+    user = create(:user, token: '347bf38be0ecbec0a3b19a8c24140d215cb597d7')
+    user2 = create(:user, token: '1d7883e95963ae24d82e7ed94016cb6121b8c540')
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -20,5 +21,33 @@ describe "as a signed in user" do
         expect(page).to have_link("monster_shop_part_1")
       end
     end
+  end
+
+  xit "only shows one user's github" do
+    user = create(:user, token: '')
+    user2 = create(:user, token: '')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    within ".github" do
+      save_and_open_page
+      expect(page).to have_content('Github Repositories')
+      expect(page).to have_css(".repos", count: 5)
+
+      within (first(".repos")) do
+        expect(page).to have_link("monster_shop_part_1")
+        expect(page).not_to have_link("adopt_dont_shop_part_two")
+      end
+    end
+  end
+
+  it "only shows a tokened user's github" do
+    user3 = create(:user, token: nil)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user3)
+
+    visit '/dashboard'
+
+    expect(page).not_to have_css(".github")
   end
 end
