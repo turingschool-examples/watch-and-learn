4b34640f5eee4b1b58b132a1575e2be0c6353ae5
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-06 15:26:42 -0700

    Add filter sensitive data setting for GitHub access token for VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<GITHUB_TOKEN>") { ENV['GITHUB_TOKEN'] }
 end
 
 
