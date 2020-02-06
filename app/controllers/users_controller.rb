b10b3205334e4a17cf75a6c3ed48b49c998623c9
Tyla Devon <tyladevon@gmail.com>
2020-02-06 15:58:35 -0700

    Add functionality of User Followers to Service and refactor
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     render locals: {
-      user_repo: GithubService.new(current_user)
+      user_git: GithubService.new(current_user)
     }
   end
 
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,17 +2,28 @@ class GithubService
   def initialize(user)
     @user = user
   end
-  def conn
-    Faraday.new(url: "https://api.github.com") do |f|
-      f.adapter Faraday.default_adapter
-      f.params[:access_token] = @user.github_token
-    end
-  end
+
   def repos
     response = conn.get("user/repos")
     repo_data = JSON.parse(response.body, symbolize_names: true)
     repo_data[0..4].map do |data|
-      GithubInfo.new(data[:name], data[:html_url])
+      GithubInfo.new(data)
+    end
+  end
+
+  def followers
+    response = conn.get("user/followers")
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+    raw_data.map do |data|
+      Follower.new(data)
+    end
+  end
+
+  private
+  def conn
+    Faraday.new(url: "https://api.github.com") do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:access_token] = @user.github_token
     end
   end
 end
