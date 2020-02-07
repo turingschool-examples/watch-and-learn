e87d06b5a74d7cd3171bcc1c29a189e81a82f9fb
Becky Robran <rerobran@gmail.com>
2020-02-06 21:17:10 -0700

    Adds helper method for url params

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,16 @@
 class GithubService
-  def get_json_repos(token)
-    response = Faraday.get("https://api.github.com/user/repos?access_token=#{token}")
+  def get_json(url)
+    response = Faraday.get("https://api.github.com#{url}")
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def repo_url(token)
+    url = "/user/repos?access_token=#{token}"
+    get_json(url)
+  end
+
+  def following_url(token)
+    url = "/user/following?access_token=#{token}"
+    get_json(url)
   end
 end
