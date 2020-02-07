2b1eb974db4ea75b6baccd95ff649586df106774
Becky Robran <rerobran@gmail.com>
2020-02-06 19:38:35 -0700

    Updates testing
    Co-Authored-By: null <darren2802@users.noreply.github.com>

--- spec/features/user/user_can_see_github_repos_on_dashboard_spec.rb
+++ spec/features/user/user_can_see_github_repos_on_dashboard_spec.rb
@@ -6,19 +6,19 @@ describe "As a user can see github repos" do
   user = create(:user, token: token)
 
   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #
-  # response = File.read('spec/fixtures/user_github_repos.json')
-  # stub_request(:get, "https://api.github.com/user/repos?access_token=#{user.token}").
-  # to_return(status: 200, body: response)
-  # response_hash = JSON.parse(response, symbolize_names: true)
-  #
-  # repos = response_hash.map do |data|
-  #   Repo.new(data)
-  # end
-  # repos.sample(5)
+
     visit dashboard_path
 
     expect(current_path).to eq(dashboard_path)
 
+    expect(page).to have_css(".repos", count: 5)
+
+    within(first(".repos")) do
+      expect(page).to have_css(".name")
+    end
+
+    click_on "Log Out"
+
+    expect(page).not_to have_css(".repos")
   end
 end
