f10516b8a3253489d14a03c0e8e7669c460184a3
Becky Robran <rerobran@gmail.com>
2020-02-06 19:38:09 -0700

    adds method to only display five repos
    Co-Authored-By: null <darren2802@users.noreply.github.com>

--- app/facades/repo_search.rb
+++ app/facades/repo_search.rb
@@ -4,6 +4,10 @@ class RepoSearch
   end
 
   def display_repos
+    find_repos.take(5)
+  end
+
+  def find_repos
       service = GithubService.new
       service.get_json_repos(@token).map do |data|
         Repo.new(data)
