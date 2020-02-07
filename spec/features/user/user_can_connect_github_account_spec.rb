4a18de88711e1159de14365eec0034c906577f1c
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-07 15:55:54 -0700

    Refactor test to avoid changing request.env for all tests

--- spec/features/user/user_can_connect_github_account_spec.rb
+++ spec/features/user/user_can_connect_github_account_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'As a user not yet connected to GitHub', type: :feature do
-  before(:each) do
+RSpec.describe 'As a user not yet connected to GitHub', :vcr, type: :feature do
+  before :each do
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
     @user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
   end
 
   describe 'When I visit /dashboard' do
@@ -22,7 +22,7 @@ RSpec.describe 'As a user not yet connected to GitHub', type: :feature do
       expect(page).to_not have_content('Your GitHub Repos')
     end
 
-    describe 'And when I click on "Connect to Github"', :vcr do
+    describe 'And when I click on "Connect to Github"' do
       before(:each) do
         click_on 'Connect to GitHub'
       end
@@ -37,5 +37,9 @@ RSpec.describe 'As a user not yet connected to GitHub', type: :feature do
         expect(page).to have_css('h2', text: "Your GitHub Repos")
       end
     end
+  end
+
+  after do
+    Rails.application.env_config["omniauth.auth"] = nil
   end
 end
