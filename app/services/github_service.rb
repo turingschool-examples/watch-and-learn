2227d4fe05a327ef3b3a2227eab182d4429d5aca
Becky Robran <rerobran@gmail.com>
2020-02-06 17:06:28 -0700

    Refactors logic out of github service class
    Co-Authored-By: null <darren2802@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,12 +1,6 @@
 class GithubService
-  def get_json_repos(user)
-    response = Faraday.get("https://api.github.com/user/repos?access_token=#{user.token}")
+  def get_json_repos(token)
+    response = Faraday.get("https://api.github.com/user/repos?access_token=#{token}")
     JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def find_repos(user)
-    get_json_repos(user).map do |data|
-      Repo.new(data)
-    end
   end
 end
