a7bc473c313287486bb26641fe085c2823e440bb
Harrison Levin <harrison483@gmail.com>
2020-02-07 13:14:33 -0700

    github controller

---
+++ app/controllers/github_controller.rb
@@ -0,0 +1,14 @@
+class GithubController < ApplicationController
+
+  def create
+     current_user.update_token(auth_hash)
+     redirect_to '/dashboard'
+   end
+
+   private
+
+   def auth_hash
+     request.env['omniauth.auth']
+   end
+
+end
