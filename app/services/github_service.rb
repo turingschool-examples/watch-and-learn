93af9e135fa607ec9a98e9b87a4be4e670a19733
Becky Robran <rerobran@gmail.com>
2020-02-07 09:54:02 -0700

    updates github service to make code more reusable
    Co-Authored-By: null <darren2802@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,13 +4,8 @@ class GithubService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def repo_url(token)
-    url = "/user/repos?access_token=#{token}"
-    get_json(url)
-  end
-
-  def following_url(token)
-    url = "/user/following?access_token=#{token}"
+  def user_url_path(path, token)
+    url = "/user/#{path}?access_token=#{token}"
     get_json(url)
   end
 end
