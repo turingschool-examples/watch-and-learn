d0b48660663e6b008be59662a407215119c88cce
Wren Steitle <lauren.steitle@gmail.com>
2020-02-07 23:40:09 -0700

    add classroom tutorial factory for testing

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     playlist_id { Faker::Crypto.md5 }
     classroom { false }
   end
+
+  factory :class_tutorial, class: Tutorial do
+    title { Faker::Name.unique.name }
+    description { Faker::HitchhikersGuideToTheGalaxy.marvin_quote }
+    thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
+    playlist_id { Faker::Crypto.md5 }
+    classroom { true }
+  end
 end
