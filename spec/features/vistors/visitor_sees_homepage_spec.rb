72d41866b574b2fdf43761346b2027b8370dcfcb
Wren Steitle <lauren.steitle@gmail.com>
2020-02-07 23:43:12 -0700

    Add additional check that when looged in videos show correctly

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -45,6 +45,19 @@ describe 'Visitor' do
         expect(page).to have_content(tutorial2.title)
         expect(page).to have_content(tutorial2.description)
       end
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 2)
+
+      within(first('.tutorials')) do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+      end
     end
   end
 end
