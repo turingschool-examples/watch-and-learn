dbd3740c3a6a8ac6398da7e8600b5222c553a77a
Wren Steitle <lauren.steitle@gmail.com>
2020-02-07 23:39:50 -0700

    Add test that tutorials marked as classroom only show when logged in

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -23,4 +23,28 @@ describe 'Visitor' do
       end
     end
   end
+
+  describe 'on the home page' do
+    it 'cant see tutorials marked classroom' do
+      tutorial1 = create(:class_tutorial)
+      tutorial2 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+      expect(page).to have_css('.tutorial', count: 1)
+
+      within(first('.tutorials')) do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).not_to have_content(tutorial1.title)
+        expect(page).not_to have_content(tutorial1.description)
+        expect(page).to have_content(tutorial2.title)
+        expect(page).to have_content(tutorial2.description)
+      end
+    end
+  end
 end
