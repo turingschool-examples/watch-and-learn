66700a14f4856f8843d9d4fad9856db2e1baacc5
Madelyn Romero <m.romero013@gmail.com>
2020-02-08 10:24:39 -0700

    Create test for seeing followers on dashboard

--- spec/features/user/user_can_see_their_github_followers_on_dashboard_spec.rb
+++ spec/features/user/user_can_see_their_github_followers_on_dashboard_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-describe "as a logged in user" do
-  xit "can see its github followers" do
+describe "as a logged in user", :vcr do
+  it "can see its github followers" do
     user = create(:user, token: ENV['GITHUB_TOKEN'])
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/dashboard'
 
-    within "#github" do
+    within ".github" do
       expect(page).to have_content("Followers")
-      expect(page).to have_css("#followers", count: 3)
+      expect(page).to have_css(".followers", count: 3)
     end
 
-    within "#followers" do
-      expect(page).to have_css("#f")
+    within first(".followers") do
+      expect(page).to have_link("jfangonilo")
     end
-
   end
 end
