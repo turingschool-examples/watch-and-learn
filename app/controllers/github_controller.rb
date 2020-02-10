b819b69a3b6c1cdfe29fe214c74df7eda723a95c
Wren Steitle <lauren.steitle@gmail.com>
2020-02-10 11:48:09 -0700

    Remove hand rolled code
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,20 +1,6 @@
 class GithubController < ApplicationController
 
   def create
-    #
-    # client_id     = ENV['RAILS_CLIENT_ID']
-    # client_secret = ENV['RAILS_CLIENT_SECRET']
-    # code          = params[:code]
-    # response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
-    #
-    # pairs = response.body.split("&")
-    # response_hash = {}
-    # pairs.each do |pair|
-    #   key, value = pair.split("=")
-    #   response_hash[key] = value
-    # end
-    #
-    # github_token = response_hash["access_token"]
     user = User.find(current_user.id)
     user.update(user_hash)
     redirect_to dashboard_path
@@ -27,7 +13,6 @@ class GithubController < ApplicationController
   end
 
   def user_hash
-    # {uid: auth_hash["uid"], token: auth_hash["credentials"]["token"]}
     {token: auth_hash["credentials"]["token"]}
   end
 end
