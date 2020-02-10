f02df26e9e0cc9e23226f7adf5a62013af483e3d
Becky Robran <rerobran@gmail.com>
2020-02-10 15:14:33 -0700

    updates user email

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def create
-    ActivationNotifier.inform(current_user, params[:email]).deliver_now
+    ActivationNotifierMailer.inform(current_user, params[:email]).deliver_now
     flash[:notice] = "An email has been sent to your account."
     redirect_to dashboard_path
   end
