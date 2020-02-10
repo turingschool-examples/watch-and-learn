12fa46086f557750a4c182de6dd65cc135828d4c
Becky Robran <rerobran@gmail.com>
2020-02-10 14:26:49 -0700

    adds active columnn to users with default of false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_222018) do
+ActiveRecord::Schema.define(version: 2020_02_10_203940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_222018) do
     t.datetime "updated_at", null: false
     t.string "token"
     t.string "uid"
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
