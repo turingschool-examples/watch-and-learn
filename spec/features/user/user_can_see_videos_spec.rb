5abb2d1547de547a2f3d9f8d72cf90d66fd28593
David Gomez <davdg25@gmail.com>
2020-02-10 12:30:11 -0700

    create test and functionality for issue 10

---
+++ spec/features/user/user_can_see_videos_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Tutorials users can see' do
+  it 'A user not logged in can only seed tutorials where classroom.tutorial == false ' do
+    tutorial_1= create(:tutorial, title: "How to Tie Your Shoes")
+    tutorial_2= create(:tutorial, title: "This was has classroom = true", classroom: true)
+    video = create(:video, title: "The Bunny Ears Technique", tutorial: tutorial_1)
+    user = create(:user)
+
+    visit '/'
+
+    expect(page).to have_content(tutorial_1.title)
+    expect(page).to_not have_content(tutorial_2.title)
+  end
+end
