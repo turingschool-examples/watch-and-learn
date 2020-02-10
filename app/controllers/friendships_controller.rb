e24c91c879c731bfd7f6490eb62e061099bb0f09
msimon42 <msimon@uab.edu>
2020-02-10 15:45:43 -0700

    Change logic in controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(github_id: params[:github_id])
-    Friendship.create(user: current_user, friend_id: friend.id)
+    current_user.friends << friend
     redirect_to '/dashboard'
     flash[:notice] = 'You have a friend.'
   end
