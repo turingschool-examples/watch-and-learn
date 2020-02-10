c4576a894ed390776594b70f3e76e6038fbdf8a6
Harrison Levin <harrison483@gmail.com>
2020-02-10 12:19:52 -0700

    validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,7 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  validates_presence_of :position
+  validates_numericality_of :position, greater_than_or_equal_to: 0
 end
