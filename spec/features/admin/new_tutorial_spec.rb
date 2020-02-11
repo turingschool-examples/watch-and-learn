a44d2d00d69aaa3c9b4b75ece5015119753ff3bf
David Gomez <davdg25@gmail.com>
2020-02-11 08:51:39 -0700

    add sad path testing

--- spec/features/admin/new_tutorial_spec.rb
+++ spec/features/admin/new_tutorial_spec.rb
@@ -17,7 +17,22 @@ RSpec.describe 'An Admin can ...' do
 
     tutorial = Tutorial.last
 
-    expect(current_path).to eq(tutorial_path(tutorial))
     expect(page).to have_content('Successfully created tutorial.')
+    expect(current_path).to eq(tutorial_path(tutorial))
+  end
+
+  it 'test fields must be entered' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit admin_dashboard_path
+    click_on 'New Tutorial'
+
+    fill_in  'Title', with: ''
+    fill_in 'Description', with: ''
+    fill_in 'Thumbnail', with: ''
+    click_on 'Save'
+
+    expect(page).to have_content("Title can't be blank, Description can't be blank, and Thumbnail can't be blank")
+    expect(current_path).to eq(admin_tutorials_path)
   end
 end
