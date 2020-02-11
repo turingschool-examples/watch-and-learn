94eb3043bdedb7cd894f145627796e924b340034
Becky Robran <rerobran@gmail.com>
2020-02-10 20:36:58 -0700

    adds email local host path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
+    config.action_mailer.default_url_options = {:host => "localhost:3000"}
     config.cache_store = :null_store
   end
 
