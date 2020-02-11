24c57a1cff493746a9aec6bf961121753fcca07c
Madelyn Romero <m.romero013@gmail.com>
2020-02-11 08:33:51 -0700

    Delete commented out code

--- app/poros/github_data_view.rb
+++ app/poros/github_data_view.rb
@@ -1,84 +1,31 @@
 class GithubDataView
 
-  attr_reader :token
+# Make private
+  attr_reader :user
 
-  def initialize(user_token)
-    @token = user_token
+  def initialize(user)
+    @user = user
   end
 
   def repos
-    repo_hash.map do |repo_data|
-        Repo.new(repo_data)
-      end
-  end
-
-  def repo_hash
-    conn = Faraday.new(url: 'https://api.github.com') do |f|
-        f.headers['Authorization'] = "token #{@token}"
-        f.adapter Faraday.default_adapter
-      end
-
-    repo_response = conn.get('/user/repos')
-    JSON.parse(repo_response.body, symbolize_names: true)[0..4]
+    service = GithubService.new
+    service.repos_by_user(user).map do |repo_data|
+      Repo.new(repo_data)
+    end
   end
 
   def followers
-    follower_hash.map do |follower_data|
+    service = GithubService.new
+    service.followers_by_user(user).map do |follower_data|
       Follower.new(follower_data)
     end
   end
 
-  def follower_hash
-    conn = Faraday.new(url: 'https://api.github.com') do |f|
-      f.headers['Authorization'] = "token #{@token}"
-      f.adapter Faraday.default_adapter
-    end
-
-    follower_response = conn.get('/user/followers')
-    JSON.parse(follower_response.body, symbolize_names: true)
-  end
-
   def following
-    following_hash.map do |following_data|
+    service = GithubService.new
+    service.following_by_user(user).map do |following_data|
       Following.new(following_data)
     end
   end
-
-  def following_hash
-    conn = Faraday.new(url: 'https://api.github.com') do |f|
-      f.headers['Authorization'] = "token #{@token}"
-      f.adapter Faraday.default_adapter
-    end
-
-    following_resp = conn.get('/user/following')
-    JSON.parse(following_resp.body, symbolize_names: true)
-  end
-
-
-
-
-  #   conn = Faraday.new(url: 'https://api.github.com') do |f|
-  #     f.headers['Authorization'] = "token #{user.token}"
-  #     f.adapter Faraday.default_adapter
-  #   end
-  #
-  #   repo_response = conn.get('/user/repos')
-  #   repo_hash = JSON.parse(repo_response.body, symbolize_names: true)[0..4]
-  #   @repos = repo_hash.map do |repo_data|
-  #     Repo.new(repo_data)
-  #   end
-  #
-  #   follower_response = conn.get('/user/followers')
-  #   follower_hash = JSON.parse(follower_response.body, symbolize_names: true)
-  #   @followers = follower_hash.map do |follower_data|
-  #     Follower.new(follower_data)
-  #   end
-  #
-  #   following_resp = conn.get('/user/following')
-  #   following_hash = JSON.parse(following_resp.body, symbolize_names: true)
-  #   @following = following_hash.map do |following_data|
-  #     Following.new(following_data)
-  #   end
-
 
 end
