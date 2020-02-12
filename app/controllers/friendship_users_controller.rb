5622ed207b17120e76ae3159d8637efe1c4684b9
Becky Robran <rerobran@gmail.com>
2020-02-11 17:00:04 -0700

    updates friendship_users_cotroller with joins
    Co-Authored-By: null <darren2802@gmail.com>

--- app/controllers/friendship_users_controller.rb
+++ app/controllers/friendship_users_controller.rb
@@ -2,5 +2,9 @@ class FriendshipUsersController < ApplicationController
   def create
     user = current_user
     friend = User.find_by(uid: params["format"])
+    user.frienders << friend
+
+    flash[:success] = "You have added a friend."
+    redirect_to dashboard_path
   end
 end
