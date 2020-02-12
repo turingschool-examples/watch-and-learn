df3577b6e88e216f3028afd4742769f380195a45
Becky Robran <rerobran@gmail.com>
2020-02-12 16:07:28 -0700

    adds sad path

--- app/controllers/friendship_users_controller.rb
+++ app/controllers/friendship_users_controller.rb
@@ -2,9 +2,12 @@ class FriendshipUsersController < ApplicationController
   def create
     user = current_user
     friend = User.find_by(uid: params["format"])
-    user.frienders << friend
-
-    flash[:success] = "You have added a friend."
+    if friend
+      user.frienders << friend
+      flash[:success] = "You have added a friend."
+    else
+      flash[:error] = "Sorry we were not able to find your friend."
+    end 
     redirect_to dashboard_path
   end
 end
