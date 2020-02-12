f4649251f0a4c62a70a8c1bad71f3dddac644e38
Madelyn Romero <m.romero013@gmail.com>
2020-02-11 17:12:25 -0700

    Delete abstracted out code and turn local variable to instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,11 @@
 class UsersController < ApplicationController
+
   def show
-    user = User.find(current_user.id)
-    if user.token
-
+    @user = User.find(current_user.id)
+    if @user.token
       render locals: {
-        search_results: GithubDataView.new(user)
+        search_results: GithubDataView.new(@user)
       }
-
-      # conn = Faraday.new(url: 'https://api.github.com') do |f|
-      #   f.headers['Authorization'] = "token #{user.token}"
-      #   f.adapter Faraday.default_adapter
-      # end
-      #
-      # repo_response = conn.get('/user/repos')
-      # repo_hash = JSON.parse(repo_response.body, symbolize_names: true)[0..4]
-      # @repos = repo_hash.map do |repo_data|
-      #   Repo.new(repo_data)
-      # end
-      #
-      # follower_response = conn.get('/user/followers')
-      # follower_hash = JSON.parse(follower_response.body, symbolize_names: true)
-      # @followers = follower_hash.map do |follower_data|
-      #   Follower.new(follower_data)
-      # end
-      #
-      # following_resp = conn.get('/user/following')
-      # following_hash = JSON.parse(following_resp.body, symbolize_names: true)
-      # @following = following_hash.map do |following_data|
-      #   Following.new(following_data)
-      # end
     end
   end
 
