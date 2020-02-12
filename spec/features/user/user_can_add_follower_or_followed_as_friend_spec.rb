90bf2ac84c71eab5055e0ab0eea3e5ca80f11f3c
Madelyn Romero <m.romero013@gmail.com>
2020-02-12 08:53:54 -0700

    Add test for followers not in user database to not have link

--- spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
+++ spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
@@ -24,9 +24,28 @@ describe "as a user" do
       expect(page).to have_link("jfangonilo")
       expect(page).to have_link("Add as Friend")
     end
+  end
 
-    within(second(".followers")) do
-      expect(page).to have_link("danmoran-pro")
+  it "can see a button to friend a follower and that follower will now be in the users friends", :vcr do
+    OmniAuth.config.test_mode = true
+    user_1 = create(:user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :provider => 'github',
+      :credentials => {:token => ENV['GITHUB_TOKEN']},
+      :extra => {
+        :raw_info => {
+          :login => "madelynrr"
+        }
+      }
+    })
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit '/dashboard'
+    click_link("Login Through Github")
+
+    within(first(".followers")) do
+      expect(page).to have_link("jfangonilo")
       expect(page).not_to have_link("Add as Friend")
     end
   end
