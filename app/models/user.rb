720cff2fb5f8302b896e1ae53cd1867fc1c127ce
Becky Robran <rerobran@gmail.com>
2020-02-11 17:00:30 -0700

    adds self referential joins relationship
    Co-Authored-By: null <darren2802@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,20 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  has_many :friended_users, foreign_key: :friender_id, class_name: "FriendUser"
+  has_many :friendees, through: :friended_users
+
+  has_many :friending_users, foreign_key: :friendee_id, class_name: "FriendUser"
+  has_many :frienders, through: :friending_users
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password_digest
   validates_presence_of :first_name
+
   enum role: %w[default admin]
   has_secure_password
+
+  def find_friends
+    require "pry"; binding.pry
+  end
 end
