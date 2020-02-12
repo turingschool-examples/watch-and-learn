dcc1b0bf21cfa1024dffa97211ec5cc5678a555f
Joshua O'bannon <jtobannon@gmail.com>
2020-02-12 16:03:36 -0700

    Finish User Notifier Story

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,14 @@
 class NotificationsController < ApplicationController
   def create
-    to_be_emailed_user = User.find_by(github_username: params[:github_username])
-    UserNotifierMailer.inform(current_user, to_be_emailed_user.email).deliver_now
-    flash[:notice] = "Successfully Sent Invite"
+    github_user_profile_hash = GithubService.new(current_user).get_specific_user(params[:github_username])
+    to_be_emailed_user_email = github_user_profile_hash[:email] 
+    if to_be_emailed_user_email == nil 
+      flash[:notice] = "No email present for this user"
+    elsif 
+      UserNotifierMailer.inform(current_user, to_be_emailed_user_email).deliver_now
+      flash[:notice] = "Successfully Sent Invite"
+    end
+
     redirect_to "/dashboard" 
   end
 end
