3668ee4d40d4c1392fd01067691927781b1b7600
Becky Robran <rerobran@gmail.com>
2020-02-12 16:08:30 -0700

    adds followers to testing

--- spec/features/users/user_can_friend_another_user_spec.rb
+++ spec/features/users/user_can_friend_another_user_spec.rb
@@ -10,7 +10,8 @@ describe "As a user we can go to our dashboard" do
     token = ENV["GITHUB_TOKEN_LOCAL"]
     user_1 = create(:user, token: token)
     user_2 = create(:user, uid: "37692413")
-    user_3 = create(:user)
+    user_3 = create(:user, uid: "40702808")
+    user_4 = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
     allow_any_instance_of(ApplicationController).to receive(:github_status).and_return("logged in")
@@ -25,10 +26,17 @@ describe "As a user we can go to our dashboard" do
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content('You have added a friend.')
 
+    within (".github_following") do
+      expect(page).to have_link("Friend this User")
+      click_link "Friend this User"
+    end
+
     within ('.friends') do
       expect(page).to have_content(user_2.first_name)
       expect(page).to have_content(user_2.last_name)
-      expect(page).not_to have_content(user_3.first_name)
+      expect(page).to have_content(user_3.first_name)
+      expect(page).to have_content(user_3.last_name)
+      expect(page).not_to have_content(user_4.first_name)
     end
   end
 end
