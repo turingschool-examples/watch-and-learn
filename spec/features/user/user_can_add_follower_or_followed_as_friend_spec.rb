2c7e7c779495d6f9da3a8931813f9b9c9926a9f8
Madelyn Romero <m.romero013@gmail.com>
2020-02-12 08:38:05 -0700

    Add login to github to test

--- spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
+++ spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 describe "as a user" do
   it "can see a button to friend a follower and that follower will now be in the users friends", :vcr do
-    user_1 = create(:user, token: ENV['GITHUB_TOKEN'])
+    OmniAuth.config.test_mode = true
+    user_1 = create(:user)
     user_2 = create(:user, github_handle: "jfangonilo")
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :provider => 'github',
+      :credentials => {:token => ENV['GITHUB_TOKEN']},
+      :extra => {
+        :raw_info => {
+          :login => "madelynrr"
+        }
+      }
+    })
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-    visit "/dashboard"
+    visit '/dashboard'
+    click_link("Login Through Github")
 
     within(first(".followers")) do
       expect(page).to have_link("jfangonilo")
