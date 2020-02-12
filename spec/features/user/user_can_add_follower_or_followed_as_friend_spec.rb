8aa31dfe7f53f7feeb173c67b14f401cc6ad14e3
Madelyn Romero <m.romero013@gmail.com>
2020-02-11 22:44:47 -0700

    Add vcr to test and visit dashboard

--- spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
+++ spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe "as a user" do
-  it "can see a button to friend a follower and that follower will now be in the users friends" do
+  it "can see a button to friend a follower and that follower will now be in the users friends", :vcr do
     user_1 = create(:user, token: ENV['GITHUB_TOKEN'])
     user_2 = create(:user, github_handle: "jfangonilo")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-    expect(current_path).to eq("/dashboard")
+    visit "/dashboard"
 
     within(first(".followers")) do
       expect(page).to have_link("jfangonilo")
