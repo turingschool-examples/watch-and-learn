1887536ef3086f4008a19ce9b370aeb8f6bd3f08
Madelyn Romero <m.romero013@gmail.com>
2020-02-11 20:56:26 -0700

    Create test to have button to add as friend next to other users

---
+++ spec/features/user/user_can_add_follower_or_followed_as_friend_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "as a user" do
+  it "can see a button to friend a follower and that follower will now be in the users friends" do
+    user_1 = create(:user, token: ENV['GITHUB_TOKEN'])
+    user_2 = create(:user, github_handle: "jfangonilo")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    expect(current_path).to eq("/dashboard")
+
+    within(first(".followers")) do
+      expect(page).to have_link("jfangonilo")
+      expect(page).to have_link("Add as Friend")
+    end
+
+    within(second(".followers")) do
+      expect(page).to have_link("danmoran-pro")
+      expect(page).not_to have_link("Add as Friend")
+    end
+  end
+end
