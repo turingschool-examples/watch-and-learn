3eddb6dc50bfad7ce87ae98e4dbb97ed0fdbe1d5
Dione Wilson <dionew1@gmail.com>
2020-03-12 12:41:10 -0600

    Add delete tutorial functionality for admins

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -18,6 +18,14 @@ class Admin::TutorialsController < Admin::BaseController
     redirect_to edit_admin_tutorial_path(tutorial)
   end
 
+  def destroy
+    tutorial = Tutorial.find(params[:id])
+    if tutorial.destroy
+      flash[:success] = "#{tutorial.title} tagged!"
+    end
+    redirect_to admin_dashboard_path
+  end
+
   private
   def tutorial_params
     params.require(:tutorial).permit(:tag_list)
---
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "An admin can delete a tutorial" do
+  scenario "and it should no longer exist" do
+    admin = create(:admin)
+    create_list(:tutorial, 2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/admin/dashboard"
+
+    expect(page).to have_css('.admin-tutorial-card', count: 2)
+
+    within(first('.admin-tutorial-card')) do
+      click_link 'Delete'
+    end
+
+    expect(page).to have_css('.admin-tutorial-card', count: 1)
+  end
+end
