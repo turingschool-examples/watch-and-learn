0a69c4b1288d5b6ee96214ecbbf0a099e527b8fc
Ryan Allen <rcallen89@gmail.com>
2020-03-23 19:42:53 -0600

    Initial Setup with 1 failing test

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -4,17 +4,20 @@ describe "An Admin can edit a tutorial" do
   let(:tutorial) { create(:tutorial) }
   let(:admin)    { create(:admin) }
 
-  scenario "by adding a video", :js, :vcr do
+  it "by adding a video", :js, :vcr do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit edit_admin_tutorial_path(tutorial)
 
     click_on "Add Video"
 
-    fill_in "video[title]", with: "How to tie your shoes."
-    fill_in "video[description]", with: "Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through."
-    fill_in "video[video_id]", with: "J7ikFUlkP_k"
-    click_on "Create Video"
+    fill_in :video['title'], with: "How to tie your shoes."
+    fill_in :video['description'], with: "Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through."
+    fill_in :video['video_id'], with: "J7ikFUlkP_k"
+    save_and_open_page
+    # click_on "Create Video"
+    find("#editbutton", visible: :hidden).click
+
 
     expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
 
