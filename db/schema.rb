13c40b614195103db9ea065a449f35dd9e702a7c
Jordan <jordan.wallace.williams@gmail.com>
2020-03-23 20:05:12 -0600

    Fix all initial errors. All tests pass

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2018_07_31_230036) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "taggings", id: :serial, force: :cascade do |t|
