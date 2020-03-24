28e565195dc3356050c5ff9fffc79db46db28636
Pierce Alworth <pbalworth@gmail.com>
2020-03-24 15:56:37 -0500

    Syntax change

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -4,12 +4,14 @@ describe "An Admin can edit a tutorial" do
   let(:tutorial) { create(:tutorial) }
   let(:admin)    { create(:admin) }
 
-  scenario "by adding a video", :js, :vcr do
+ vcr_options = {record: :new_episodes }
+  it "by adding a video", :js, :vcr => vcr_options do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit edit_admin_tutorial_path(tutorial)
 
     click_on "Add Video"
+    
 
     within "#new-video-form" do
       fill_in 'video_title', with: "How to tie your shoes."
@@ -19,8 +21,8 @@ describe "An Admin can edit a tutorial" do
     end
 
     expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
-
     within(first(".video")) do
+
       expect(page).to have_content("How to tie your shoes.")
     end
   end
