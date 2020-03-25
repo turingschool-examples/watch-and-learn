5c49356b4a92850f8df0ef6e59f2e8196ab4b5b8
Daniel <danmoran.pro@gmail.com>
2020-03-24 18:43:01 -0600

    Setup complete. All test passing.

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -11,7 +11,7 @@ describe "An Admin can edit a tutorial" do
 
     click_on "Add Video"
 
-    within('new-video-form') do 
+    within('#new-video-form') do 
       fill_in 'video_title', with: "How to tie your shoes."
       fill_in 'video_description', with: "Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through."
       fill_in 'video_video_id', with: "J7ikFUlkP_k"
