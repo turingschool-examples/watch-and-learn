2a290d35069dd88a950ae238ea3a6a62dfc4ee31
Daniel <danmoran.pro@gmail.com>
2020-03-25 14:45:11 -0600

    all test are passing again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<AIzaSyA3aWVtjpgzsVK0N4leuWkcx631eKk4D8s>") { ENV['AIzaSyA3aWVtjpgzsVK0N4leuWkcx631eKk4D8s'] }
 end
 
 
@@ -27,7 +27,7 @@ end
 Capybara.javascript_driver = :selenium_chrome_headless
 
 Capybara.configure do |config|
-  config.default_max_wait_time = 5
+  config.default_max_wait_time = 7
 end
 
 SimpleCov.start "rails"
