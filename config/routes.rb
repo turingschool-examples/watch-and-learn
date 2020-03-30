d538e55b6330eed55b9d9334baf1eff5557d1bb9
Daniel <danmoran.pro@gmail.com>
2020-03-30 12:28:17 -0600

    All test besides video passing

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#index'
+  get '/auth/github/callback', to: 'signup#create'
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
 
