ab3fac68b77be5ee6ae188f7c45f2861f1d04431
DeMarcus Kirby <tkd024@gmail.com>
2020-03-30 20:39:36 -0600

    Update signup to save username
    Co-Authored-By: Daniel "Danny" Moran  <danmoran-pro@users.noreply.github.com>

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,12 +1,14 @@
 class SignupController < ApplicationController
-  def create 
+  def create
     token = auth_hash['credentials']['token']
     github_id = auth_hash["uid"]
-    current_user.update_attributes(github_token: token, uid: github_id)
+    username = auth_hash["info"]["nickname"]
+    current_user.update_attributes!(github_token: token, uid: github_id, username: username)
+    current_user.save
     redirect_to dashboard_path
   end
   private
-  
+
   def auth_hash
     @auth_hash ||= request.env['omniauth.auth']
   end
