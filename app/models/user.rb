aa1347f4ec8c997114a731b493e25bdb41ceff55
Daniel <danmoran.pro@gmail.com>
2020-04-01 17:34:36 -0600

    Frindships WIP Co-Authored-By: DeMarcus Kirby <tkd024@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key:  "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
   validates_presence_of :first_name
