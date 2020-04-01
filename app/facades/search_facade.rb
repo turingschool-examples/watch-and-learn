0e0610319947a020edaed9d6ec0f168bd291ed21
DeMarcus Kirby <tkd024@gmail.com>
2020-03-31 19:11:57 -0600

    add following section to search facade
    Co-Authored-By: Daniel "Danny" Moran  <danmoran-pro@users.noreply.github.com>

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -5,20 +5,28 @@ class SearchFacade
     @user = user
     @git_service = GithubService.new(@user.github_token, @user.username)
   end
-  
+
   def repos
     data = @git_service.get_repos
     @repos = data.map do |repo|
       Repository.new(repo)
-    end 
+    end
     return @repos
-  end 
+  end
 
   def followers
     data = @git_service.get_followers
     @followers = data.map do |follower|
       Follower.new(follower)
-    end 
+    end
     return @followers
+  end
+
+  def following
+    data = @git_service.get_followings
+    @following = data.map do |following|
+      Following.new(following)
+    end
+    return @following
   end 
 end
