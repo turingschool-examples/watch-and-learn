82dddb9778e25202e4ca85b8960b8fc2b2ef96c7
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-31 22:22:43 -0600

    Add current_user method

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,4 +4,12 @@ class Admin::BaseController < ApplicationController
   def require_admin!
     four_oh_four unless current_user.admin?
   end
+
+  def current_user
+    @current_user ||= if session[:user_id]
+                        User.find(session[:user_id])
+                      else
+                        User.new
+                      end
+  end
 end
