0b9d89846f542ab12795da95120d103737812eec
Elom Amouzou <elom1990@yahoo.com>
2020-04-01 17:25:15 -0600

    add bookmarks test

--- spec/features/user/dashboard_show_spec.rb
+++ spec/features/user/dashboard_show_spec.rb
@@ -67,4 +67,59 @@ RSpec.describe 'A registered user' do
       expect(page).to have_link('dionew1', href: 'https://github.com/dionew1')
     end
   end
+
+  it 'displays a list of all bookmarked segments under the Bookmarked Segments section', :vcr do
+    user = create(:user)
+
+    tutorial1 = create(:tutorial, title: "First Tutorial")
+    video1 = create(:video, title: "First on page", tutorial_id: tutorial1.id, position: 1)
+    UserVideo.create!(user: user, video: video1)
+
+    tutorial2 = create(:tutorial, title: "Second Tutorial")
+    video2 = create(:video, title: "# 1", tutorial_id: tutorial2.id, position: 0)
+    video3 = create(:video, title: "# 2", tutorial_id: tutorial2.id, position: 1)
+    UserVideo.create!(user: user, video: video2)
+    UserVideo.create!(user: user, video: video3)
+
+    tutorial3 = create(:tutorial)
+    no_bookmark_video = create(:video, tutorial_id: tutorial3.id)
+
+    tutorial4 = create(:tutorial, title: "Third Tutorial")
+    video4 = create(:video, title: "# 2", tutorial_id: tutorial4.id, position: 1)
+    UserVideo.create!(user: user, video: video4)
+    video5 = create(:video, title: "Last on page", tutorial_id: tutorial4.id, position: 2)
+    UserVideo.create!(user: user, video: video5)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    expect(page).to have_css(".bookmarks", count: 1)
+
+    within(".bookmarks") do
+      expect(page).to have_content("Bookmarked Segments")
+      expect(page).to_not have_content(no_bookmark_video.title)
+    end
+
+    within(".bookmark-#{video1.id}") do
+      expect(page).to have_link(video1.title, href: "/tutorials/#{tutorial1.id}?video_id=#{video1.id}")
+    end
+
+    within(".bookmark-#{video2.id}") do
+      expect(page).to have_link(video2.title, href: "/tutorials/#{tutorial2.id}?video_id=#{video2.id}")
+    end
+
+    within(".bookmark-#{video3.id}") do
+      expect(page).to have_link(video3.title, href: "/tutorials/#{tutorial2.id}?video_id=#{video3.id}")
+    end
+
+    within(".bookmark-#{video4.id}") do
+      expect(page).to have_link(video4.title, href: "/tutorials/#{tutorial4.id}?video_id=#{video4.id}")
+    end
+
+    within(".bookmark-#{video5.id}") do
+      expect(page).to have_link(video5.title, href: "/tutorials/#{tutorial4.id}?video_id=#{video5.id}")
+    end
+  end
+
 end
