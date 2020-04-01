ebaf6b5bd8b77d84e45dda282dbcfc5e51c27af4
Elom Amouzou <elom1990@yahoo.com>
2020-04-01 17:25:06 -0600

    add class method, model test passes

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,4 +1,13 @@
 class UserVideo < ApplicationRecord
   belongs_to :video, foreign_key: "video_id"
   belongs_to :user, foreign_key: "user_id"
+
+  def self.get_bookmarked_video_info(id)
+    joins(video: :tutorial)
+    .select('videos.title, videos.id AS video_id, tutorials.id AS tutorial_id')
+    .select('tutorials.title AS tutorial_title, videos.position AS video_position')
+    .where(user_id: id)
+    .order(:tutorial_title)
+    .order(:video_position)
+  end
 end
