9ff84c217d1ecea41ce4a477b40065a6ca1117df
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-31 22:23:05 -0600

    Add Sessions Controller with update action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
     @user ||= User.new
@@ -5,13 +7,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
-      flash[:error] = "Looks like your email or password is invalid"
+      flash[:error] = 'Looks like your email or password is invalid'
       render :new
     end
+  end
+
+  def update
+    current_user.update(github_token: auth_github_token, github_username: auth_github_username)
+    redirect_to '/dashboard'
   end
 
   def destroy
@@ -19,4 +26,13 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  private
+
+  def auth_github_token
+    request.env['omniauth.auth']['credentials']['token']
+  end
+
+  def auth_github_username
+  request.env['omniauth.auth']['info']['nickname']
+  end
 end
