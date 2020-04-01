c7751adb41a7331a907029c9d4bbfc75ceb35c19
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-31 22:51:04 -0600

    Add VCR config to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,11 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data('<GITHUB_USER_TOKEN>') { ENV['GITHUB_USER_TOKEN'] }
+  config.allow_http_connections_when_no_cassette = true
+  config.filter_sensitive_data('<GITHUB_CLIENT_ID>') { ENV['GITHUB_CLIENT_ID'] }
+  config.filter_sensitive_data('<GITHUB_CLIENT_SECRET>') { ENV['GITHUB_CLIENT_SECRET'] }
 end
 
 
