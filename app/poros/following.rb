bb6f7350917b01d449cb063aa94e8b42d9255190
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-31 22:24:34 -0600

    Add Following poro

---
+++ app/poros/following.rb
@@ -0,0 +1,15 @@
+class Following
+  attr_reader :login, :html_url
+
+  def initialize(attributes = {})
+    @login = attributes[:login]
+    @html_url = attributes[:html_url]
+  end
+
+  def friendable?(current_user)
+    return false if current_user.friends.pluck(:github_username).include?(login)
+
+    User.exists?(github_username: login)
+  end
+
+end
