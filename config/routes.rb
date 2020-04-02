68afcddaa702ee2208046a0b5378f26adaa5a345
Elom Amouzou <elom1990@yahoo.com>
2020-04-01 19:27:48 -0600

    add activation route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
+  get '/activation/:user_id', to: 'activation#create'
 
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
