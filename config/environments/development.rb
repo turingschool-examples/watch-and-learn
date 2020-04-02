1c0a3eb7477a958605b197dbef8295bf115a84ac
Elom Amouzou <elom1990@yahoo.com>
2020-04-01 20:53:48 -0600

    fix config error; mail sends in localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   # Print deprecation notices to the Rails logger.
