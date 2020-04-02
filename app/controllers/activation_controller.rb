f5eec5a5b1e258ddf7be19d4ecc2a4e932c55d1a
Elom Amouzou <elom1990@yahoo.com>
2020-04-01 19:28:01 -0600

    add activation controller and create action

---
+++ app/controllers/activation_controller.rb
@@ -0,0 +1,17 @@
+class ActivationController < ApplicationController
+  def create
+    user = User.find(params[:user_id])
+    user.update(github_token: auth_github_token, github_username: auth_github_username)
+    redirect_to '/dashboard'
+  end
+
+  private
+
+  def auth_github_token
+    request.env['omniauth.auth']['credentials']['token']
+  end
+
+  def auth_github_username
+    request.env['omniauth.auth']['info']['nickname']
+  end
+end
