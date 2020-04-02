e4e08e44c47805a2fe0c739d443627d75c041093
Elom Amouzou <elom1990@yahoo.com>
2020-04-01 19:35:45 -0600

    refactor user controller, add helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,7 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
-      ActivationMailer.activate_user(user).deliver_now
-      user_name = user.first_name + " " + user.last_name
-      flash[:success] = "Logged in as #{user_name}"
-      flash[:email] = "This account has not yet been activated. Please check your email."
+      activation_process(user)
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
@@ -29,6 +26,18 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
+
+  def activation_process(user)
+    ActivationMailer.activate_user(user).deliver_now
+    render_flash(user)
+
+  end
+
+  def render_flash(user)
+    user_name = user.first_name + " " + user.last_name
+    flash[:success] = "Logged in as #{user_name}"
+    flash[:email] = "This account has not yet been activated. Please check your email."
   end
 
 end
