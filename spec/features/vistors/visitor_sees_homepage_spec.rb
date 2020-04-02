6e195e5b585988ed693d3e413451b3b821eaff68
Elom Amouzou <elom1990@yahoo.com>
2020-04-02 01:03:29 -0600

    add test to make bookmark display falsh message for visitor

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -22,5 +22,22 @@ describe 'Visitor' do
         expect(page).to have_content(tutorial1.description)
       end
     end
+
+    it 'bookmarks displays message if user is not logged in' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit "/tutorials/#{tutorial1.id}"
+
+      click_button 'Bookmark'
+
+      expect(page).to have_content('User must login to bookmark videos.')
+    end
+
   end
 end
