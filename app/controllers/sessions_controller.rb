016d19e9a1bb916de0118494f19f8b6473f0b15c
Maxwell Baird <bairdm71@gmail.com>
2020-05-05 17:22:02 -0600

    progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,36 @@ class SessionsController < ApplicationController
     end
   end
 
+  def edit
+    client_id     = 'e5b765e6ce409b1727a6'
+    client_secret = '2b8abb6c548ffb70c17cca09746c43606c15f8c3'
+    code          = params[:code]
+    response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+
+    pairs = response.body.split("&")
+    response_hash = {}
+    pairs.each do |pair|
+      key, value = pair.split("=")
+      response_hash[key] = value
+    end
+
+    token = response_hash["access_token"]
+
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+    auth = JSON.parse(oauth_response.body)
+
+    user          = User.find_or_create_by(uid: auth["id"])
+    user.username = auth["login"]
+    user.uid      = auth["id"]
+    user.token    = token
+    user.save
+
+    session[:user_id] = user.id
+
+    redirect_to dashboard_path
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path
