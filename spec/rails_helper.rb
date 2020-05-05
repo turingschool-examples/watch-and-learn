e0185dc4984dce347a43b6d9e73b9caacf8d8fdc
Jordan Sewell <jrsewell400@gmail.com>
2020-05-04 18:45:51 -0600

    Rework default_max_wait_time

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,9 +15,11 @@ end
 
 Capybara.javascript_driver = :selenium_chrome_headless
 
-Capybara.configure do |config|
-  config.default_max_wait_time = 180
-end
+# Capybara.configure do |config|
+#   config.default_max_wait_time = 5
+# end
+
+Capybara.default_max_wait_time = 30
 
 SimpleCov.start "rails"
 
