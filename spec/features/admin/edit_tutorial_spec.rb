dee724004503a30cecffffcda2c49fd1c7795f15
Jordan Sewell <jrsewell400@gmail.com>
2020-05-04 18:15:45 -0600

    Add missing quotation mark

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -20,7 +20,7 @@ describe "An Admin can edit a tutorial" do
 
     expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
 
-    within(first(".video)) do
+    within(first(".video")) do
       expect(page).to have_content("How to tie your shoes.")
     end
   end
