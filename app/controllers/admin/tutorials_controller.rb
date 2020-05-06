d3dee4b7f854b583aebc718b46cd692ff2f05c4c
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-06 07:07:06 -0600

    Updates syntax issues found by Rubocop

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -3,10 +3,10 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.find(params[:id])
   end
 
-  def create 
+  def create
     @tutorial = Tutorial.new(tutorial_params)
     if @tutorial.save
-      flash[:success] = "Successfully created tutorial."
+      flash[:success] = 'Successfully created tutorial.'
       redirect_to tutorial_path(@tutorial)
     else
       flash[:error] = @tutorial.errors.full_messages.to_sentence
@@ -35,6 +35,9 @@ class Admin::TutorialsController < Admin::BaseController
   private
 
   def tutorial_params
-    params.require(:tutorial).permit(:tag_list, :title, :description, :thumbnail)
+    params.require(:tutorial).permit(:tag_list,
+                                     :title,
+                                     :description,
+                                     :thumbnail)
   end
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -3,5 +3,5 @@ class Tutorial < ApplicationRecord
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
 
-  validates_presence_of :title, :description, :thumbnail
+  validates :title, :description, :thumbnail, presence: true
 end
