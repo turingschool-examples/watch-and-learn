42a20625d58654ef1fb48f19a156dea4daa76cf8
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-05 23:20:06 -0600

    Adds sad path testing when creating a tutorial

--- spec/features/admin/admin_create_tutorial_spec.rb
+++ spec/features/admin/admin_create_tutorial_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "As an Admin", type: :feature do
 
     expect(current_path).to eq(new_admin_tutorial_path)
 
-    fill_in "Title", with: "A New Tutor Title"
+    fill_in "Title", with: "A New Tutorial Title"
     fill_in "Description", with: "Some instructional structuring"
     fill_in "Thumbnail", with: "https://www.techymob.com/wp-content/uploads/2019/10/How-to-Download-Thumbnail-from-YouTube.jpg"
 
@@ -20,7 +20,23 @@ RSpec.describe "As an Admin", type: :feature do
     
     expect(current_path).to eq(tutorial_path(tutorial))
     expect(page).to have_content("Successfully created tutorial.")
-
   end
+
+  it "I see an error message if info is omitted during new tutorial creation" do
+    admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit new_admin_tutorial_path
+
+    fill_in "Title", with: "Another How-To Title"
+    fill_in "Description", with: ""
+    fill_in "Thumbnail", with: ""
+    
+    click_button "Save"
+    
+    expect(page).to have_content("New Tutorial")
+    expect(page).to have_content("oops")
+  end
+
 
 end
