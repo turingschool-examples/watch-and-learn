dd19760766353b075947974ccf00720d18e20fdb
Anthony Gomes <code@tonygom.es>
2020-05-06 15:45:29 -0600

    Add feature tests

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -10,19 +10,7 @@ describe 'User' do
     fill_in 'session[password]', with: @user.password
     click_on 'Log In'
   end
-  it "once logged in, can see their repositories if they have a token" do
-    @user.update(token: ENV['GITHUB_TOKEN_1'])
-    @user2.update(token: ENV['GITHUB_TOKEN_2'])
 
-    visit '/dashboard'
-
-    expect(page).to have_content("Github")
-    expect(page).to have_link("futbol")
-    expect(page).to have_link("brownfield-of-dreams")
-    expect(page).to have_link("alpha_paradigms_academy")
-    expect(page).to have_link("metaphysics")
-    expect(page).to have_link("metaphysics-dev")
-  end
   it "once logged in, cannot see repositories if missing token" do
     visit '/dashboard'
 
@@ -33,5 +21,27 @@ describe 'User' do
     expect(page).to_not have_link("alpha_paradigms_academy")
     expect(page).to_not have_link("metaphysics")
     expect(page).to_not have_link("metaphysics-dev")
+  end
+
+  it "once logged in, can see their repositories if they have a token" do
+    @user.update(token: ENV['GITHUB_TOKEN_1'])
+    # @user2.update(token: ENV['GITHUB_TOKEN_2'])
+
+    visit '/dashboard'
+
+    expect(page).to have_link("futbol")
+    expect(page).to have_css('a[href="https://github.com/iEv0lv3/futbol"]')
+
+    expect(page).to have_link("brownfield-of-dreams")
+    expect(page).to have_css('a[href="https://github.com/javier-aguilar/brownfield-of-dreams"]')
+
+    expect(page).to have_link("alpha_paradigms_academy")
+    expect(page).to have_css('a[href="https://github.com/quiet-storm/alpha_paradigms_academy"]')
+
+    expect(page).to have_link("metaphysics")
+    expect(page).to have_css('a[href="https://github.com/quiet-storm/metaphysics"]')
+
+    expect(page).to have_link("metaphysics-dev")
+    expect(page).to have_css('a[href="https://github.com/quiet-storm/metaphysics-dev"]')
   end
 end
