c22f7b88c8d04f34e2f60cb3a41ea566133d538c
Jordan Sewell <jrsewell400@gmail.com>
2020-05-07 14:24:53 -0600

    Create new file and copy over remaining code from api.rb

---
+++ app/poros/github/results.rb
@@ -0,0 +1,35 @@
+require 'faraday'
+require 'json'
+require_relative 'follower'
+require_relative 'following'
+require_relative 'repo'
+
+class Results
+  attr_reader :followers, :following, :repos
+
+  def initialize(current_user)
+    @connection = GithubService.connect(current_user)
+    @followers = parse_followers
+    @following = parse_following
+    @repos = parse_repos
+  end
+
+  private
+
+  def parse_info(info)
+    conn = @connection.get("/user/#{info}")
+    JSON.parse(conn.body, symbolize_names: true)
+  end
+
+  def parse_followers
+    parse_info('followers').map { |follower| Follower.new(follower) }
+  end
+
+  def parse_following
+    parse_info('following').map { |user| Following.new(user) }
+  end
+
+  def parse_repos
+    parse_info('repos?sort="created"').map { |repo| Repo.new(repo) }
+  end
+end
