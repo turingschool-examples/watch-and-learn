2b73bddc46b03317db87eeb542655c03d796ad45
David Tran <davidttemail@gmail.com>
2020-05-10 15:38:03 -0600

    Fix rubocop errors

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,7 +1,7 @@
 class Tutorial < ApplicationRecord
   has_many :videos, -> { order(position: :ASC) },
-                    inverse_of: :tutorial,
-                    dependent: :destroy
+                               inverse_of: :tutorial,
+                               dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
 end
