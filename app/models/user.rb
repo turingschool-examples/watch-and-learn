942dc535e18e2931f9e3ead787770dad25bbd246
David Tran <davidttemail@gmail.com>
2020-05-10 15:27:18 -0600

    Update helper method to use [:token]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
   validates :first_name, presence: true
   enum role: { default: 0, admin: 1 }
   has_secure_password
 
   def git_hub_token?
-    !self[:git_hub_token].nil?
+    !self[:token].nil?
   end
 end
