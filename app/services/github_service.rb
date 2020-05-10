dae2c5e30d0e52c1600118dd0ee68269d4ff739a
David Tran <davidttemail@gmail.com>
2020-05-10 15:27:35 -0600

    Remove test key

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,6 @@
 class GithubService
   def self.connect(current_user)
     Faraday.new(url: 'https://api.github.com',
-                params: { access_token: current_user[:git_hub_token] })
+                params: { access_token: current_user[:token] })
   end
 end
