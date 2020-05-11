29d295c4181f968232613bc7b565c53deb6b5708
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-10 21:07:32 -0600

    Adds validation of tutorial id to video spec and model

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,6 @@ class Video < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  validates_presence_of :tutorial_id
 end
