3d46e58737089dc9b144599bae262dec5f74f81e
Jordan Sewell <jrsewell400@gmail.com>
2020-05-12 17:55:14 -0600

    Add get and post routes for sending invite

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,9 @@ Rails.application.routes.draw do
   get '/about', to: 'about#show'
   get '/get_started', to: 'get_started#show'
 
+  get '/invite', to: 'invite#new'
+  post '/invite', to: 'invite#create'
+
   # Is this being used?
   get '/video', to: 'video#show'
 
