7cb18d946d1a33f8cc2d5dc2e1020500cab7ff4f
Maxwell Baird <bairdm71@gmail.com>
2020-05-11 18:47:11 -0600

    fixed rubocop

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,5 +1,6 @@
 class Tutorial < ApplicationRecord
-  has_many :videos, -> { order(position: :ASC) }, inverse_of: :tutorial, dependent: :destroy
+  has_many :videos, -> { order(position: :ASC) }, inverse_of: :tutorial,
+                                                  dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
 
