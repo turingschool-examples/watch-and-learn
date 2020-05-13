e25a313ac534eee2548ca2ebb6f7575011940d99
David Tran <davidttemail@gmail.com>
2020-05-13 15:18:27 -0600

    Configure for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
-    :port => 587,
+    :port => '587',
     :domain => "heroku.com",
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
