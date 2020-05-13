403a8e519a67ceb67808a814ab2419fb38164d36
David Tran <davidttemail@gmail.com>
2020-05-13 14:59:42 -0600

    Add active to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_205503) do
+ActiveRecord::Schema.define(version: 2020_05_13_205302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_205503) do
     t.datetime "updated_at", null: false
     t.string "uid"
     t.string "token"
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
