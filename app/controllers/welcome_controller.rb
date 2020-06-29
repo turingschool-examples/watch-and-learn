7d9028ce359fe67c6d8bf1f98caef06e407c54f1
Mad Hallé <mad.halle@icloud.com>
2020-06-29 14:51:32 -0600

    rubocop fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
-      @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-    end
+    @tutorials = if params[:tag]
+                   Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+                 else
+                   Tutorial.all.paginate(page: params[:page], per_page: 5)
+                 end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
-  validates_presence_of :first_name
+  validates :column, presence: :password
+  validates :column, presence: :first_name
   enum role: { default: 0, admin: 1 }
   has_secure_password
 end
