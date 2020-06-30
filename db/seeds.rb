edb181a51a968b81aa8f0aa4ed3e32f6b4ffecd0
Melanie Tran <melaniexx02@gmail.com>
2020-06-30 15:39:58 -0600

    Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,17 @@ prework_tutorial_data = {
   "playlist_id"=>"PL1Y67f0xPzdN6C-LPuTQ5yzlBoz2joWa5",
   "classroom"=>false,
 }
+
 prework_tutorial = Tutorial.create! prework_tutorial_data
 
 prework_tutorial.videos.create!({
   "title"=>"Prework - Environment Setup",
-  "description"=> Faker::Hipster.paragraph(sentence_count:2, true ),
+  "description"=> Faker::Hipster.paragraph(sentence_count:2, true),
   "video_id"=>"qMkRHW9zE1c",
   "thumbnail"=>"https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg",
   "position"=>1
 })
+
 prework_tutorial.videos.create!({
   "title"=>"Prework - SSH Key Setup",
   "description"=> Faker::Hipster.paragraph(sentence_count:2, true),
