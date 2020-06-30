ee6cf9d319ef215666ed5667a2d0c975936e7b4d
Ross Perry <perryr16@gmail.com>
2020-06-29 19:41:18 -0600

    api call works if user is known, need to apply key somehow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-  def show; end
+  def show
+    token = "484cabdee3b4e5de4226ed80470600b274e435d9"
+    conn = Faraday.new(url: "https://api.github.com") 
+    response = conn.get("/users/perryr16/repos")
+    body = JSON(response.body, symbolize_names: true)
+    repos = body.map{|repo| repo[:name]}
+    repos_5 = repos[0..4]
+    binding.pry
+    
+  end
 
   def new
     @user = User.new
