d33baa4ff8d367f069ac3a106208e488b102df1d
Jack Cullen <jpcullen20@gmail.com>
2020-06-30 14:59:59 -0600

    Add token to users

--- spec/features/user/user_has_a_github_section_spec.rb
+++ spec/features/user/user_has_a_github_section_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 describe 'A registered user' do
   it "can see a github section with 5 repos" do
-    conn = Faraday.new("https://api.github.com") do |req|
-      req.headers["Authorization"] = "token #{ENV["GITHUB_TOKEN"]}"
-    end
+    user = create(:user, token: ENV["GITHUB_TOKEN"])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    repos = conn.get("/user/repos")
-    parsed_repos = JSON.parse(repos.body, symbolize_names: true)
-    require "pry"; binding.pry
+    visit(dashboard_path)
+    expect(page).to have_css('.github')
   end
 end
+
+# conn = Faraday.new("https://api.github.com") do |req|
+#   req.headers["Authorization"] = "token #{ENV["GITHUB_TOKEN"]}"
+# end
+#
+# repos = conn.get("/user/repos")
+# parsed_repos = JSON.parse(repos.body, symbolize_names: true)
+# require "pry"; binding.pry
