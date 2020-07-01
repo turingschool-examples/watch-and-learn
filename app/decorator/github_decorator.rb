351de1e99db04ca4f28cd3a86906ff91adc32323
Taylor Keller <takeller17@gmail.com>
2020-07-01 15:17:42 -0600

    Remove extra variable from list_five_repos

--- app/decorator/github_decorator.rb
+++ app/decorator/github_decorator.rb
@@ -11,7 +11,7 @@ class GithubDecorator
 
   def list_five_repos
     repos = @github_service.user_repos
-    repos = repos[0..4].map do |repo|
+    repos[0..4].map do |repo|
       UserRepository.new({
         name: repo[:name],
         html_url: repo[:html_url]
