d11e6c78d35aadd7e652329993a17d8bcc10ea4e
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-01 17:28:03 -0600

    Created show method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
-  def show; end
+  def show
+    
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers['Authorization'] = "token #{current_user.token}"
+    end
+
+    response = conn.get("user/repos")
+
+
+    parsed = JSON.parse(response.body, symbolized_names: true)[0..4]
+
+    @links = Hash.new(0)
+      parsed.map do |entry|
+      @links[entry["name"]] = entry["url"]
+    end
+  end
+
+
+
+  end
+
+
+
 
   def new
     @user = User.new
