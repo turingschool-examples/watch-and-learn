b0e785deb59c9a09ee92a477f79e67396c2f94fe
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-01 16:35:07 -0600

    simplify methods using helper

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,18 +1,21 @@
 class GithubService
+
   def conn
     Faraday.new(url: 'https://api.github.com') do |faraday|
       faraday.headers['Authorization'] = ENV['GITHUB_API_KEY']
-      # faraday.headers['Authorization'] = "token 484cabdee3b4e5de4226ed80470600b274e435d9"
     end
   end
 
-  def repos_body
-    response = conn.get('/user/repos')
+  def get_url(url)
+    response = conn.get("/user/#{url}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def followers_body
-    response = conn.get('/user/followers')
-    JSON.parse(response.body, symbolize_names: true)
+  def repos
+    get_url('repos')
+  end
+
+  def followers
+    get_url('followers')
   end
 end
