4fc161c2b4d08b45bf01b0603200800f475e97d5
Michael Gallup <Gallup93@icloud.com>
2020-07-01 16:16:29 -0600

    Remove unused code

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 describe 'Github Service' do
-  # let :user do
-  #   create(:user)
-  # end
   describe '#fetch_repos_for_user' do
     it 'returns a list of user repositories' do
       fetched_repos = GithubService.new.fetch_repos_for_user
@@ -14,5 +11,5 @@ describe 'Github Service' do
       expect(first_repo).to have_key :name
       expect(first_repo).to have_key :url
     end
-  end 
+  end
 end
