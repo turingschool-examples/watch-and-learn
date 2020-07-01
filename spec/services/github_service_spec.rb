20a8f5a304ec6ebbb03cd46d53cdf72f409933ce
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-01 16:32:33 -0600

    add test for followers

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe GithubService do
   context "instance methods" do
     it "returns all repos" do
-      service = GithubService.new 
-      repos = service.repos_body 
+      service = GithubService.new
+      repos = service.repos
       expect(repos).to be_an Array
       expect(repos.first).to be_a Hash
       repo_1 = repos.first
@@ -12,5 +12,17 @@ describe GithubService do
       expect(repo_1).to have_key :name
       expect(repo_1).to have_key :html_url
     end
+
+    it "returns all followers" do
+      service = GithubService.new
+      followers = service.followers
+      expect(followers).to be_an Array
+      expect(followers.first).to be_a Hash
+      follower_1 = followers.first
+      
+      expect(follower_1).to have_key :login
+      expect(follower_1).to have_key :html_url
+    end
+
   end
 end
