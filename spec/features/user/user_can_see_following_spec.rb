a4ce2a414c69f11bff09865e2d0ae671c2386755
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-02 10:58:12 -0600

    update test to follow css is view

--- spec/features/user/user_can_see_following_spec.rb
+++ spec/features/user/user_can_see_following_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 describe 'As a registered user' do
   describe 'When I visit the dashboard' do
-    it 'I see a list of my github followers' do
+    it 'I see a list of who i follow on github' do
       user = create(:user, token:  ENV["GITHUB_API_TOKEN_R"])
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/dashboard'
-      within('#github-followers') do
-        expect(page).to have_content("HughBerriez")
-        expect(page).to have_content("rickbacci")
-        expect(page).to have_css(".follower-handle", count:2)
+      within('#github-following') do
+        expect(page).to have_css(".following-handle", count: 3)
       end
     end
 
-    it 'has a link to the github followers handles' do
+    it 'has a link to the github following handles' do
       user = create(:user, token:  ENV["GITHUB_API_TOKEN_R"])
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -24,19 +22,20 @@ describe 'As a registered user' do
 
         expect(page).to have_link("HughBerriez", :href => "https://github.com/HughBerriez")
         expect(page).to have_link("rickbacci", :href => "https://github.com/rickbacci")
+        expect(page).to have_link("stcho", :href => "https://github.com/stcho")
     end
 
-    it 'Does not display a github followers section if the user does not have a token' do
+    it 'Does not display a github following section if the user does not have a token' do
       user = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/dashboard'
 
-      expect(page).to_not have_css(".github_followers")
+      expect(page).to_not have_css('.following-handle')
     end
 
-    it 'Shows correct followers when there is more than one user with different github tokens' do
+    it 'Shows correct following when there is more than one user with different github tokens' do
       user = create(:user, token:  ENV["GITHUB_API_TOKEN"])
       rostam = create(:user, token:  ENV["GITHUB_API_TOKEN_R"])
 
@@ -44,9 +43,10 @@ describe 'As a registered user' do
 
       visit '/dashboard'
 
-      within('#github-followers') do
+      within('#github-following') do
         expect(page).to have_content("HughBerriez")
         expect(page).to have_content("rickbacci")
+        expect(page).to have_content("stcho")
       end
     end
   end
