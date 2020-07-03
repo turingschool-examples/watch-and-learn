d5fdd59b66920e44ec56d500204ecd66946f8057
Mad Hallé <mad.halle@icloud.com>
2020-07-03 09:54:45 -0600

    terminal fix and all tests passing for followings

--- spec/features/user/user_has_a_github_section_spec.rb
+++ spec/features/user/user_has_a_github_section_spec.rb
@@ -53,14 +53,14 @@ describe 'A registered user' do
     #   expect(current_path).to not_eq(dashboard_path)
     # end
   end
-  
+
   it "can see all github followings" do
     user_2 = create(:user, token: ENV["GITHUB_TOKEN_2"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
     visit(dashboard_path)
 
     within(".followings") do
-      expect(page).to have_css(".following", count: 4)
+      expect(page).to have_css(".following", count: 5)
       expect(page).to have_content("jpc20")
 
     end
