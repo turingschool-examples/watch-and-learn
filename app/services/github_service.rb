9d069edcb32e2116c6e319e9104ecca626949123
Josh Tukman <josh.tukman@gmail.com>
2020-07-03 13:34:41 -0600

    Create followers and following methods and refactor api call

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,9 +1,28 @@
 class GithubService
 
-    def repos(token)
-        auth = Faraday.get("https://api.github.com/user?access_token=#{token}")
-        response = JSON.parse(auth.body, symbolize_names: true)
-        all_repos = Faraday.get(response[:repos_url])
+    def initialize(token)
+        @token = token
+    end
+
+    def repos
+        all_repos = conn.get("user/repos")
         repos = JSON.parse(all_repos.body, symbolize_names: true)[0..4].map{|repo| [repo[:html_url], repo[:name]]} 
     end
+
+    def followers
+        auth = conn.get("user/followers")
+        response = JSON.parse(auth.body, symbolize_names: true).map{|follower_info| GithubUser.new(follower_info)}     
+    end
+
+    def following
+        auth = conn.get("user/following")
+        response = JSON.parse(auth.body, symbolize_names: true).map{|follower_info| GithubUser.new(follower_info)}     
+    end
+
+    def conn
+        Faraday.new("https://api.github.com") do |f|
+            f.headers["Authorization"] = "token #{@token}"
+        end
+    end
+
 end
