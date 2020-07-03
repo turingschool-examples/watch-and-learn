c775ac2a3d0f238316855a51c6d0eb921aa256dd
Josh Tukman <josh.tukman@gmail.com>
2020-07-03 13:31:54 -0600

    Add followers and followed_users to show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show    
     if !current_user.token.nil?
-      @repos = SearchResults.new.repos
+      @repos = SearchResults.new.repos(current_user.token)
+      @followers = SearchResults.new.followers(current_user.token)
+      @followed_users = SearchResults.new.following(current_user.token)
     end
   end
 
