3de59073793e73f928f64874aebef88d1c64c1e7
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-03 15:50:42 -0600

    create new tutorial from playlist id

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -3,7 +3,14 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.find(params[:id])
   end
 
-  def create; end
+  def create
+    tutorial_from_playlist = Tutorial.create(new_tutorial_params)
+    tutorial_from_playlist.create_playlist_videos
+    if tutorial_from_playlist.save
+      flash[:success] = "Successfully created tutorial. #{view_context.link_to("View it here", tutorial_path(tutorial_from_playlist.id))}."
+      redirect_to admin_dashboard_path
+    end 
+  end
 
   def new
     @tutorial = Tutorial.new
@@ -28,4 +35,10 @@ class Admin::TutorialsController < Admin::BaseController
   def tutorial_params
     params.require(:tutorial).permit(:tag_list)
   end
+
+  def new_tutorial_params
+    results = YoutubePlaylistResults.new(params[:playlist_id])
+    results.parameters
+  end
+
 end
