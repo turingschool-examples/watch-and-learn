fb5bbb308aec809bbdc4200f743ec49b96d2fd97
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-04 10:42:28 -0600

    controller which specifically deals with github oAuth process

---
+++ app/controllers/github_sessions_controller.rb
@@ -0,0 +1,33 @@
+class GithubSessionsController < ApplicationController
+  def create
+    client_id = ENV['GITHUB_API_KEY']
+    client_secret = ENV['CLIENT_API_KEY']
+    code = params[:code]
+    response = Faraday.post(
+      <<-HEREDOC
+      "https://github.com/login/oauth/access_token?client_id=#{client_id}
+      &client_secret=#{client_secret}&code=#{code}")
+      HEREDOC
+
+    response_hash = {}
+    response.body.split("&").each do |pair|
+      key, value = pair.split("=")
+      response_hash[key] = value
+    end
+
+    token = response_hash["access_token"]
+
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+    auth = JSON.parse(oauth_response.body)
+
+    current_user.username = auth["login"]
+    current_user.uid      = auth["id"]
+    current_user.token    = token
+    current_user.save
+
+    session[:user_id] = current_user.id
+
+    redirect_to dashboard_path
+  end
+end
