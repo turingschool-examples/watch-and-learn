8a5fb433bbadcf64e8e9f6b95f56c9e9ffa3d90b
Melanie Tran <melaniexx02@gmail.com>
2020-07-04 11:25:37 -0600

    Create test for user seeing followers

---
+++ spec/features/user/user_can_see_followers_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'User Dashboard' do
+  it 'displays users repos' do
+    user = create(:user, github_token: ENV["GITHUB_ACCESS_TOKEN"])
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within('.github') do
+        expect(page).to have_css('.followers', count: 3)
+    end
+
+  end
+end
+# As a logged in user
+# When I visit /dashboard
+# Then I should see a section for "Github"
+# And under that section I should see another section titled "Followers"
+# And I should see list of all followes with their handles linking to their Github profile
