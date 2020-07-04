5c18cf4e1909680e501039d34fe7950b4f7c0cdd
Melanie Tran <melaniexx02@gmail.com>
2020-07-04 12:37:53 -0600

    Edit description in test to make it more specific

--- spec/features/user/user_can_see_repos_spec.rb
+++ spec/features/user/user_can_see_repos_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User Dashboard' do
-  it 'displays users repos' do
+  it 'displays users repos for login user only' do
     user1 = create(:user, github_token: ENV["GITHUB_ACCESS_TOKEN"])
     user2 = create(:user, github_token: ENV["SECOND_GITHUB_ACCESS_TOKEN"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
