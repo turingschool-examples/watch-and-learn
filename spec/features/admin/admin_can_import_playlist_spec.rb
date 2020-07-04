c4d4cb9db5e3de21c86ed78026b711f59292d011
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-03 23:48:37 -0600

    add sad path test

--- spec/features/admin/admin_can_import_playlist_spec.rb
+++ spec/features/admin/admin_can_import_playlist_spec.rb
@@ -7,29 +7,41 @@ feature "An admin visiting the admin dashboard" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit new_admin_tutorial_path
-
     click_on "Import YouTube Playlist"
 
     expect(current_path).to eq(new_admin_youtube_playlist_path)
 
     fill_in :playlist_id, with: "PL1Y67f0xPzdOq2FcpWnawJeyJ3ELUdBkJ"
-
     click_on "Import Playlist"
 
     expect(current_path).to eq(admin_dashboard_path)
-
     expect(page).to have_content("Successfully created tutorial. View it here.")
 
     click_on "View it here"
     tutorial = Tutorial.last
+
     expect(current_path).to eq(tutorial_path(tutorial))
-
     expect(page).to have_css(".video-link", count: 10)
+    video_1 = Video.find_by(position: 0)
+    video_2 = Video.find_by(position: 1)
+    video_3 = Video.find_by(position: 2)
 
-    # confirm order with orderly using id = video.position
+    expect(video_1.title).to appear_before(video_2.title)
+    expect(video_2.title).to appear_before(video_3.title)
   end
 
-  xscenario "cannot add a tutorial using bad youtube playlist id" do
-    #create sad path tests for invalid playlist id
+  scenario "cannot add a tutorial using bad youtube playlist id" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit new_admin_tutorial_path
+    click_on "Import YouTube Playlist"
+
+    expect(current_path).to eq(new_admin_youtube_playlist_path)
+
+    fill_in :playlist_id, with: "12345abcde"
+    click_on "Import Playlist"
+
+    expect(current_path).to eq(new_admin_youtube_playlist_path)
+    expect(page).to have_content("Sorry, that ID is not valid. Try again?")
   end
 end
