f3ac8ab5283472019b8d55a3b5ce38df1bcb2b82
Josh Tukman <josh.tukman@gmail.com>
2020-07-05 11:24:40 -0600

    Add 'on create' to password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates_presence_of :password, on: :create
   validates_presence_of :first_name
   enum role: { default: 0, admin: 1 }
   has_secure_password
