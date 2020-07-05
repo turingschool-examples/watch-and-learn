805e259642da2fadc32a3c0dfa88dfd5e79fdf4c
Jesse Gietzen <gietzenjesse@gmail.com>
2020-07-05 11:37:56 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, on: :create
-  validates_presence_of :first_name
+  validates :password, presence: true, on: :create
+  validates :first_name, presence: true
   enum role: { default: 0, admin: 1 }
   has_secure_password
 end
