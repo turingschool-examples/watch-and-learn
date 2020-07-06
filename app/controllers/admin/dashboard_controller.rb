c1806c32e202cb75f269dae9dd8bc7d3960e56bb
Josh Tukman <josh.tukman@gmail.com>
2020-07-06 16:01:03 -0600

    Add query param to show action that triggers a flash message with tutorial is created

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < Admin::BaseController
   def show
+    if params[:tutorial] == "create"
+      tutorial = Tutorial.last
+      flash[:success] = %Q[Successfully created tutorial. To view click here <a href="/tutorials/#{tutorial.id}">"#{tutorial.title}"</a>].html_safe
+    end
     @facade = AdminDashboardFacade.new
   end
+
 end
