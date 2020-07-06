59674236cc777550e7a88bfd4d196231204deff4
Josh Tukman <josh.tukman@gmail.com>
2020-07-06 16:06:01 -0600

    Validates presence of tutorial_id for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ApplicationRecord
+  validates :tutorial_id, presence: true
+  
   has_many :user_videos, dependent: :destroy
   has_many :users, through: :user_videos
   belongs_to :tutorial
