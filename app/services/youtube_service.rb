4c505d5a7b7220c3e1133cc506309f99165ed8e2
Josh Tukman <josh.tukman@gmail.com>
2020-07-06 16:07:07 -0600

    Add playlist and playlist_items actions

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -1,4 +1,36 @@
 class YoutubeService
+  
+  def playlist(id)
+    params = { part: 'snippet', id: id }
+
+    get_json('youtube/v3/playlists', params)
+  end
+
+  def playlist_items(id, tutorial)
+    params = { part: 'snippet', playlistId: id, maxResults: 50 }
+
+    response = get_json('youtube/v3/playlistItems', params)
+    response[:items].each_with_index do |item, index|
+      tutorial.videos.create(title: item[:snippet][:title], 
+        description: item[:snippet][:description], 
+        video_id: item[:snippet][:resourceId][:videoId],
+        thumbnail: item[:snippet][:thumbnails][:default][:url],
+        tutorial_id: tutorial.id,
+        position: index)
+      end
+  end
+
+  # def playlist_items_with_thumbnail(id, tutorial)
+    
+  #   params = { part: 'snippet', thumbnails: id, maxResults: 50 }
+
+  #   response = get_json('youtube/v3/playlistItems', params)
+   
+
+  #   response[:items].each_with_index do |item, index|
+  #   end
+  # end
+
   def video_info(id)
     params = { part: 'snippet,contentDetails,statistics', id: id }
 
