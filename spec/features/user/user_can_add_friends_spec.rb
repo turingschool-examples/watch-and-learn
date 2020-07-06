7b8c247032d8fb053151aef2789ef7784d8b2dc8
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-06 16:50:38 -0600

    Added fixture files for followers following and repos

---
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -0,0 +1,13 @@
+# require 'rails_helper'
+#
+#   feature 'A user can follow another user' do
+#     scenario 'if both are in the database and they are in the github following' do
+#       user_1 = create(:user)
+#       user_2 = create(:user)
+#       user_3 = create(:user)
+#
+#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+#
+#       visit "/dashboard"
+# end
+#end
