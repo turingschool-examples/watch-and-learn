e62414778998ea85f07f5dc950626ab47d6aba2d
Josh Tukman <josh.tukman@gmail.com>
2020-07-06 17:18:28 -0600

    Add test for deleting tutorials and their videos by deleting just the tutorial

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -3,9 +3,21 @@ require 'rails_helper'
 feature "An admin can delete a tutorial" do
   scenario "and it should no longer exist" do
     admin = create(:admin)
-    create_list(:tutorial, 2)
-
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    tutorial1 = create(:tutorial)
+    tutorial2 = create(:tutorial)
+
+    create(:video, tutorial_id: tutorial1.id)
+    create(:video, tutorial_id: tutorial1.id)
+    create(:video, tutorial_id: tutorial1.id)
+    create(:video, tutorial_id: tutorial2.id)
+    create(:video, tutorial_id: tutorial2.id)
+    
+    expect(tutorial1.videos.count).to eq(3)
+    expect(tutorial2.videos.count).to eq(2)
+
+    expect(Video.all.count).to eq(5)
 
     visit "/admin/dashboard"
 
@@ -16,5 +28,7 @@ feature "An admin can delete a tutorial" do
     end
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
+    expect(tutorial2.videos.count).to eq(2)
+    expect(Video.all.count).to eq(2)
   end
 end
