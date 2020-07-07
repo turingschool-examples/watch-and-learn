2ed97f61a974af3bacd345b8d820ea4deb45841f
Melanie Tran <melaniexx02@gmail.com>
2020-07-07 17:00:19 -0600

    Deleted unnecessary tests

--- spec/features/user/user_can_see_followers_spec.rb
+++ spec/features/user/user_can_see_followers_spec.rb
@@ -9,19 +9,7 @@ describe 'User Dashboard' do
     visit dashboard_path
 
     within('.github') do
-      expect(page).to have_css(".follower", count: 4)
-      expect(page).to have_content("melatran")
-      expect(page).to have_content("stellakunzang")
-      expect(page).to have_content("Rostammahabadi")
+      expect(page).to have_css(".follower", count: 5)
     end
   end
 end
-
-
-
-
-# As a logged in user
-# When I visit /dashboard
-# Then I should see a section for "Github"
-# And under that section I should see another section titled "Followers"
-# And I should see list of all followes with their handles linking to their Github profile
