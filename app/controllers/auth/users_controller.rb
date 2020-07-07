3ec98e14770edae281653a0d88934490a0e35bec
Jane Greene <janegreenecasa@gmail.com>
2020-07-06 18:43:37 -0600

    Added github username to user table still wip on friendships

--- app/controllers/auth/users_controller.rb
+++ app/controllers/auth/users_controller.rb
@@ -3,7 +3,8 @@ class Auth::UsersController < ApplicationController
 
   def update
     user = User.find(current_user.id)
-    user.update(github_token: auth_hash[:credentials][:token])
+    user.update(github_token: auth_hash[:credentials][:token],
+    github_username: auth_hash[:info][:nickname])
     redirect_to dashboard_path
   end
 
---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,5 @@
+class FriendsController < ApplicationController
+  def create
+    
+  end
+end
