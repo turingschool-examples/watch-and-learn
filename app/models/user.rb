aeff18549f1e95fbf03ca990e067859f0cc9993b
Michael Gallup <Gallup93@icloud.com>
2020-07-07 09:15:13 -0600

    Add model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
 
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
   validates :first_name, presence: true
