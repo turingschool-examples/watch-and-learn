7611b8dbba4dc1e20ec9c0ad3ef8fdadc420bf08
Taylor Keller <takeller17@gmail.com>
2020-07-06 18:30:30 -0600

    Adds sort_by_tutorial class method to UserVideo model

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,4 +1,13 @@
 class UserVideo < ApplicationRecord
   belongs_to :video
   belongs_to :user
+
+  def self.sort_by_tutorial
+
+    videos = all.map { |user_video| user_video.video }
+    grouped_videos = videos.group_by { |video| video.tutorial }
+    grouped_videos.map do |tutorial, videos|
+      [tutorial, videos.sort_by { |video| video.position }]
+    end.to_h
+  end
 end
