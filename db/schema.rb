be2ddfcf968e78a899bb0addd7985d7b304161bf
Michael Gallup <Gallup93@icloud.com>
2020-07-07 09:01:31 -0600

    Add freindships join table to database, Add ghub_username column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_211012) do
+ActiveRecord::Schema.define(version: 2020_07_07_031557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
@@ -68,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_211012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.string "ghub_username"
     t.index ["email"], name: "index_users_on_email"
   end
 
