24de8ad06266d768778aa980e19ccd131a04dc8e
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-07 17:35:00 -0600

    added method filter_tutorials to account for if a user is logged in to show the tutorials which are classroom

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @tutorials = if params[:tag]
-                   Tutorial.tagged_with(params[:tag])
-                           .paginate(page: params[:page], per_page: 5)
-                 else
-                   Tutorial.all.paginate(page: params[:page], per_page: 5)
-                 end
+    @tutorials = Tutorial.filter_tutorials(params, current_user)
   end
 end
