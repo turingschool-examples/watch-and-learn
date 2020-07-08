10f7e8d2a8bbacdcc8b5669b22222f800a99160a
Michael Gallup <Gallup93@icloud.com>
2020-07-07 22:34:07 -0600

    Add emails controller

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,16 @@
+class EmailsController < ApplicationController
+  def create
+    recipient = User.find(session[:user_id])
+    EmailConfirmationMailer.inform(recipient).deliver_now
+
+    flash[:success] = "Logged in as #{recipient.first_name}"
+    flash[:notice] = 'This account has not yet been activated. Please check your email.'
+    redirect_to "/dashboard"
+  end
+
+  def update
+    user = current_user.update_attribute(:email_status, true)
+    flash[:success] = "Thank you! Your account is now activated."
+    redirect_to "/activated"
+  end
+end
