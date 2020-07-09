ff920d3136a67dd0ee0ca252c6aaa82d9298986a
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-08 23:29:58 -0600

    add github authorization

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :github, ENV['GITHUB_API_KEY'], ENV['GITHUB_SECRET']
 end
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :github, ENV['GITHUB_API_TOKEN_R'], ENV['CLIENT_API_KEY']
-# end
