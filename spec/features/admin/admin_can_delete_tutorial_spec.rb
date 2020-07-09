c145fa92cd29b005045196010a8516cf5460699d
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-08 20:29:32 -0600

    added test for deleting videos associated with the tutorial when deleted

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -17,4 +17,29 @@ feature "An admin can delete a tutorial" do
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
+
+  it "deleted videos associated with the tutorial" do
+    admin = create(:admin)
+
+    tutorial1 = create(:tutorial)
+    tutorial2 = create(:tutorial)
+
+    video1 = create(:video, tutorial_id: tutorial1.id)
+    video2 = create(:video, tutorial_id: tutorial1.id)
+    video3 = create(:video, tutorial_id: tutorial2.id)
+    video4 = create(:video, tutorial_id: tutorial2.id)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/admin/dashboard"
+
+    within(first('.admin-tutorial-card')) do
+      click_on 'Delete'
+    end
+
+    videos = Video.all
+
+    expect(videos.length).to eq(2)
+  end
+
 end
